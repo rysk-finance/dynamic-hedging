64301e46b0686863f2e7d777d978aa42e0185594
Joshua Hart <joshua.hart@protonmail.com>
2022-03-30 14:45:25 +0100

    migrate deposit/withdraw tests to v2

--- test/LiquidityPoolDepositWithdraw.ts
+++ test/LiquidityPoolDepositWithdraw.ts
@@ -10,7 +10,8 @@ import {
 	fromUSDC,
 	fmtExpiration,
 	toOpyn,
-	tFormatUSDC
+	tFormatUSDC,
+	scaleNum
 } from "../utils/conversion-helper"
 import { deployMockContract, MockContract } from "@ethereum-waffle/mock-contract"
 import moment from "moment"
@@ -20,16 +21,22 @@ import bs from "black-scholes"
 import { expect } from "chai"
 import Otoken from "../artifacts/contracts/packages/opyn/core/Otoken.sol/Otoken.json"
 import LiquidityPoolSol from "../artifacts/contracts/LiquidityPool.sol/LiquidityPool.json"
+import { UniswapV3HedgingReactor } from "../types/UniswapV3HedgingReactor"
 import { ERC20 } from "../types/ERC20"
 import { ERC20Interface } from "../types/ERC20Interface"
 import { MintableERC20 } from "../types/MintableERC20"
-import { OptionRegistry } from "../types/OptionRegistry"
+import { OptionRegistryV2 } from "../types/OptionRegistryV2"
 import { Otoken as IOToken } from "../types/Otoken"
 import { PriceFeed } from "../types/PriceFeed"
 import { LiquidityPool } from "../types/LiquidityPool"
 import { Volatility } from "../types/Volatility"
 import { WETH } from "../types/WETH"
 import { Protocol } from "../types/Protocol"
+import { Controller } from "../types/Controller"
+import { AddressBook } from "../types/AddressBook"
+import { Oracle } from "../types/Oracle"
+import { NewMarginCalculator } from "../types/NewMarginCalculator"
+import { setupTestOracle } from "./helpers"
 import {
 	ADDRESS_BOOK,
 	GAMMA_CONTROLLER,
@@ -37,11 +44,15 @@ import {
 	OTOKEN_FACTORY,
 	USDC_ADDRESS,
 	USDC_OWNER_ADDRESS,
-	WETH_ADDRESS
+	WETH_ADDRESS,
+	CONTROLLER_OWNER,
+	GAMMA_ORACLE_NEW
 } from "./constants"
+import { MockChainlinkAggregator } from "../types/MockChainlinkAggregator"
+
 let usd: MintableERC20
 let weth: WETH
-let optionRegistry: OptionRegistry
+let optionRegistryV2: OptionRegistryV2
 let optionProtocol: Protocol
 let signers: Signer[]
 let volatility: Volatility
@@ -52,6 +63,14 @@ let priceFeed: PriceFeed
 let ethUSDAggregator: MockContract
 let rate: string
 
+let ethLiquidityPool: LiquidityPool
+let uniswapV3HedgingReactor: UniswapV3HedgingReactor
+let controller: Controller
+let addressBook: AddressBook
+let newCalculator: NewMarginCalculator
+let oracle: Oracle
+let opynAggregator: MockChainlinkAggregator
+
 const IMPLIED_VOL = "60"
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
@@ -59,8 +78,9 @@ const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
 // Date for option to expire on format yyyy-mm-dd
 // Will automatically convert to 08:00 UTC timestamp
-// First mined block will be timestamped 2021-07-13 20:44 UTC
-const expiryDate: string = "2021-08-05"
+// First mined block will be timestamped 2022-02-27 19:05 UTC
+const expiryDate: string = "2022-04-05"
+
 // decimal representation of a percentage
 const rfr: string = "0.03"
 // edit depending on the chain id to be tested on
@@ -87,6 +107,19 @@ const minExpiry = 86400 * 7
 // 365 days in seconds
 const maxExpiry = 86400 * 365
 
+const productSpotShockValue = scaleNum("0.6", 27)
+// array of time to expiry
+const day = 60 * 60 * 24
+const timeToExpiry = [day * 7, day * 14, day * 28, day * 42, day * 56]
+// array of upper bound value correspond to time to expiry
+const expiryToValue = [
+	scaleNum("0.1678", 27),
+	scaleNum("0.237", 27),
+	scaleNum("0.3326", 27),
+	scaleNum("0.4032", 27),
+	scaleNum("0.4603", 27)
+]
+
 /* --- end variables to change --- */
 
 const expiration = moment.utc(expiryDate).add(8, "h").valueOf() / 1000
@@ -103,31 +136,177 @@ describe("Liquidity Pools Deposit Withdraw", async () => {
 					forking: {
 						chainId: 1,
 						jsonRpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY}`,
-						blockNumber: 12821000
+						blockNumber: 14290000
 					}
 				}
 			]
 		})
-	})
-	//   after(async function () {
-	//     await network.provider.request({
-	//       method: 'hardhat_reset',
-	//       params: [],
-	//     })
-	//   })
-	it("Deploys the Option Registry", async () => {
+		// impersonate the opyn controller owner
+		await network.provider.request({
+			method: "hardhat_impersonateAccount",
+			params: [CONTROLLER_OWNER[chainId]]
+		})
 		signers = await ethers.getSigners()
+		const [sender] = signers
+
+		const signer = await ethers.getSigner(CONTROLLER_OWNER[chainId])
+		await sender.sendTransaction({
+			to: signer.address,
+			value: ethers.utils.parseEther("1.0") // Sends exactly 1.0 ether
+		})
+		// get an instance of the controller
+		controller = (await ethers.getContractAt(
+			"contracts/packages/opyn/core/Controller.sol:Controller",
+			GAMMA_CONTROLLER[chainId]
+		)) as Controller
+		// get an instance of the addressbook
+		addressBook = (await ethers.getContractAt(
+			"contracts/packages/opyn/core/AddressBook.sol:AddressBook",
+			ADDRESS_BOOK[chainId]
+		)) as AddressBook
+		// get the oracle
+		const res = await setupTestOracle(await signers[0].getAddress())
+		//@ts-ignore
+		oracle = res[0]
+		//@ts-ignore
+		opynAggregator = res[1]
+		// deploy the new calculator
+		const newCalculatorInstance = await ethers.getContractFactory("NewMarginCalculator")
+		newCalculator = (await newCalculatorInstance.deploy(
+			GAMMA_ORACLE_NEW[chainId],
+			ADDRESS_BOOK[chainId]
+		)) as NewMarginCalculator
+		// deploy the new whitelist
+		const newWhitelistInstance = await ethers.getContractFactory("NewWhitelist")
+		const newWhitelist = await newWhitelistInstance.deploy(ADDRESS_BOOK[chainId])
+		// update the addressbook with the new calculator and whitelist addresses
+		await addressBook.connect(signer).setMarginCalculator(newCalculator.address)
+		await addressBook.connect(signer).setWhitelist(newWhitelist.address)
+		// update the whitelist and calculator in the controller
+		await controller.connect(signer).refreshConfiguration()
+		// whitelist collateral
+		await newWhitelist.whitelistCollateral(WETH_ADDRESS[chainId])
+		await newWhitelist.whitelistCollateral(USDC_ADDRESS[chainId])
+		// whitelist products
+		// normal calls
+		await newWhitelist.whitelistProduct(
+			WETH_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			WETH_ADDRESS[chainId],
+			false
+		)
+		// normal puts
+		await newWhitelist.whitelistProduct(
+			WETH_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			true
+		)
+		// usd collateralised calls
+		await newWhitelist.whitelistProduct(
+			WETH_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			false
+		)
+		// eth collateralised puts
+		await newWhitelist.whitelistProduct(
+			WETH_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			WETH_ADDRESS[chainId],
+			true
+		)
+		// whitelist vault type 0 collateral
+		await newWhitelist.whitelistCoveredCollateral(WETH_ADDRESS[chainId], WETH_ADDRESS[chainId], false)
+		await newWhitelist.whitelistCoveredCollateral(USDC_ADDRESS[chainId], WETH_ADDRESS[chainId], true)
+		// whitelist vault type 1 collateral
+		await newWhitelist.whitelistNakedCollateral(USDC_ADDRESS[chainId], WETH_ADDRESS[chainId], false)
+		await newWhitelist.whitelistNakedCollateral(WETH_ADDRESS[chainId], WETH_ADDRESS[chainId], true)
+		// set product spot shock values
+		// usd collateralised calls
+		await newCalculator.setSpotShock(
+			WETH_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			false,
+			productSpotShockValue
+		)
+		// usd collateralised puts
+		await newCalculator.setSpotShock(
+			WETH_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			true,
+			productSpotShockValue
+		)
+		// eth collateralised calls
+		await newCalculator.setSpotShock(
+			WETH_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			WETH_ADDRESS[chainId],
+			false,
+			productSpotShockValue
+		)
+		// eth collateralised puts
+		await newCalculator.setSpotShock(
+			WETH_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			WETH_ADDRESS[chainId],
+			true,
+			productSpotShockValue
+		)
+		// set expiry to value values
+		// usd collateralised calls
+		await newCalculator.setUpperBoundValues(
+			WETH_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			false,
+			timeToExpiry,
+			expiryToValue
+		)
+		// usd collateralised puts
+		await newCalculator.setUpperBoundValues(
+			WETH_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			true,
+			timeToExpiry,
+			expiryToValue
+		)
+		// eth collateralised calls
+		await newCalculator.setUpperBoundValues(
+			WETH_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			WETH_ADDRESS[chainId],
+			false,
+			timeToExpiry,
+			expiryToValue
+		)
+		// eth collateralised puts
+		await newCalculator.setUpperBoundValues(
+			WETH_ADDRESS[chainId],
+			USDC_ADDRESS[chainId],
+			WETH_ADDRESS[chainId],
+			true,
+			timeToExpiry,
+			expiryToValue
+		)
+	})
+
+	it("Deploys the Option Registry", async () => {
+		signers = await hre.ethers.getSigners()
 		senderAddress = await signers[0].getAddress()
 		receiverAddress = await signers[1].getAddress()
 		// deploy libraries
-		const constantsFactory = await ethers.getContractFactory("Constants")
-		const interactionsFactory = await ethers.getContractFactory("OpynInteractions")
+		const constantsFactory = await hre.ethers.getContractFactory("Constants")
+		const interactionsFactory = await hre.ethers.getContractFactory("OpynInteractionsV2")
 		const constants = await constantsFactory.deploy()
 		const interactions = await interactionsFactory.deploy()
 		// deploy options registry
-		const optionRegistryFactory = await ethers.getContractFactory("OptionRegistry", {
+		const optionRegistryV2Factory = await hre.ethers.getContractFactory("OptionRegistryV2", {
 			libraries: {
-				OpynInteractions: interactions.address
+				OpynInteractionsV2: interactions.address
 			}
 		})
 		// get and transfer weth
@@ -143,45 +322,37 @@ describe("Liquidity Pools Deposit Withdraw", async () => {
 		const signer = await ethers.getSigner(USDC_OWNER_ADDRESS[chainId])
 		await usd.connect(signer).transfer(senderAddress, toWei("1000").div(oTokenDecimalShift18))
 		await weth.deposit({ value: utils.parseEther("99") })
-		const _optionRegistry = (await optionRegistryFactory.deploy(
+		const _optionRegistryV2 = (await optionRegistryV2Factory.deploy(
 			USDC_ADDRESS[chainId],
 			OTOKEN_FACTORY[chainId],
 			GAMMA_CONTROLLER[chainId],
 			MARGIN_POOL[chainId],
 			senderAddress,
 			ADDRESS_BOOK[chainId]
-		)) as OptionRegistry
-		optionRegistry = _optionRegistry
-		expect(optionRegistry).to.have.property("deployTransaction")
+		)) as OptionRegistryV2
+		optionRegistryV2 = _optionRegistryV2
+		expect(optionRegistryV2).to.have.property("deployTransaction")
 	})
 	it("Should deploy price feed", async () => {
-		ethUSDAggregator = await deployMockContract(signers[0], AggregatorV3Interface.abi)
-
 		const priceFeedFactory = await ethers.getContractFactory("PriceFeed")
 		const _priceFeed = (await priceFeedFactory.deploy()) as PriceFeed
 		priceFeed = _priceFeed
-		await priceFeed.addPriceFeed(ZERO_ADDRESS, usd.address, ethUSDAggregator.address)
-		await priceFeed.addPriceFeed(weth.address, usd.address, ethUSDAggregator.address)
-		const feedAddress = await priceFeed.priceFeeds(ZERO_ADDRESS, usd.address)
-		expect(feedAddress).to.eq(ethUSDAggregator.address)
-		rate = "56770839675"
-		await ethUSDAggregator.mock.latestRoundData.returns(
-			"55340232221128660932",
-			rate,
-			"1607534965",
-			"1607535064",
-			"55340232221128660932"
-		)
-		await ethUSDAggregator.mock.decimals.returns("8")
+		await priceFeed.addPriceFeed(ZERO_ADDRESS, usd.address, opynAggregator.address)
+		await priceFeed.addPriceFeed(weth.address, usd.address, opynAggregator.address)
+		// oracle returns price denominated in 1e8
+		const oraclePrice = await oracle.getPrice(weth.address)
+		// pricefeed returns price denominated in 1e18
+		const priceFeedPrice = await priceFeed.getNormalizedRate(weth.address, usd.address)
+		expect(oraclePrice.mul(10_000_000_000)).to.equal(priceFeedPrice)
 	})
 
 	it("Should deploy option protocol and link to registry/price feed", async () => {
 		const protocolFactory = await ethers.getContractFactory("Protocol")
 		optionProtocol = (await protocolFactory.deploy(
-			optionRegistry.address,
+			optionRegistryV2.address,
 			priceFeed.address
 		)) as Protocol
-		expect(await optionProtocol.optionRegistry()).to.equal(optionRegistry.address)
+		expect(await optionProtocol.optionRegistryV2()).to.equal(optionRegistryV2.address)
 	})
 
 	it("Creates a liquidity pool with USDC (erc20) as strikeAsset", async () => {
@@ -242,15 +413,15 @@ describe("Liquidity Pools Deposit Withdraw", async () => {
 				maxCallStrikePrice,
 				minPutStrikePrice,
 				maxPutStrikePrice,
-				minExpiry: fmtExpiration(minExpiry),
-				maxExpiry: fmtExpiration(maxExpiry)
+				minExpiry: minExpiry,
+				maxExpiry: maxExpiry
 			},
 			await signers[0].getAddress()
 		)) as LiquidityPool
 
 		const lpAddress = lp.address
 		liquidityPool = new Contract(lpAddress, LiquidityPoolSol.abi, signers[0]) as LiquidityPool
-		optionRegistry.setLiquidityPool(liquidityPool.address)
+		optionRegistryV2.setLiquidityPool(liquidityPool.address)
 	})
 
 	it("Deposit to the liquidityPool", async () => {
@@ -291,7 +462,7 @@ describe("Liquidity Pools Deposit Withdraw", async () => {
 		const [sender, receiver] = signers
 		await usd.approve(liquidityPool.address, toUSDC(liquidityPoolUsdcDeposit))
 		await liquidityPool.deposit(toUSDC(liquidityPoolUsdcDeposit), senderAddress)
-		const sendAmount = toUSDC("1000")
+		const sendAmount = toUSDC("10000")
 		const usdReceiver = usd.connect(receiver)
 		await usdReceiver.approve(liquidityPool.address, sendAmount)
 		const lpReceiver = liquidityPool.connect(receiver)
@@ -311,8 +482,9 @@ describe("Liquidity Pools Deposit Withdraw", async () => {
 		const { timestamp } = block
 		const priceQuote = await priceFeed.getNormalizedRate(weth.address, usd.address)
 		const strikePrice = priceQuote.sub(toWei(strike))
+		console.log({ strikePrice: utils.formatEther(strikePrice) })
 		const proposedSeries = {
-			expiration: fmtExpiration(expiration),
+			expiration: expiration,
 			isPut: PUT_FLAVOR,
 			strike: BigNumber.from(strikePrice),
 			strikeAsset: usd.address,
