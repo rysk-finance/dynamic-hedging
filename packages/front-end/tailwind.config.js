eb36459fc21ed235c43d097a84627d55b4ce9d09
Hari Chauhan <hari-c@hotmail.co.uk>
2022-04-22 15:34:35 +0200

    Add mono font

--- packages/front-end/tailwind.config.js
+++ packages/front-end/tailwind.config.js
@@ -8,6 +8,23 @@ module.exports = {
         bone: "#e6e4d8",
         cyan: "#05f7e7",
       },
+      keyframes: {
+        "border-round": {
+          "0%": { borderRadius: "0px" },
+          "100%": { borderRadius: "9999px" },
+        },
+      },
+      animation: {
+        "border-round": "border-round 5s ease-in-out",
+      },
+      fontFamily: {
+        "dm-mono": ["DM Mono"],
+      },
+    },
+  },
+  variants: {
+    extend: {
+      animation: ["hover", "group-hover"],
     },
   },
   plugins: [],
