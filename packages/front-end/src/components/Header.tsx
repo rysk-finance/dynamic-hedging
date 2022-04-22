aafadbc2157392ef12c328912e58a4b53b62c4d5
Hari Chauhan <hari-c@hotmail.co.uk>
2022-04-22 15:55:57 +0200

    Add wallet connect button

--- packages/front-end/src/components/Header.tsx
+++ packages/front-end/src/components/Header.tsx
@@ -1,5 +1,14 @@
+import { useConnectWallet } from "@web3-onboard/react";
 import React from "react";
+import { Button } from "./Button";
 
 export const Header: React.FC = () => {
-  return <div className="fixed w-full h-16 bg-black"></div>;
+  const [{ wallet }, connect] = useConnectWallet();
+
+  return (
+    <div className="fixed w-full h-16 t-0 flex items-center px-8 justify-between">
+      <h1>Rysk</h1>
+      {!wallet && <Button onClick={() => connect({})}>Connect</Button>}
+    </div>
+  );
 };
