f06fd58e904fa927d8de993c97d55d2600219dbc
Hari Chauhan <hari-c@hotmail.co.uk>
2022-04-22 13:42:13 +0200

    Add tailwind color config

--- packages/front-end/tailwind.config.js
+++ packages/front-end/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#000000",
+        white: "#ffffff",
+        bone: "#e6e4d8",
+        cyan: "#05f7e7",
+      },
+    },
   },
   plugins: [],
 };
