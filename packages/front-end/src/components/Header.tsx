9a5191538a6a883887e169ad080afb8b4c0e3d83
Hari Chauhan <hari-c@hotmail.co.uk>
2022-04-22 16:33:46 +0200

    Styling update

--- packages/front-end/src/components/Header.tsx
+++ packages/front-end/src/components/Header.tsx
@@ -6,8 +6,8 @@ export const Header: React.FC = () => {
   const [{ wallet }, connect] = useConnectWallet();
 
   return (
-    <div className="fixed w-full h-16 t-0 flex items-center px-8 justify-between">
-      <h1>Rysk</h1>
+    <div className="fixed w-full h-16 t-0 flex items-center px-16 justify-between">
+      <h3>Rysk</h3>
       {!wallet && <Button onClick={() => connect({})}>Connect</Button>}
     </div>
   );
