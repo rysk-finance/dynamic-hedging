863a8921b2d984485100ef586cb24413e2fe1683
Hari Chauhan <hari-c@hotmail.co.uk>
2022-04-23 19:33:49 +0200

    Add logo to header

--- packages/front-end/src/components/Header.tsx
+++ packages/front-end/src/components/Header.tsx
@@ -1,20 +1,34 @@
-import { useConnectWallet } from "@web3-onboard/react";
 import React from "react";
+import { Link } from "react-router-dom";
 import { useWalletContext } from "../App";
 import { Button } from "./Button";
 
 export const Header: React.FC = () => {
-  const [{ wallet }] = useConnectWallet();
   const { connectWallet, provider } = useWalletContext();
 
   return (
-    <div className="fixed w-full h-16 t-0 flex items-center px-16 justify-between">
-      <h3>Rysk</h3>
-      {!provider ? (
-        <Button onClick={() => connectWallet?.()}>Connect</Button>
-      ) : (
-        <p>Connected</p>
-      )}
+    <div className="fixed w-full h-24 t-0 flex items-center px-16 justify-between border-b-2 border-black">
+      <img src={"/logo.png"} alt="logo" className="h-[50%]" />
+      <div className="flex">
+        <div className="mr-4">
+          <Link to="/">
+            <Button className="mr-2 border-none bg-transparent">Vault</Button>
+          </Link>
+          <Link to="/options">
+            <Button className="mr-2 border-none bg-transparent">Options</Button>
+          </Link>
+          <Link to="/dashboard">
+            <Button className="mr-2 border-none bg-transparent">
+              Dashboard
+            </Button>
+          </Link>
+        </div>
+        {!provider ? (
+          <Button onClick={() => connectWallet?.()}>Connect</Button>
+        ) : (
+          <p>Connected</p>
+        )}
+      </div>
     </div>
   );
 };
