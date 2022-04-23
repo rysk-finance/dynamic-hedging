a2b31e4f13542065391a505be0a8dca868b3107b
Hari Chauhan <hari-c@hotmail.co.uk>
2022-04-23 12:03:57 +0200

    attempt using onboard

--- packages/front-end/src/components/Header.tsx
+++ packages/front-end/src/components/Header.tsx
@@ -1,14 +1,20 @@
 import { useConnectWallet } from "@web3-onboard/react";
 import React from "react";
+import { useWalletContext } from "../App";
 import { Button } from "./Button";
 
 export const Header: React.FC = () => {
-  const [{ wallet }, connect] = useConnectWallet();
+  const [{ wallet }] = useConnectWallet();
+  const { connectWallet, provider } = useWalletContext();
 
   return (
     <div className="fixed w-full h-16 t-0 flex items-center px-16 justify-between">
       <h3>Rysk</h3>
-      {!wallet && <Button onClick={() => connect({})}>Connect</Button>}
+      {!provider ? (
+        <Button onClick={() => connectWallet?.()}>Connect</Button>
+      ) : (
+        <p>Connected</p>
+      )}
     </div>
   );
 };
--- packages/front-end/src/components/VaultDepositWithdraw.tsx
+++ packages/front-end/src/components/VaultDepositWithdraw.tsx
@@ -1,20 +1,47 @@
-import React, { useState } from "react";
+import { contractsContextFactory, useEthersContext } from "eth-hooks/context";
+import React, { useEffect, useState } from "react";
 import { Button } from "./Button";
 import { TextInput } from "./TextInput";
+import addresses from "../contracts.json";
+import * as ethers from "ethers";
+import DummyVaultABI from "../artifacts/contracts/DummyVault.sol/DummyVault.json";
+import { DummyVault } from "../types/DummyVault";
+import { useWalletContext } from "../App";
 
 enum Mode {
   DEPOSIT,
   WITHDRAW,
 }
 
+const dummyVaultAddress = addresses.localhost.DummyVault;
+
 export const VaultDepositWithdraw = () => {
+  const { provider } = useWalletContext();
+
+  const [dummyVaultContract, setDummyVaultContract] =
+    useState<ethers.ethers.Contract | null>(null);
+
   const [mode, setMode] = useState<Mode>(Mode.DEPOSIT);
+
+  const test = async () => {
+    if (provider) {
+      const contract = new ethers.Contract(
+        dummyVaultAddress,
+        DummyVaultABI.abi,
+        provider
+      );
+      debugger;
+      const a = await contract.getBalance();
+      setDummyVaultContract(contract);
+    }
+  };
 
   return (
     <div className="flex-col items-center">
       <div className="w-full flex justify-between mb-8">
         <Button onClick={() => setMode(Mode.DEPOSIT)}>Deposit</Button>
         <Button onClick={() => setMode(Mode.WITHDRAW)}>Withdraw</Button>
+        <Button onClick={() => test()}>test</Button>
       </div>
       <TextInput placeholder="Amount" />
     </div>
