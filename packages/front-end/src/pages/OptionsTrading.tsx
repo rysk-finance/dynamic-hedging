54d4af2ed9300964dd1149a5e7238e08ea7e049c
Hari Chauhan <hari-c@hotmail.co.uk>
2022-04-24 15:04:38 +0200

    Build out UI of OptionsTrading page

--- packages/front-end/src/pages/OptionsTrading.tsx
+++ packages/front-end/src/pages/OptionsTrading.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { ETHPriceIndicator } from "../components/shared/ETHPriceIndicator";
 
 export const OptionsTrading = () => {
-  return <h1>Options trading</h1>;
+  return (
+    <>
+      <div className="col-start-1 col-end-17 mb-16 flex justify-between">
+        <h3>Trade Options</h3>
+        <div className="flex items-start">
+          <ETHPriceIndicator />
+        </div>
+      </div>
+      <div className="col-start-1 col-end-6 p-4">
+        <div className="bg-white border-black border-2"></div>
+      </div>
+      <div className="col-start-6 col-end-12 p-4">
+        <div className="bg-white border-black border-2"></div>
+      </div>
+      <div className="col-start-12 col-end-17 p-4">
+        <div className="bg-white border-black border-2"></div>
+      </div>
+    </>
+  );
 };
