574f3982c7a241180bc298a7b4aea2cb66a38a0c
Hari Chauhan <hari-c@hotmail.co.uk>
2022-04-24 17:50:02 +0200

    Add icon prop to text input component

--- packages/front-end/src/components/shared/TextInput.tsx
+++ packages/front-end/src/components/shared/TextInput.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 
-export const TextInput: React.FC<
-  React.DetailedHTMLProps<
-    React.InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
-  >
-> = (props) => {
+type TextInputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & { iconLeft?: React.ReactElement };
+
+export const TextInput: React.FC<TextInputProps> = ({ iconLeft, ...props }) => {
   return (
-    <input
-      type="text"
-      {...props}
-      className={`border-black border-2 w-full p-2 ${props.className ?? ""}`}
-    />
+    <div className="relative">
+      <input
+        type="text"
+        {...props}
+        className={`border-black border-2 w-full p-2 ${props.className ?? ""}`}
+      />
+      {iconLeft && (
+        <div className="h-full absolute left-0 top-0 w-fit">{iconLeft}</div>
+      )}
+    </div>
   );
 };
