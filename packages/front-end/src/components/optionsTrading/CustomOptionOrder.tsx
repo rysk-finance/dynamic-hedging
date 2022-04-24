3086c8d9fe2234063f37c6c90c12f9c6517b4fba
Hari Chauhan <hari-c@hotmail.co.uk>
2022-04-24 17:50:43 +0200

    Add custom strike price input to custom option order

--- packages/front-end/src/components/optionsTrading/CustomOptionOrder.tsx
+++ packages/front-end/src/components/optionsTrading/CustomOptionOrder.tsx
@@ -4,6 +4,8 @@ import { OptionsTradingActionType, OptionType } from "../../state/types";
 import { RadioButtonList } from "../shared/RadioButtonList";
 import { Option } from "../../types";
 import { ExpiryDatePicker } from "./ExpiryDatePicker";
+import { TextInput } from "../shared/TextInput";
+import { Button } from "../shared/Button";
 
 const optionTypeOptions: Option<OptionType>[] = [
   { label: "Calls", value: OptionType.CALL },
@@ -29,8 +31,22 @@ export const CustomOptionOrder: React.FC = () => {
           setSelected={setOptionType}
         />
       </div>
-      <div className="">
+      <div className="mb-4">
         <ExpiryDatePicker />
+      </div>
+      <div>
+        <h5 className="mb-2">Custom Strike</h5>
+        <div className="flex">
+          <TextInput
+            className="text-right"
+            iconLeft={
+              <div className="px-2 flex items-center h-full">
+                <p className="text-gray-600">$</p>
+              </div>
+            }
+          />
+          <Button className="ml-8 px-8">Add</Button>
+        </div>
       </div>
     </div>
   );
