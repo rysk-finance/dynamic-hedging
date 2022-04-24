c88de67b895278bdc040350a1afbecb5438363e9
Hari Chauhan <hari-c@hotmail.co.uk>
2022-04-24 18:23:57 +0200

    Update options trading page layout

--- packages/front-end/src/pages/OptionsTrading.tsx
+++ packages/front-end/src/pages/OptionsTrading.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { CustomOptionOrder } from "../components/optionsTrading/CustomOptionOrder";
+import { OptionsChart } from "../components/optionsTrading/OptionsChart";
 import { ETHPriceIndicator } from "../components/shared/ETHPriceIndicator";
 import { OptionsTradingProvider } from "../state/OptionsTradingContext";
 
@@ -12,16 +13,18 @@ export const OptionsTrading = () => {
           <ETHPriceIndicator />
         </div>
       </div>
-      <div className="col-start-1 col-end-6 p-4">
+      <div className="col-start-1 col-end-6 pr-4 py-4">
         <div className="bg-white border-black border-2 p-4">
           <CustomOptionOrder />
         </div>
       </div>
       <div className="col-start-6 col-end-12 p-4">
-        <div className="bg-white border-black border-2"></div>
+        <div className="bg-white border-black border-2 pt-4">
+          <OptionsChart />
+        </div>
       </div>
-      <div className="col-start-12 col-end-17 p-4">
-        <div className="bg-white border-black border-2"></div>
+      <div className="col-start-12 col-end-17 pl-4 pl-4 py-4">
+        <div className="bg-white border-black border-2 p-4"></div>
       </div>
     </OptionsTradingProvider>
   );
