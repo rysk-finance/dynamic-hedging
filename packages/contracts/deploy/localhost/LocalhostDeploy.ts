29b1bc464d13658b149ed3a8a4e3f5d03cf0f39d
ugolino <dan.ugolini@gmail.com>
2022-04-26 14:30:06 +0200

    init localhost deploy script

--- packages/contracts/deploy/localhost/LocalhostDeploy.ts
+++ packages/contracts/deploy/localhost/LocalhostDeploy.ts
@@ -1,13 +1,26 @@
-import { Signer } from "ethers"
+import { BigNumberish, Contract, Signer, utils } from "ethers"
 import fs from "fs"
-import { deployments, ethers, getNamedAccounts } from "hardhat"
+import { deployments, ethers, getNamedAccounts, network } from "hardhat"
 import { DeployFunction } from "hardhat-deploy/types"
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import path from "path"
-import { ADDRESS_BOOK, GAMMA_ORACLE_NEW } from "../../test/constants"
-import { AddressBook } from "../../types/AddressBook"
-import { Oracle } from "../../types/Oracle"
-import { scaleNum } from "../../utils/conversion-helper"
+import { ADDRESS_BOOK, GAMMA_CONTROLLER, GAMMA_ORACLE_NEW, MARGIN_POOL, OTOKEN_FACTORY } from "../../test/constants"
+import { toWei, scaleNum } from "../../utils/conversion-helper"
+import LiquidityPoolSol from "../../artifacts/contracts/LiquidityPool.sol/LiquidityPool.json"
+
+import {
+	USDC_ADDRESS,
+	WETH_ADDRESS,
+	CONTROLLER_OWNER
+} from "../../test/constants"
+import { deployOpyn } from "../../utils/opyn-deployer"
+import { OptionRegistry } from "../../types/OptionRegistry"
+import { PriceFeed } from "../../types/PriceFeed"
+import { Protocol } from "../../types/Protocol"
+import { Volatility } from "../../types/Volatility"
+import { MintableERC20 } from "../../types/MintableERC20"
+import { WETH } from "../../types/WETH"
+import { LiquidityPool } from "../../types/LiquidityPool"
 
 const chainId = 1
 
@@ -17,42 +30,10 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	const { deploy, execute, read, log } = deployments
 	const { deployer } = await getNamedAccounts()
 	const signers: Signer[] = await ethers.getSigners()
-	const [signer] = signers
 
 	// Deploy Opyn Contracts
-	const addressBook = (await ethers.getContractAt(
-		"contracts/packages/opyn/core/AddressBook.sol:AddressBook",
-		ADDRESS_BOOK[chainId]
-	)) as AddressBook
-
-	// get the oracle
-	const oracle = (await ethers.getContractAt(
-		"contracts/packages/opyn/core/Oracle.sol:Oracle",
-		GAMMA_ORACLE_NEW[chainId]
-	)) as Oracle
-
-	const BSLib = await deploy("BlackScholes", {
-		from: deployer,
-		log: true
-	})
-
-	const LiquidityPool = await deploy("LiquidityPool", {
-		from: deployer,
-		log: true,
-		libraries: {
-			BlackScholes: BSLib.address
-		},
-		args: []
-	})
-
-	let dummyVaultDeploy = await deploy("DummyVault", {
-		from: deployer,
-		log: true
-	})
-
 	const productSpotShockValue = scaleNum("0.6", 27)
 	const day = 60 * 60 * 24
-
 	const timeToExpiry = [day * 7, day * 14, day * 28, day * 42, day * 56]
 
 	const expiryToValue = [
@@ -63,23 +44,158 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 		scaleNum("0.4603", 27)
 	]
 
-	// let opynParams = await deployOpyn(signers, productSpotShockValue, timeToExpiry, expiryToValue)
+	let opynParams = await deployOpyn(signers, productSpotShockValue, timeToExpiry, expiryToValue)
 
-	// let liquidtyPoolDeploy = await deploy("LiquidityPool", {
-	// 	from: deployer,
-	// 	log: true,
-	// 	args: [
+	const opynController = opynParams.controller
+	const opynAddressBook = opynParams.addressBook
+	const	opynOracle = opynParams.oracle
+	const opynNewCalculator = opynParams.newCalculator
+	
+	const [sender] = signers
+	const signer = await ethers.getSigner(CONTROLLER_OWNER[chainId])
+	const senderAddress = await signers[0].getAddress()
 
-	// 	]
-	// })
+	// deploy libraries
+	const constantsFactory = await hre.ethers.getContractFactory("Constants")
+	const interactionsFactory = await hre.ethers.getContractFactory("OpynInteractions")
+	const constants = await constantsFactory.deploy()
+	const interactions = await interactionsFactory.deploy()
+	// deploy options registry
+	const optionRegistryFactory = await hre.ethers.getContractFactory("OptionRegistry", {
+		libraries: {
+			OpynInteractions: interactions.address
+		}
+	})
+
+	
+
+	const _optionRegistry = (await optionRegistryFactory.deploy(
+		USDC_ADDRESS[chainId],
+		OTOKEN_FACTORY[chainId],
+		GAMMA_CONTROLLER[chainId],
+		MARGIN_POOL[chainId],
+		senderAddress,
+		ADDRESS_BOOK[chainId]
+	)) as OptionRegistry
+
+	let optionRegistry = _optionRegistry
+
+
+	const priceFeedFactory = await ethers.getContractFactory("PriceFeed")
+	const _priceFeed = (await priceFeedFactory.deploy()) as PriceFeed
+	let priceFeed = _priceFeed
+
+	// deploy option protocol and link to registry/price feed
+	const protocolFactory = await ethers.getContractFactory("contracts/OptionsProtocol.sol:Protocol")
+	const	optionProtocol = (await protocolFactory.deploy(
+		optionRegistry.address,
+		priceFeed.address
+	)) as Protocol
+
+	// deploy liquidity pool
+	type int7 = [
+		BigNumberish,
+		BigNumberish,
+		BigNumberish,
+		BigNumberish,
+		BigNumberish,
+		BigNumberish,
+		BigNumberish
+	]
+	type number7 = [number, number, number, number, number, number, number]
+	const coefInts: number7 = [
+		1.42180236,
+		0,
+		-0.08626792,
+		0.07873822,
+		0.00650549,
+		0.02160918,
+		-0.1393287
+	]
+	//@ts-ignore
+	const coefs: int7 = coefInts.map(x => toWei(x.toString()))
+
+	const normDistFactory = await ethers.getContractFactory("NormalDist", {
+		libraries: {}
+	})
+	const normDist = await normDistFactory.deploy()
+	const volFactory = await ethers.getContractFactory("Volatility", {
+		libraries: {}
+	})
+	const volatility = (await volFactory.deploy()) as Volatility
+	const blackScholesFactory = await ethers.getContractFactory("BlackScholes", {
+		libraries: {
+			NormalDist: normDist.address
+		}
+	})
+	const blackScholesDeploy = await blackScholesFactory.deploy()
+
+	const liquidityPoolFactory = await ethers.getContractFactory("LiquidityPool", {
+		libraries: {
+			BlackScholes: blackScholesDeploy.address
+		}
+	})
+
+	let	usd = (await ethers.getContractAt(
+		"contracts/tokens/ERC20.sol:ERC20",
+		USDC_ADDRESS[chainId]
+	)) as MintableERC20
+
+	let	weth = (await ethers.getContractAt(
+			"contracts/interfaces/WETH.sol:WETH",
+			WETH_ADDRESS[chainId]
+		)) as WETH
+
+	const rfr: string = "0.03"
+	const minCallStrikePrice = utils.parseEther("500")
+	const maxCallStrikePrice = utils.parseEther("10000")
+	const minPutStrikePrice = utils.parseEther("500")
+	const maxPutStrikePrice = utils.parseEther("10000")
+	// one week in seconds
+	const minExpiry = 86400 * 7
+	// 365 days in seconds
+	const maxExpiry = 86400 * 365
+
+	const lp = (await liquidityPoolFactory.deploy(
+		optionProtocol.address,
+		usd.address,
+		weth.address,
+		usd.address,
+		toWei(rfr),
+		coefs,
+		coefs,
+		"ETH/USDC",
+		"EDP",
+		{
+			minCallStrikePrice,
+			maxCallStrikePrice,
+			minPutStrikePrice,
+			maxPutStrikePrice,
+			minExpiry: minExpiry,
+			maxExpiry: maxExpiry
+		},
+		//@ts-ignore
+		await signers[0].getAddress()
+	)) as LiquidityPool
+
+	const lpAddress = lp.address
+	let liquidityPool = new Contract(lpAddress, LiquidityPoolSol.abi, signers[0]) as LiquidityPool
+	optionRegistry.setLiquidityPool(liquidityPool.address)
 
 	// @ts-ignore
 	const contractAddresses = JSON.parse(fs.readFileSync(addressPath))
 
 	// @ts-ignore
-	contractAddresses["localhost"]["DummyVault"] = dummyVaultDeploy.address
-	// contractAddresses["localhost"]["LiquidityPool"] = liquidtyPoolDeploy.address
-
+	// contractAddresses["localhost"]["DummyVault"] = dummyVaultDeploy.address
+	contractAddresses["localhost"]["OpynController"] = opynController.address
+	contractAddresses["localhost"]["OpynAddressBook"] = opynAddressBook.address
+	contractAddresses["localhost"]["OpynOracle"] = opynOracle.address
+	contractAddresses["localhost"]["OpynNewCalculator"] = opynNewCalculator.address
+	contractAddresses["localhost"]["OpynOptionRegistry"] = _optionRegistry.address
+	contractAddresses["localhost"]["priceFeed"] = priceFeed.address
+	contractAddresses["localhost"]["optionProtocol"] = optionProtocol.address
+	contractAddresses["localhost"]["liquidityPool"] = liquidityPool.address
+	
 	fs.writeFileSync(addressPath, JSON.stringify(contractAddresses, null, 4))
 }
 
