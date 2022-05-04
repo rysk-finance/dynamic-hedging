d34c95d2de264a2846fe0d6ae2fce3aa6f84cb8f
Hari Chauhan <hari-c@hotmail.co.uk>
2022-05-04 14:02:07 +0100

    Add underline to header buttons when selected

--- packages/front-end/src/components/Header.tsx
+++ packages/front-end/src/components/Header.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useWalletContext } from "../App";
 import { AppPaths } from "../config/appPaths";
 import { Button } from "./shared/Button";
 
 export const Header: React.FC = () => {
   const { connectWallet, provider } = useWalletContext();
+  const { pathname } = useLocation();
 
   return (
     <div className="fixed w-full h-24 t-0 flex items-center px-16 justify-between border-b-2 border-black bg-bone">
@@ -13,17 +14,29 @@ export const Header: React.FC = () => {
       <div className="flex">
         <div className="mr-4">
           <Link to={AppPaths.VAULT}>
-            <button className="mr-2 border-none bg-transparent p-2">
+            <button
+              className={`mr-2 border-none bg-transparent p-2 underline-offset-2	${
+                pathname === AppPaths.VAULT ? "underline" : ""
+              }`}
+            >
               Vault
             </button>
           </Link>
           <Link to={AppPaths.TRADE}>
-            <button className="mr-2 border-none bg-transparent p-2">
+            <button
+              className={`mr-2 border-none bg-transparent p-2 underline-offset-2 ${
+                pathname === AppPaths.TRADE ? "underline" : ""
+              }`}
+            >
               Trade Options
             </button>
           </Link>
           <Link to={AppPaths.DASHBOARD}>
-            <button className="mr-2 border-none bg-transparent p-2">
+            <button
+              className={`mr-2 border-none bg-transparent p-2 underline-offset-2 ${
+                pathname === AppPaths.DASHBOARD ? "underline" : ""
+              }`}
+            >
               Dashboard
             </button>
           </Link>
