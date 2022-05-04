0aab860d5f3b04e40129d9ac319b7bc07b60b8cc
Hari Chauhan <hari-c@hotmail.co.uk>
2022-05-04 16:56:13 +0100

    Create Radio button slider component (sans animation for now)

---
+++ packages/front-end/src/components/shared/RadioButtonSlider.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Option } from "../../types";
+
+type RadioButtonSliderProps<T> = {
+  options: Option<T>[];
+  selected: T | null;
+  setSelected: (value: T) => void;
+  predicate?: (arg: T) => boolean;
+};
+
+type RadioButtonSliderType<T = any> = React.FC<RadioButtonSliderProps<T>>;
+
+export const RadioButtonSlider: RadioButtonSliderType = ({
+  options,
+  selected,
+  setSelected,
+  predicate,
+}) => {
+  return (
+    <div className={`flex w-full box-content relative`}>
+      {options.map((option) => {
+        const isSelected = predicate
+          ? predicate(option.value)
+          : option.value === selected;
+        return (
+          // TODO(HC): Can add some animation here to move slide the background between
+          // selected buttons.
+          <button
+            key={option.key}
+            className={`px-4 py-2 rounded-full border-2 ${
+              isSelected ? "bg-white border-black box-border my-[-2px]" : ""
+            }`}
+            onClick={() => setSelected(option.value)}
+          >
+            {option.label}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
