aab8e8e927a9b898b758008bffbfe6707871a908
thegeronimo <thegeronimo@protonmail.com>
2022-05-05 14:34:47 -0400

    add events util

---
+++ packages/contracts/utils/events.ts
@@ -0,0 +1,35 @@
+import { utils, Event, ContractReceipt } from "ethers"
+import LiquidityPoolSol from "../artifacts/contracts/LiquidityPool.sol/LiquidityPool.json"
+
+const FAILED = "failed"
+// Event names
+export const WRITE_OPTION = "WriteOption"
+
+type EventsMap = Record<string, any>
+const eventsMap: EventsMap = {
+	[WRITE_OPTION]: {
+		["abi"]: LiquidityPoolSol.abi
+	}
+}
+
+function decodeEvents(events: Event[] | undefined, eventName: string) {
+	const eventInfo = eventsMap[eventName]
+	let iface = new utils.Interface(eventInfo["abi"])
+	if (!events) return []
+	const mapped = events.map(x => {
+		try {
+			const decoded = iface.decodeEventLog(eventName, x.data, x.topics)
+			return decoded
+		} catch (e) {
+			return FAILED
+		}
+	})
+	const filtered = mapped.filter(x => x !== FAILED)
+	return filtered
+}
+
+export function getMatchingEvents(receipt: ContractReceipt, eventName: string) {
+	if (!receipt.events) return []
+	const events: Event[] = receipt.events
+	return decodeEvents(events, eventName)
+}
