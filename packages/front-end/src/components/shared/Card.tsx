f8071e65cba4e5985bc5652122d9df3c68a2559e
Hari Chauhan <hari-c@hotmail.co.uk>
2022-05-10 11:09:05 +0100

    Make <Card> SVG params props

--- packages/front-end/src/components/shared/Card.tsx
+++ packages/front-end/src/components/shared/Card.tsx
@@ -1,13 +1,30 @@
 import React, { useCallback, useState } from "react";
 
-const LINE_WIDTH = 2;
-const MAIN_BORDER_RADIUS = 12;
-const MINOR_BORDER_RADIUS = 5;
-const TAB_HEIGHT = 20;
-const TAB_WIDTH = 100;
-const TAB_SLOPE_WIDTH = 30;
+const DEFAULT_LINE_WIDTH = 2;
+const DEFAULT_MAIN_BORDER_RADIUS = 12;
+const DEFAULT_MINOR_BORDER_RADIUS = 5;
+const DEFAULT_TAB_HEIGHT = 20;
+const DEFAULT_TAB_WIDTH = 100;
+const DEFAULT_TAB_SLOPE_WIDTH = 30;
 
-export const Card: React.FC = ({ children }) => {
+type CardProps = {
+  lineWidth?: number;
+  mainBorderRadius?: number;
+  minorBorderRadius?: number;
+  tabHeight?: number;
+  tabWidth?: number;
+  tabSlopeWidth?: number;
+};
+
+export const Card: React.FC<CardProps> = ({
+  children,
+  lineWidth = DEFAULT_LINE_WIDTH,
+  mainBorderRadius = DEFAULT_MAIN_BORDER_RADIUS,
+  minorBorderRadius = DEFAULT_MINOR_BORDER_RADIUS,
+  tabHeight = DEFAULT_TAB_HEIGHT,
+  tabWidth = DEFAULT_TAB_WIDTH,
+  tabSlopeWidth = DEFAULT_TAB_SLOPE_WIDTH,
+}) => {
   const [rect, setRect] = useState<DOMRect | null>(null);
 
   const getSVGRect = useCallback((element: SVGSVGElement) => {
@@ -32,37 +49,35 @@ export const Card: React.FC = ({ children }) => {
           <svg
             width="100%"
             height="100%"
-            strokeWidth={`${LINE_WIDTH}px`}
+            strokeWidth={`${lineWidth}px`}
             ref={svgRef}
           >
             {rect && (
               <path
-                d={`m ${MINOR_BORDER_RADIUS} ${LINE_WIDTH / 2}
-              L ${TAB_WIDTH} ${LINE_WIDTH / 2}
+                d={`m ${minorBorderRadius} ${lineWidth / 2}
+              L ${tabWidth} ${lineWidth / 2}
 
-              C ${TAB_WIDTH + MINOR_BORDER_RADIUS * 2} ${LINE_WIDTH / 2}, ${
-                  TAB_WIDTH + TAB_SLOPE_WIDTH - MINOR_BORDER_RADIUS * 2
-                } ${TAB_HEIGHT}, ${TAB_WIDTH + TAB_SLOPE_WIDTH} ${TAB_HEIGHT}
+              C ${tabWidth + minorBorderRadius * 2} ${lineWidth / 2}, ${
+                  tabWidth + tabSlopeWidth - minorBorderRadius * 2
+                } ${tabHeight}, ${tabWidth + tabSlopeWidth} ${tabHeight}
               
-              L ${
-                rect.width - MAIN_BORDER_RADIUS - LINE_WIDTH / 2
-              } ${TAB_HEIGHT}
-              A ${MAIN_BORDER_RADIUS} ${MAIN_BORDER_RADIUS} 1 0 1 ${
-                  rect.width - LINE_WIDTH / 2
-                } ${TAB_HEIGHT + MAIN_BORDER_RADIUS}
-              L ${rect.width - LINE_WIDTH / 2} ${
-                  rect.height - MAIN_BORDER_RADIUS - LINE_WIDTH / 2
+              L ${rect.width - mainBorderRadius - lineWidth / 2} ${tabHeight}
+              A ${mainBorderRadius} ${mainBorderRadius} 1 0 1 ${
+                  rect.width - lineWidth / 2
+                } ${tabHeight + mainBorderRadius}
+              L ${rect.width - lineWidth / 2} ${
+                  rect.height - mainBorderRadius - lineWidth / 2
                 }
-              A ${MAIN_BORDER_RADIUS} ${MAIN_BORDER_RADIUS} 1 0 1 ${
-                  rect.width - MAIN_BORDER_RADIUS - LINE_WIDTH / 2
-                } ${rect.height - LINE_WIDTH / 2}
-              L ${MAIN_BORDER_RADIUS} ${rect.height - LINE_WIDTH / 2}
-              A ${MAIN_BORDER_RADIUS} ${MAIN_BORDER_RADIUS} 1 0 1 ${
-                  LINE_WIDTH / 2
-                } ${rect.height - MAIN_BORDER_RADIUS}
-              L ${LINE_WIDTH / 2} ${MINOR_BORDER_RADIUS}
-              A ${MINOR_BORDER_RADIUS} ${MINOR_BORDER_RADIUS} 1 0 1 ${MINOR_BORDER_RADIUS} ${
-                  LINE_WIDTH / 2
+              A ${mainBorderRadius} ${mainBorderRadius} 1 0 1 ${
+                  rect.width - mainBorderRadius - lineWidth / 2
+                } ${rect.height - lineWidth / 2}
+              L ${mainBorderRadius} ${rect.height - lineWidth / 2}
+              A ${mainBorderRadius} ${mainBorderRadius} 1 0 1 ${
+                  lineWidth / 2
+                } ${rect.height - mainBorderRadius}
+              L ${lineWidth / 2} ${minorBorderRadius}
+              A ${minorBorderRadius} ${minorBorderRadius} 1 0 1 ${minorBorderRadius} ${
+                  lineWidth / 2
                 }`}
                 fill="transparent"
                 stroke="black"
@@ -74,8 +89,8 @@ export const Card: React.FC = ({ children }) => {
       <div
         className="w-full"
         style={{
-          paddingTop: TAB_HEIGHT,
-          height: `calc(100% - ${TAB_HEIGHT}px)`,
+          paddingTop: tabHeight,
+          height: `calc(100% - ${tabHeight}px)`,
         }}
       >
         {children}
