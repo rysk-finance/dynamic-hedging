ef5d164eb9b173d2ce905c3cf28696f1b251d58e
Josh <55187183+JoshDAO@users.noreply.github.com>
2022-05-11 14:28:07 +0100

    move quotePriceGreeks to optionsCompute

--- packages/contracts/contracts/libraries/OptionsCompute.sol
+++ packages/contracts/contracts/libraries/OptionsCompute.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+import "./BlackScholes.sol";
 import "prb-math/contracts/PRBMathUD60x18.sol";
 import "prb-math/contracts/PRBMathSD59x18.sol";
 import "./Types.sol";
@@ -64,7 +65,7 @@ library OptionsCompute {
 		Types.PortfolioValues memory portfolioValues,
 		uint256 maxTimeDeviationThreshold,
 		uint256 maxPriceDeviationThreshold
-	) public view {
+	) internal view {
 		uint256 timeDelta = block.timestamp - portfolioValues.timestamp;
 		// If too much time has passed we want to prevent a possible oracle attack
 		if (timeDelta > maxTimeDeviationThreshold) {
@@ -75,5 +76,93 @@ library OptionsCompute {
 		if (priceDelta > maxPriceDeviationThreshold) {
 			revert CustomErrors.PriceDeltaExceedsThreshold(priceDelta);
 		}
+	}
+
+	function getUtilizationPrice(
+		uint256 _utilizationBefore,
+		uint256 _utilizationAfter,
+		uint256 _totalOptionPrice,
+		uint256 _utilizationFunctionThreshold,
+		uint256 _belowThresholdGradient,
+		uint256 _aboveThresholdGradient,
+		uint256 _aboveThresholdYIntercept
+	) internal pure returns (uint256 utilizationPrice) {
+		if (
+			_utilizationBefore <= _utilizationFunctionThreshold &&
+			_utilizationAfter <= _utilizationFunctionThreshold
+		) {
+			// linear function up to 50% utilization
+			// adds 10% of utilization percentage on to price
+			// eg at 50% utilization, options will be priced 5% more expensive
+			return
+				_totalOptionPrice +
+				_totalOptionPrice.mul(_utilizationBefore + _utilizationAfter).mul(_belowThresholdGradient).div(
+					2e18
+				);
+		} else if (
+			_utilizationBefore >= _utilizationFunctionThreshold &&
+			_utilizationAfter >= _utilizationFunctionThreshold
+		) {
+			// over 50% utilization the skew factor will follow a steeper line
+
+			uint256 multiplicationFactor = _aboveThresholdGradient
+				.mul(_utilizationBefore + _utilizationAfter)
+				.div(2e18) - _aboveThresholdYIntercept;
+
+			return _totalOptionPrice + _totalOptionPrice.mul(uint256(multiplicationFactor));
+		} else {
+			// _utilizationAfter will always be greater than _utilizationBefore
+			// finds the ratio of the distance below the threshold to the distance above the threshold
+			uint256 weightingRatio = (_utilizationFunctionThreshold - _utilizationBefore).div(
+				_utilizationAfter - _utilizationFunctionThreshold
+			);
+			// finds the average y value on the part of the function below threshold
+			uint256 averageFactorBelow = (_utilizationFunctionThreshold + _utilizationBefore).div(2e18).mul(
+				_belowThresholdGradient
+			);
+			// finds average y value on part of the function above threshold
+			uint256 averageFactorAbove = (_utilizationAfter + _utilizationFunctionThreshold).div(2e18).mul(
+				_aboveThresholdGradient
+			) - _aboveThresholdYIntercept;
+			// finds the weighted average of the two above averaged to find the average utilization skew over the range of utilization
+			uint256 multiplicationFactor = (weightingRatio.mul(averageFactorBelow) + averageFactorAbove).div(
+				1e18 + weightingRatio
+			);
+			return _totalOptionPrice + _totalOptionPrice.mul(multiplicationFactor);
+		}
+	}
+
+	/**
+	 * @notice get the greeks of a quotePrice for a given optionSeries
+	 * @param  optionSeries Types.OptionSeries struct for describing the option to price greeks - strike in e18
+	 * @return quote           Quote price of the option - in e18
+	 * @return delta           delta of the option being priced - in e18
+	 */
+	function quotePriceGreeks(
+		Types.OptionSeries memory optionSeries,
+		bool isBuying,
+		uint256 bidAskIVSpread,
+		uint256 riskFreeRate,
+		uint256 iv,
+		uint256 underlyingPrice
+	) internal view returns (uint256 quote, int256 delta) {
+		if (iv == 0) {
+			revert CustomErrors.IVNotFound();
+		}
+		if (isBuying) {
+			iv = (iv * (1e18 - (bidAskIVSpread))) / 1e18;
+		}
+		// revert CustomErrors.if the expiry is in the past
+		if (optionSeries.expiration <= block.timestamp) {
+			revert CustomErrors.OptionExpiryInvalid();
+		}
+		(quote, delta) = BlackScholes.blackScholesCalcGreeks(
+			underlyingPrice,
+			optionSeries.strike,
+			optionSeries.expiration,
+			iv,
+			riskFreeRate,
+			optionSeries.isPut
+		);
 	}
 }
---
+++ packages/contracts/test/LiquidityPoolIntegration.ts
@@ -0,0 +1,282 @@
+import hre, { ethers, network } from "hardhat"
+import { BigNumberish, Contract, utils, Signer, BigNumber } from "ethers"
+import {
+	toWei,
+	truncate,
+	tFormatEth,
+	call,
+	put,
+	genOptionTimeFromUnix,
+	fromWei,
+	percentDiff,
+	toUSDC,
+	fmtExpiration,
+	fromOpyn,
+	toOpyn,
+	tFormatUSDC,
+	PUT,
+	fromUSDC,
+	scaleNum
+} from "../utils/conversion-helper"
+import { deployMockContract, MockContract } from "@ethereum-waffle/mock-contract"
+import moment from "moment"
+import AggregatorV3Interface from "../artifacts/contracts/interfaces/AggregatorV3Interface.sol/AggregatorV3Interface.json"
+//@ts-ignore
+import bs from "black-scholes"
+import { expect } from "chai"
+import Otoken from "../artifacts/contracts/packages/opyn/core/Otoken.sol/Otoken.json"
+import LiquidityPoolSol from "../artifacts/contracts/LiquidityPool.sol/LiquidityPool.json"
+import { MockPortfolioValuesFeed } from "../types/MockPortfolioValuesFeed"
+import { MintableERC20 } from "../types/MintableERC20"
+import { OptionRegistry } from "../types/OptionRegistry"
+import { Otoken as IOToken } from "../types/Otoken"
+import { PriceFeed } from "../types/PriceFeed"
+import { LiquidityPool } from "../types/LiquidityPool"
+import { WETH } from "../types/WETH"
+import { Protocol } from "../types/Protocol"
+import { NewController } from "../types/NewController"
+import { AddressBook } from "../types/AddressBook"
+import { NewMarginCalculator } from "../types/NewMarginCalculator"
+import { Volatility } from "../types/Volatility"
+import { Oracle } from "../types/Oracle"
+import { setupTestOracle } from "./helpers"
+import {
+	GAMMA_CONTROLLER,
+	MARGIN_POOL,
+	OTOKEN_FACTORY,
+	USDC_ADDRESS,
+	USDC_OWNER_ADDRESS,
+	WETH_ADDRESS,
+	ADDRESS_BOOK
+} from "./constants"
+import { MockChainlinkAggregator } from "../types/MockChainlinkAggregator"
+import { deployOpyn } from "../utils/opyn-deployer"
+import { VolatilityFeed } from "../types/VolatilityFeed"
+import { UniswapV3HedgingReactor } from "../types/UniswapV3HedgingReactor"
+import { deployLiquidityPool, deploySystem } from "../utils/generic-system-deployer"
+import { OptionHandler } from "../types/OptionHandler"
+let usd: MintableERC20
+let weth: WETH
+let optionRegistry: OptionRegistry
+let optionProtocol: Protocol
+let signers: Signer[]
+let senderAddress: string
+let receiverAddress: string
+let liquidityPool: LiquidityPool
+let ethLiquidityPool: LiquidityPool
+let volFeed: VolatilityFeed
+let priceFeed: PriceFeed
+let volatility: Volatility
+let portfolioValuesFeed: MockPortfolioValuesFeed
+let proposedSeries: any
+let ethUSDAggregator: MockContract
+let uniswapV3HedgingReactor: UniswapV3HedgingReactor
+let rate: string
+let controller: NewController
+let addressBook: AddressBook
+let newCalculator: NewMarginCalculator
+let oracle: Oracle
+let opynAggregator: MockChainlinkAggregator
+let handler: OptionHandler
+
+const IMPLIED_VOL = "60"
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
+const ETH_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+
+/* --- variables to change --- */
+
+// Date for option to expire on format yyyy-mm-dd
+// Will automatically convert to 08:00 UTC timestamp
+// First mined block will be timestamped 2022-02-27 19:05 UTC
+const expiryDate: string = "2022-04-05"
+
+const invalidExpiryDateLong: string = "2024-09-03"
+const invalidExpiryDateShort: string = "2022-03-01"
+// decimal representation of a percentage
+const rfr: string = "0.03"
+// edit depending on the chain id to be tested on
+const chainId = 1
+const oTokenDecimalShift18 = 10000000000
+// amount of dollars OTM written options will be (both puts and calls)
+// use negative numbers for ITM options
+const strike = "20"
+
+// hardcoded value for strike price that is outside of accepted bounds
+const invalidStrikeHigh = utils.parseEther("12500")
+const invalidStrikeLow = utils.parseEther("200")
+
+// balances to deposit into the LP
+const liquidityPoolUsdcDeposit = "10000"
+const liquidityPoolWethDeposit = "1"
+
+// balance to withdraw after deposit
+const liquidityPoolWethWidthdraw = "0.1"
+
+const minCallStrikePrice = utils.parseEther("500")
+const maxCallStrikePrice = utils.parseEther("10000")
+const minPutStrikePrice = utils.parseEther("500")
+const maxPutStrikePrice = utils.parseEther("10000")
+// one week in seconds
+const minExpiry = 86400 * 7
+// 365 days in seconds
+const maxExpiry = 86400 * 365
+
+// time travel period between each expiry
+const expiryPeriod = {
+	days: 0,
+	weeks: 0,
+	months: 1,
+	years: 0
+}
+const productSpotShockValue = scaleNum("0.6", 27)
+// array of time to expiry
+const day = 60 * 60 * 24
+const timeToExpiry = [day * 7, day * 14, day * 28, day * 42, day * 56]
+// array of upper bound value correspond to time to expiry
+const expiryToValue = [
+	scaleNum("0.1678", 27),
+	scaleNum("0.237", 27),
+	scaleNum("0.3326", 27),
+	scaleNum("0.4032", 27),
+	scaleNum("0.4603", 27)
+]
+
+/* --- end variables to change --- */
+
+const expiration = moment.utc(expiryDate).add(8, "h").valueOf() / 1000
+const invalidExpirationLong = moment.utc(invalidExpiryDateLong).add(8, "h").valueOf() / 1000
+const invalidExpirationShort = moment.utc(invalidExpiryDateShort).add(8, "h").valueOf() / 1000
+
+const CALL_FLAVOR = false
+const PUT_FLAVOR = true
+describe("Liquidity Pool Integration Simulation", async () => {
+	before(async function () {
+		await hre.network.provider.request({
+			method: "hardhat_reset",
+			params: [
+				{
+					forking: {
+						chainId: 1,
+						jsonRpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY}`,
+						blockNumber: 14290000
+					}
+				}
+			]
+		})
+		signers = await ethers.getSigners()
+		let opynParams = await deployOpyn(signers, productSpotShockValue, timeToExpiry, expiryToValue)
+		controller = opynParams.controller
+		addressBook = opynParams.addressBook
+		oracle = opynParams.oracle
+		newCalculator = opynParams.newCalculator
+		// get the oracle
+		const res = await setupTestOracle(await signers[0].getAddress())
+		oracle = res[0] as Oracle
+		opynAggregator = res[1] as MockChainlinkAggregator
+		let deployParams = await deploySystem(signers, oracle, opynAggregator)
+		weth = deployParams.weth
+		const wethERC20 = deployParams.wethERC20
+		usd = deployParams.usd
+		optionRegistry = deployParams.optionRegistry
+		priceFeed = deployParams.priceFeed
+		volFeed = deployParams.volFeed
+		portfolioValuesFeed = deployParams.portfolioValuesFeed
+		optionProtocol = deployParams.optionProtocol
+		let lpParams = await deployLiquidityPool(
+			signers,
+			optionProtocol,
+			usd,
+			wethERC20,
+			rfr,
+			minCallStrikePrice,
+			minPutStrikePrice,
+			maxCallStrikePrice,
+			maxPutStrikePrice,
+			minExpiry,
+			maxExpiry,
+			optionRegistry,
+			portfolioValuesFeed
+		)
+		volatility = lpParams.volatility
+		liquidityPool = lpParams.liquidityPool
+		handler = lpParams.handler
+		signers = await hre.ethers.getSigners()
+		senderAddress = await signers[0].getAddress()
+		receiverAddress = await signers[1].getAddress()
+	})
+	it("Deposit to the liquidityPool", async () => {
+		const USDC_WHALE = "0x55fe002aeff02f77364de339a1292923a15844b8"
+		await hre.network.provider.request({
+			method: "hardhat_impersonateAccount",
+			params: [USDC_WHALE]
+		})
+		const usdcWhale = await ethers.getSigner(USDC_WHALE)
+		const usdWhaleConnect = await usd.connect(usdcWhale)
+		await weth.deposit({ value: toWei(liquidityPoolWethDeposit) })
+		await usdWhaleConnect.transfer(senderAddress, toUSDC("1000000"))
+		await usdWhaleConnect.transfer(receiverAddress, toUSDC("1000000"))
+		const balance = await usd.balanceOf(senderAddress)
+		await usd.approve(liquidityPool.address, toUSDC(liquidityPoolUsdcDeposit))
+		const deposit = await liquidityPool.deposit(toUSDC(liquidityPoolUsdcDeposit), senderAddress)
+		const liquidityPoolBalance = await liquidityPool.balanceOf(senderAddress)
+		const receipt = await deposit.wait(1)
+		const event = receipt?.events?.find(x => x.event == "Deposit")
+		const newBalance = await usd.balanceOf(senderAddress)
+		expect(event?.event).to.eq("Deposit")
+		expect(balance.sub(newBalance)).to.eq(toUSDC(liquidityPoolUsdcDeposit))
+		expect(liquidityPoolBalance.toString()).to.eq(toWei(liquidityPoolUsdcDeposit))
+	})
+	it("Returns a quote for a ETH/USD put with utilization", async () => {
+		const totalLiqidity = await liquidityPool.totalSupply()
+		const amount = toWei("1")
+		const blockNum = await ethers.provider.getBlockNumber()
+		const block = await ethers.provider.getBlock(blockNum)
+		const { timestamp } = block
+		const timeToExpiration = genOptionTimeFromUnix(Number(timestamp), expiration)
+		const priceQuote = await priceFeed.getNormalizedRate(weth.address, usd.address)
+		const strikePrice = priceQuote.sub(toWei(strike))
+		const priceNorm = fromWei(priceQuote)
+		const utilization = Number(fromWei(amount)) / Number(fromWei(totalLiqidity))
+		const utilizationPrice = Number(priceNorm) * utilization
+		const optionSeries = {
+			expiration: expiration,
+			strike: strikePrice,
+			isPut: PUT_FLAVOR,
+			strikeAsset: usd.address,
+			underlying: weth.address,
+			collateral: usd.address
+		}
+		const iv = await liquidityPool.getImpliedVolatility(
+			optionSeries.isPut,
+			priceQuote,
+			optionSeries.strike,
+			optionSeries.expiration
+		)
+		const localBS = bs.blackScholes(
+			priceNorm,
+			fromWei(strikePrice),
+			timeToExpiration,
+			fromWei(iv),
+			parseFloat(rfr),
+			"put"
+		)
+		const finalQuote = utilizationPrice > localBS ? utilizationPrice : localBS
+		const quote = (
+			await liquidityPool.quotePriceWithUtilizationGreeks(
+				{
+					expiration: expiration,
+					strike: BigNumber.from(strikePrice),
+					isPut: PUT_FLAVOR,
+					strikeAsset: usd.address,
+					underlying: weth.address,
+					collateral: usd.address
+				},
+				amount
+			)
+		)[0]
+		const truncQuote = truncate(finalQuote)
+		const chainQuote = tFormatEth(quote.toString())
+		const diff = percentDiff(truncQuote, chainQuote)
+		expect(diff).to.be.within(0, 0.01)
+	})
+})
