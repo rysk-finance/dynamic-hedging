456ce4ee9ca9e51360113c5b81a000aec0df5302
Hari Chauhan <hari-c@hotmail.co.uk>
2022-05-12 14:30:42 +0100

    Make popover close on click-off

--- packages/front-end/src/components/HeaderPopover.tsx
+++ packages/front-end/src/components/HeaderPopover.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { toast } from "react-toastify";
 import { useWalletContext } from "../App";
 import { Settings } from "./dashboard/Settings";
@@ -6,11 +6,35 @@ import { Button } from "./shared/Button";
 
 export const HeaderPopover: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const isOpenRef = useRef(false);
+  const containerRef = useRef<HTMLDivElement | null>(null);
 
   const { account, disconnect } = useWalletContext();
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      isOpenRef.current &&
+      containerRef.current &&
+      !containerRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    isOpenRef.current = isOpen;
+  }, [isOpen]);
+
+  useEffect(() => {
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="flex items-center">
+    <div className="flex items-center" ref={containerRef}>
       <Button type="button" onClick={() => setIsOpen((old) => !old)}>
         {`${account?.slice(0, 4)}...${account?.slice(
           account.length - 4,
