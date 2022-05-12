9c1724ee70283215aaf556a0d7f97958ec88ff58
Hari Chauhan <hari-c@hotmail.co.uk>
2022-05-12 14:10:46 +0100

    Add contents to header popover

--- packages/front-end/src/components/HeaderPopover.tsx
+++ packages/front-end/src/components/HeaderPopover.tsx
@@ -1,19 +1,52 @@
 import React, { useState } from "react";
+import { toast } from "react-toastify";
+import { useWalletContext } from "../App";
+import { Button } from "./shared/Button";
 
 export const HeaderPopover: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { account, disconnect } = useWalletContext();
 
   return (
     <div className="flex items-center">
-      <button
-        type="button"
-        onClick={() => setIsOpen((old) => !old)}
-        className="rounded-full flex items-center justify-center w-10 h-10 border-2 border-black bg-white"
-      ></button>
+      <Button type="button" onClick={() => setIsOpen((old) => !old)}>
+        {`${account?.slice(0, 4)}...${account?.slice(
+          account.length - 4,
+          account.length
+        )}`}{" "}
+        {isOpen ? "▼" : "▲"}
+      </Button>
 
       {isOpen && (
-        <div className="fixed top-[72px] bg-bone border-2 border-black right-16 p-8">
-          Popper element
+        <div className="fixed top-[72px] bg-bone border-2 border-black right-16 p-2">
+          <div className="flex flex-col">
+            <Button
+              onClick={() => {
+                account && navigator.clipboard.writeText(account);
+                toast("Address coppied to clipboard", { autoClose: 1000 });
+              }}
+              className="mb-[-2px] bg-bone"
+            >
+              Copy address
+            </Button>
+            <Button
+              onClick={() => {
+                account &&
+                  window.open(`https://etherscan.io/address/${account}`);
+              }}
+              className="mb-4"
+            >
+              Open in Etherscan
+            </Button>
+            <Button
+              onClick={() => {
+                disconnect?.();
+              }}
+            >
+              Disconnect
+            </Button>
+          </div>
         </div>
       )}
     </div>
