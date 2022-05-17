ef94d3efeea769d8a873c54fc6d5db5c50e9d15b
Josh <55187183+JoshDAO@users.noreply.github.com>
2022-05-17 16:51:39 +0200

    lint

--- packages/contracts/contracts/interfaces/ILiquidityPool.sol
+++ packages/contracts/contracts/interfaces/ILiquidityPool.sol
@@ -3,37 +3,55 @@
 pragma solidity >=0.8.9;
 import { Types } from "../libraries/Types.sol";
 import "../interfaces/IOptionRegistry.sol";
-interface ILiquidityPool {
-  function deposit(uint _amount, address _recipient) external returns (uint256);
-  function withdraw(uint _shares, address _recipient) external returns (uint256);
-  function collateralAsset() external view returns (address);
-  function handlerIssue(Types.OptionSeries memory optionSeries) external returns (address);
-  function resetEphemeralValues() external;
-  function handlerWriteOption(
-    Types.OptionSeries memory optionSeries, 
-    address seriesAddress, 
-    uint256 amount, 
-    IOptionRegistry optionRegistry, 
-    uint256 premium,
-    int256 delta,
-    address recipient
-  ) external returns (uint256);
-  function handlerBuybackOption(Types.OptionSeries memory optionSeries, uint256 amount, IOptionRegistry optionRegistry, address seriesAddress, uint256 premium, int256 delta, address seller) external returns (uint256);
-  function handlerIssueAndWriteOption(Types.OptionSeries memory optionSeries, uint256 amount, uint256 premium, int256 delta, address recipient) external returns (uint256, address);
-  function getPortfolioDelta() external view returns (int256);
-  function quotePriceWithUtilizationGreeks(
-    Types.OptionSeries memory optionSeries,
-    uint amount
-  )
-    external
-    view
-    returns (uint256 quote, int256 delta);
-  function quotePriceBuying(
-      Types.OptionSeries memory optionSeries,
-      uint amount
-  ) 
-  external
-  view
-  returns (uint256, int256);
 
+interface ILiquidityPool {
+	function deposit(uint256 _amount, address _recipient) external returns (uint256);
+
+	function withdraw(uint256 _shares, address _recipient) external returns (uint256);
+
+	function collateralAsset() external view returns (address);
+
+	function handlerIssue(Types.OptionSeries memory optionSeries) external returns (address);
+
+	function resetEphemeralValues() external;
+
+	function handlerWriteOption(
+		Types.OptionSeries memory optionSeries,
+		address seriesAddress,
+		uint256 amount,
+		IOptionRegistry optionRegistry,
+		uint256 premium,
+		int256 delta,
+		address recipient
+	) external returns (uint256);
+
+	function handlerBuybackOption(
+		Types.OptionSeries memory optionSeries,
+		uint256 amount,
+		IOptionRegistry optionRegistry,
+		address seriesAddress,
+		uint256 premium,
+		int256 delta,
+		address seller
+	) external returns (uint256);
+
+	function handlerIssueAndWriteOption(
+		Types.OptionSeries memory optionSeries,
+		uint256 amount,
+		uint256 premium,
+		int256 delta,
+		address recipient
+	) external returns (uint256, address);
+
+	function getPortfolioDelta() external view returns (int256);
+
+	function quotePriceWithUtilizationGreeks(Types.OptionSeries memory optionSeries, uint256 amount)
+		external
+		view
+		returns (uint256 quote, int256 delta);
+
+	function quotePriceBuying(Types.OptionSeries memory optionSeries, uint256 amount)
+		external
+		view
+		returns (uint256, int256);
 }
