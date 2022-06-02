4255058ce4d13a079e1fc003f43e236f35df18d3
Jake Mc <3658522+startswithaj@users.noreply.github.com>
2022-06-03 00:00:37 +1000

    feat(integration-tests): attempt to run a single test (#138)
    * feat(intyegration-tests): attempt to run a single test
    
    * feat(integration-tests): move to workflows
    
    * feat(integration-tests): fix yaml
    
    * feat(integration-tests): work around hard import
    
    * feat(integration-tests): disable executeEpoch which has circular references
    
    * feat(integration-tests): enable all tests
    
    * feat(integration-tests): tidy
    
    * fix: ci circular issue and rounding
    
    * fix: move execute epoch to scripts
    
    * fix: rounding
    
    Co-authored-by: kjr217 <kjraja217@gmail.com>

--- packages/contracts/scripts/executeEpoch.ts
+++ packages/contracts/scripts/executeEpoch.ts
@@ -1,10 +1,12 @@
 import hre from "hardhat"
-import { task } from "hardhat/config"
 import "@nomiclabs/hardhat-ethers"
+// This is a circular reference
+// Hardhat cannot compile because it relies on this file which is the output of a compile
 import { abi } from "../artifacts/contracts/LiquidityPool.sol/LiquidityPool.json"
+// This file doesn't exist in CI, only exists locally (in git ignore)
 import { localhost } from "../contracts.json"
 
-task("executeEpoch", "Executes the current epoch").setAction(async (_, hre) => {
+async function main() {
 	try {
 		await hre.network.provider.request({
 			method: "hardhat_impersonateAccount",
@@ -26,4 +28,11 @@ task("executeEpoch", "Executes the current epoch").setAction(async (_, hre) => {
 	} catch (err) {
 		console.log(err)
 	}
-})
+}
+
+main()
+	.then(() => process.exit())
+	.catch((error) => {
+		console.error(error);
+		process.exit(1)
+	})
