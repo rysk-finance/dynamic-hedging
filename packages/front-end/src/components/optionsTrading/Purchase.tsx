125d4754eb8e7d1f3ca4ad6319b847c65fd05437
Hari Chauhan <hari-c@hotmail.co.uk>
2022-06-07 15:42:12 +0100

    Add UI for purchase window

--- packages/front-end/src/components/optionsTrading/Purchase.tsx
+++ packages/front-end/src/components/optionsTrading/Purchase.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useOptionsTradingContext } from "../../state/OptionsTradingContext";
 import { ReturnLineChart } from "./ReturnLineChart";
 import { Serie } from "@nivo/line";
 import { OptionsTradingActionType, Option } from "../../state/types";
+import { Button } from "../shared/Button";
+import { TextInput } from "../shared/TextInput";
 
 // TODO(HC): Make this not dummy data...
 const DUMMY_DATA: Serie[] = [
@@ -25,28 +27,71 @@ export const Purchase: React.FC = () => {
     dispatch({ type: OptionsTradingActionType.SET_SELECTED_OPTION, option });
   };
 
+  const [uiOrderSize, setUIOrderSize] = useState("");
+
+  const buyIsDisabled = !uiOrderSize;
+
   return (
-    <div className="p-4">
+    <div>
       {selectedOption ? (
         <>
-          <div className="w-full flex justify-end">
-            <button className="text-xl" onClick={() => setSelectedOption(null)}>
+          <div className="w-full flex justify-between relative">
+            <button
+              className="absolute text-xl top-4 right-4"
+              onClick={() => setSelectedOption(null)}
+            >
               ✕
             </button>
-          </div>
-          <div className="mb-16">
-            <h4>Buy: {selectedOption.type}</h4>
-            <p>Strike: {selectedOption.strike}</p>
-            <p>IV: {selectedOption.IV}</p>
-            <p>Delta: {selectedOption.delta}</p>
-            <p>Price: {selectedOption.price}</p>
-          </div>
-          <div className="h-32 w-full border-black border-2">
-            <ReturnLineChart data={DUMMY_DATA} />
+            <div className="w-1/2 border-r-2 border-black">
+              <div className="w-full p-4">
+                <div className="flex items-center">
+                  <h4 className="font-parabole mr-2 pb-2">Option:</h4>
+                  {selectedOption && (
+                    <p className="pb-1">{selectedOption.type}</p>
+                  )}
+                </div>
+                <p>Strike: {selectedOption.strike}</p>
+                <p>IV: {selectedOption.IV}</p>
+                <p>Delta: {selectedOption.delta}</p>
+                <p>Price: {selectedOption.price}</p>
+              </div>
+              <div className="w-full ">
+                <TextInput
+                  value={uiOrderSize}
+                  setValue={setUIOrderSize}
+                  className="text-right border-x-0 w-full"
+                  iconLeft={
+                    <div className="px-2 flex items-center h-full">
+                      <p className="text-gray-600">Shares</p>
+                    </div>
+                  }
+                  numericOnly
+                  maxNumDecimals={2}
+                />
+              </div>
+              <div className="w-full">
+                <div className="w-full -mb-1">
+                  <div className="w-full p-4 flex items-center">
+                    <h4 className="font-parabole mr-2 pb-1">Total price:</h4>
+                    {uiOrderSize && (
+                      <p>{Number(uiOrderSize) * selectedOption.price} USDC</p>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <Button
+                disabled={buyIsDisabled}
+                className={`w-full border-b-0 border-x-0 !py-4 !bg-black text-white ${
+                  buyIsDisabled ? "!bg-gray-300" : ""
+                }`}
+              >
+                Buy
+              </Button>
+            </div>
           </div>
         </>
       ) : (
-        <p>Select an option first</p>
+        <p className="p-4">Select an option first</p>
       )}
     </div>
   );
