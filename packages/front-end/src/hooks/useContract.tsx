2571d47bc4414d5261ec61f5a42c4d292b164b8e
Hari Chauhan <hari-c@hotmail.co.uk>
2022-06-15 18:41:40 +0100

    Update useContract to handle events

--- packages/front-end/src/hooks/useContract.tsx
+++ packages/front-end/src/hooks/useContract.tsx
@@ -1,28 +1,52 @@
-import * as ethers from "ethers";
-import { useCallback, useEffect, useState } from "react";
-import { useWalletContext } from "../App";
-import { toast } from "react-toastify";
 import { TransactionResponse } from "@ethersproject/abstract-provider";
+import * as ethers from "ethers";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { toast } from "react-toastify";
+import { useWalletContext } from "../App";
 import addresses from "../contracts.json";
 import { ContractAddresses, ETHNetwork } from "../types";
 
-type useContractRyskContractArgs = {
+type EventName = string;
+type EventData = any[];
+
+type EventHandler<T extends EventData> = (...args: T) => void;
+
+type EventHandlerMap<T extends Record<EventName, EventData>> = {
+  [event in keyof T]: EventHandler<T[event]>;
+};
+
+type IsListeningMap<T extends Partial<Record<EventName, EventData>>> = {
+  [event in keyof T]: boolean;
+};
+
+type useContractRyskContractArgs<T extends Record<EventName, EventData>> = {
   contract: keyof ContractAddresses;
   ABI: ethers.ContractInterface;
   readOnly?: boolean;
+  events?: EventHandlerMap<T>;
+  isListening?: IsListeningMap<T>;
 };
 
-type useContractExternalContractArgs = {
+type useContractExternalContractArgs<T extends Record<EventName, EventData>> = {
   contractAddress: string;
   ABI: ethers.ContractInterface;
   readOnly?: boolean;
+  events?: EventHandlerMap<T>;
+  isListening?: IsListeningMap<T>;
 };
 
-type useContractArgs =
-  | useContractRyskContractArgs
-  | useContractExternalContractArgs;
+type useContractArgs<T extends Record<EventName, EventData>> =
+  | useContractRyskContractArgs<T>
+  | useContractExternalContractArgs<T>;
 
-export const useContract = (args: useContractArgs) => {
+/**
+ *
+ * @param args
+ * @returns
+ */
+export const useContract = <T extends Record<EventName, EventData> = any>(
+  args: useContractArgs<T>
+) => {
   const [network] = useState(
     process.env.REACT_APP_NETWORK as keyof typeof addresses | undefined
   );
@@ -31,11 +55,15 @@ export const useContract = (args: useContractArgs) => {
     null
   );
 
+  // Store events in state on init, to avoid re-adding event handlers
+  // when reference changes on re-renders.
+  const [contractEvents] = useState(() => args.events);
+
   const callWithErrorHandling = useCallback(
     async ({
       method,
       args,
-      successMessage: successMessage = "✅ Transaction successful",
+      successMessage: successMessage = "✅ Transaction created",
       onComplete,
       onFail,
     }: {
@@ -76,14 +104,80 @@ export const useContract = (args: useContractArgs) => {
       const address =
         "contract" in args
           ? (addresses as Record<ETHNetwork, ContractAddresses>)[network][
-              (args as useContractRyskContractArgs).contract
+              (args as useContractRyskContractArgs<T>).contract
             ]
-          : (args as useContractExternalContractArgs).contractAddress;
+          : (args as useContractExternalContractArgs<T>).contractAddress;
       setEthersContract(
         new ethers.Contract(address, args.ABI, signerOrProvider)
       );
     }
   }, [args, provider, network, ethersContract]);
+
+  // Takes the handlers map and creates an updated map where the handler
+  // is wrapped in an if statement that checks whether the isListening values
+  // is true (or if it's not present, in which case we assume true).
+  const updatedHandlers = useMemo(() => {
+    const handlers: Partial<EventHandlerMap<T>> = {};
+
+    if (contractEvents) {
+      const eventNames = Object.keys(
+        contractEvents
+      ) as (keyof EventHandlerMap<T>)[];
+      eventNames.forEach((eventName) => {
+        const updatedHandler: EventHandlerMap<T>[typeof eventName] = (
+          ...handlerArgs
+        ) => {
+          const shouldRunHandler =
+            !args.isListening ||
+            !args.isListening[eventName] ||
+            (args.isListening && args.isListening[eventName]);
+          if (shouldRunHandler) {
+            const handler = contractEvents[eventName];
+            handler(...handlerArgs);
+          }
+        };
+        handlers[eventName] = updatedHandler;
+      });
+    }
+
+    return handlers;
+  }, [args.isListening, contractEvents]);
+
+  // Attaches and removes the handlers as isListening map changes.
+  useEffect(() => {
+    if (ethersContract && updatedHandlers) {
+      const eventNames = Object.keys(
+        updatedHandlers
+      ) as (keyof EventHandlerMap<T>)[];
+
+      eventNames.forEach((eventName) => {
+        const handler = updatedHandlers[eventName];
+        // Need to do this check as we instanced handlers as a Partial in the Memo above.
+        // Should always be true.
+        if (handler) {
+          // @ts-ignore - unable to tell ethers that this handler
+          // takes specific args, and not just any[]
+          ethersContract.on(eventName as string, handler);
+        }
+      });
+    }
+
+    return () => {
+      if (ethersContract && updatedHandlers) {
+        const eventNames = Object.keys(
+          updatedHandlers
+        ) as (keyof EventHandlerMap<T>)[];
+
+        eventNames.forEach((eventName) => {
+          const handler = updatedHandlers[eventName];
+          if (handler) {
+            // @ts-ignore - same as above where we're adding the listener.
+            ethersContract.off(eventName as string, handler);
+          }
+        });
+      }
+    };
+  }, [updatedHandlers, ethersContract]);
 
   return [ethersContract, callWithErrorHandling] as const;
 };
