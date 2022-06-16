518aed328fe7150769b445272538601f4d1915b3
Hari Chauhan <hari-c@hotmail.co.uk>
2022-06-16 20:20:26 +0100

    Patch CI

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -128,7 +128,7 @@ module.exports = {
 		},
 		rinkeby: {
 			url: rinkeby,
-			accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+			accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : accounts,
 			chainId: 4,
 			saveDeployments: true
 		},
@@ -136,7 +136,7 @@ module.exports = {
 			url: arbitrumRinkeby,
 			chainId: 421611,
 			saveDeployments: true,
-			accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+			accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : accounts,
 			gas: 500000000
 		}
 	},
