3b12bc44703594b7c0a69b81b80c1706f19236f7
Hari Chauhan <hari-c@hotmail.co.uk>
2022-06-17 18:08:16 +0100

    Update layout of trading card

--- packages/front-end/src/components/optionsTrading/CustomOptionOrder.tsx
+++ packages/front-end/src/components/optionsTrading/CustomOptionOrder.tsx
@@ -3,16 +3,9 @@ import React, { useState } from "react";
 import { BIG_NUMBER_DECIMALS, DECIMALS } from "../../config/constants";
 import { useOptionsTradingContext } from "../../state/OptionsTradingContext";
 import { OptionsTradingActionType, OptionType } from "../../state/types";
-import { Option } from "../../types";
 import { Button } from "../shared/Button";
-import { RadioButtonList } from "../shared/RadioButtonList";
 import { TextInput } from "../shared/TextInput";
 import { ExpiryDatePicker } from "./ExpiryDatePicker";
-
-const optionTypeOptions: Option<OptionType>[] = [
-  { key: OptionType.CALL, label: "Calls", value: OptionType.CALL },
-  { key: OptionType.PUT, label: "Puts", value: OptionType.PUT },
-];
 
 export const CustomOptionOrder: React.FC = () => {
   const {
@@ -25,10 +18,6 @@ export const CustomOptionOrder: React.FC = () => {
   const {
     state: { expiryDate },
   } = useOptionsTradingContext();
-
-  const setOptionType = (optionType: OptionType) => {
-    dispatch({ type: OptionsTradingActionType.SET_OPTION_TYPE, optionType });
-  };
 
   const handleSubmit = () => {
     dispatch({
@@ -56,78 +45,75 @@ export const CustomOptionOrder: React.FC = () => {
 
   return (
     <div className="w-full min-w-[420px]">
-      <div className="bg-black p-2 text-white border-r-2 border-white">
-        <p>Select Type</p>
+      <div className="bg-black p-2 text-white border-white border-r-2">
+        <p>
+          <b>2a. Custom Option</b>
+        </p>
       </div>
-      <div className="mb-4 border-b-2 border-black">
-        <RadioButtonList
-          options={optionTypeOptions}
-          selected={optionType}
-          setSelected={setOptionType}
-          removeOuterBorder
-        />
-      </div>
-      <div className="bg-black p-2 text-white border-white">
+      <div className="h-4 border-r-2 border-black" />
+      <div className="bg-black p-2 text-white">
         <p>Select Expiry</p>
       </div>
-      <div className="mb-4">
-        <ExpiryDatePicker />
-      </div>
-      <div>
-        <div className="bg-black p-2 text-white border-white">
-          <p>Add a custom strike</p>
-        </div>
-        <div className="flex flex-col px-4 mb-4">
-          <div className="flex items-center">
-            <h4 className="font-parabole mr-2 pb-1">Custom Strike:</h4>
-            {uiStrikePrice && <p>{uiStrikePrice} USDC</p>}
-          </div>
-          <p className="text-gray-500 text-xs">
-            Min: $
-            {optionParams
-              ? optionType === OptionType.CALL
-                ? optionParams.minCallStrikePrice
-                    .div(BIG_NUMBER_DECIMALS.RYSK)
-                    .toString()
-                : optionParams.minPutStrikePrice
-                    .div(BIG_NUMBER_DECIMALS.RYSK)
-                    .toString()
-              : ""}{" "}
-            / Max: $
-            {optionParams
-              ? optionType === OptionType.CALL
-                ? optionParams.maxCallStrikePrice
-                    .div(BIG_NUMBER_DECIMALS.RYSK)
-                    .toString()
-                : optionParams.maxPutStrikePrice
-                    .div(BIG_NUMBER_DECIMALS.RYSK)
-                    .toString()
-              : ""}
-          </p>
-        </div>
+      <div className="border-r-2 border-black">
         <div className="mb-4">
-          <TextInput
-            value={uiStrikePrice}
-            setValue={setUIStrikePrice}
-            className="text-right border-x-0 w-full"
-            iconLeft={
-              <div className="px-2 flex items-center h-full">
-                <p className="text-gray-600">$</p>
-              </div>
-            }
-            numericOnly
-            maxNumDecimals={6}
-          />
+          <ExpiryDatePicker />
         </div>
-        <Button
-          disabled={submitIsDisabled}
-          className={`!py-4 w-full !bg-black text-white mb-4 border-x-0 ${
-            submitIsDisabled && "!bg-gray-300"
-          }`}
-          onClick={handleSubmit}
-        >
-          Add
-        </Button>
+        <div>
+          <div className="bg-black p-2 text-white border-white">
+            <p>Add a custom strike</p>
+          </div>
+          <div className="flex flex-col p-4">
+            <div className="flex items-center">
+              <h4 className="font-parabole mr-2 pb-1">Custom Strike:</h4>
+              {uiStrikePrice && <p>{uiStrikePrice} USDC</p>}
+            </div>
+            <p className="text-gray-500 text-xs">
+              Min: $
+              {optionParams
+                ? optionType === OptionType.CALL
+                  ? optionParams.minCallStrikePrice
+                      .div(BIG_NUMBER_DECIMALS.RYSK)
+                      .toString()
+                  : optionParams.minPutStrikePrice
+                      .div(BIG_NUMBER_DECIMALS.RYSK)
+                      .toString()
+                : ""}{" "}
+              / Max: $
+              {optionParams
+                ? optionType === OptionType.CALL
+                  ? optionParams.maxCallStrikePrice
+                      .div(BIG_NUMBER_DECIMALS.RYSK)
+                      .toString()
+                  : optionParams.maxPutStrikePrice
+                      .div(BIG_NUMBER_DECIMALS.RYSK)
+                      .toString()
+                : ""}
+            </p>
+          </div>
+          <div className="mb-4">
+            <TextInput
+              value={uiStrikePrice}
+              setValue={setUIStrikePrice}
+              className="text-right border-x-0 w-full"
+              iconLeft={
+                <div className="px-2 flex items-center h-full">
+                  <p className="text-gray-600">$</p>
+                </div>
+              }
+              numericOnly
+              maxNumDecimals={6}
+            />
+          </div>
+          <Button
+            disabled={submitIsDisabled}
+            className={`!py-4 w-full !bg-black text-white mb-4 border-x-0 ${
+              submitIsDisabled && "!bg-gray-300"
+            }`}
+            onClick={handleSubmit}
+          >
+            Add
+          </Button>
+        </div>
       </div>
     </div>
   );
--- packages/front-end/src/components/optionsTrading/OptionsTradingContent.tsx
+++ packages/front-end/src/components/optionsTrading/OptionsTradingContent.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { useGlobalContext } from "../../state/GlobalContext";
 import { useOptionsTradingContext } from "../../state/OptionsTradingContext";
-import { OptionsTradingActionType, Option } from "../../state/types";
+import {
+  Option,
+  OptionsTradingActionType,
+  OptionType,
+} from "../../state/types";
+import { Option as ListOption } from "../../types";
 import { Card } from "../shared/Card";
 import { ETHPriceIndicator } from "../shared/ETHPriceIndicator";
+import { RadioButtonList } from "../shared/RadioButtonList";
 import { CustomOptionOrder } from "./CustomOptionOrder";
 import { OptionsTable } from "./OptionsTable";
 import { Purchase } from "./Purchase";
+
+const optionTypeOptions: ListOption<OptionType>[] = [
+  { key: OptionType.CALL, label: "Calls", value: OptionType.CALL },
+  { key: OptionType.PUT, label: "Puts", value: OptionType.PUT },
+];
 
 export const OptionsTradingContent = () => {
   const {
@@ -15,8 +26,12 @@ export const OptionsTradingContent = () => {
 
   const {
     dispatch,
-    state: { selectedOption },
+    state: { selectedOption, optionType },
   } = useOptionsTradingContext();
+
+  const setOptionType = (optionType: OptionType) => {
+    dispatch({ type: OptionsTradingActionType.SET_OPTION_TYPE, optionType });
+  };
 
   const setSelectedOption = (option: Option | null) => {
     dispatch({ type: OptionsTradingActionType.SET_SELECTED_OPTION, option });
@@ -46,18 +61,32 @@ export const OptionsTradingContent = () => {
               <ETHPriceIndicator />
             </div>
           </div>
+          <div className="w-full">
+            <div className="bg-black p-2 text-white">
+              <p>
+                <b>1. Select Type</b>
+              </p>
+            </div>
+            <div className="border-b-2 border-black mb-4">
+              <RadioButtonList
+                options={optionTypeOptions}
+                selected={optionType}
+                setSelected={setOptionType}
+                removeOuterBorder
+              />
+            </div>
+          </div>
           <div className="flex grow items-stretch">
             <div className="bg-bone rounded-bl-lg flex flex-col min-w-[420px]">
-              <div className="bg-black p-2 text-white border-r-2 border-white">
-                <p>Custom Option</p>
-              </div>
-              <div className="grow border-r-2 border-black rounded-bl-lg grow">
+              <div className="grow rounded-bl-lg grow">
                 <CustomOptionOrder />
               </div>
             </div>
             <div className="grow flex-col rounded-br-lg">
               <div className="bg-black p-2 text-white ">
-                <p>Options</p>
+                <p>
+                  <b>2b. Options</b>
+                </p>
               </div>
               <div className="grow border-black rounded-br-lg">
                 <OptionsTable />
@@ -67,7 +96,9 @@ export const OptionsTradingContent = () => {
           <div className="py-2 border-y-2 border-black" />
           <div className="grow rounded-br-lg flex flex-col">
             <div className="bg-black p-2 text-white flex justify-between">
-              <p>Complete order</p>
+              <p>
+                <b>3. Complete order</b>
+              </p>
               {selectedOption && (
                 <button
                   className="text-xl top-4 right-4 text-white"
