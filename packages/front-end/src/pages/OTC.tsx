d900b3489fb68b85e12a3bbda6c93cf7b5aa99b5
Hari Chauhan <hari-c@hotmail.co.uk>
2022-07-21 20:48:33 +0200

    Add content to OTC page

--- packages/front-end/src/pages/OTC.tsx
+++ packages/front-end/src/pages/OTC.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { OTCPageContent } from "../components/OTC/OTCPageContent";
 
 export const OTC = () => {
-  return <div></div>;
+  return (
+    <div className="col-start-1 col-end-17">
+      <OTCPageContent />
+    </div>
+  );
 };
