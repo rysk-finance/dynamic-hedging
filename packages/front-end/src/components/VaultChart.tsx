2e512eb04ddd99b7c568350bbb1097e9ed093e59
ugolino <dan.ugolini@gmail.com>
2022-08-03 14:23:12 +0200

    added data chart

--- packages/front-end/src/components/VaultChart.tsx
+++ packages/front-end/src/components/VaultChart.tsx
@@ -12,17 +12,17 @@ import {
   Label,
 } from "recharts";
 const data = [
-  { value: 0, uv: 400, pv: 2400, amt: 2400 },
-  { value: 1, uv: 540, pv: 2400, amt: 2400 },
-  { value: 2, uv: 600, pv: 2400, amt: 2400 },
-  { value: 3, uv: 700, pv: 2400, amt: 2400 },
-  { value: 4, uv: 200, pv: 2400, amt: 2400 },
-  { value: 5, uv: 100, pv: 2400, amt: 2400 },
+  { date: "jun 1", yield: 0.03, pv: 2400, amt: 2400 },
+  { date: "jun 2", yield: 0.07, pv: 2400, amt: 2400 },
+  { date: "jun 3", yield: 0.11, pv: 2400, amt: 2400 },
+  { date: "jun 4", yield: 0.07, pv: 2400, amt: 2400 },
+  { date: "jun 5", yield: 0.13, pv: 2400, amt: 2400 },
+  { date: "jun 6", yield: 0.13, pv: 2400, amt: 2400 },
 ];
 
 export const VaultChart = () => {
   return (
-    <Card tabPunchColor="bone" headerContent="DHV.performance">
+    <Card tabPunchColor="bone" headerContent="DHV.cumulativeYield(%)">
       <div className="pb-8 py-12 px-8 flex flex-col lg:flex-row h-full">
         <div className="flex h-full w-full justify-around">
           <ResponsiveContainer width={"95%"} height={400}>
@@ -30,13 +30,13 @@ export const VaultChart = () => {
               data={data}
               margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
             >
-              <Line type="monotone" dataKey="uv" stroke="black" />
+              <Line type="monotone" dataKey="yield" stroke="black" />
               <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-              <XAxis dataKey="name" label={"Date"} angle={90} />
+              <XAxis dataKey="date" angle={0} />
               <YAxis>
                 <Label
                   angle={-90}
-                  value="Performance"
+                  // value="Cumulative Yield (%)"
                   position="center"
                   dx={-20}
                 />
