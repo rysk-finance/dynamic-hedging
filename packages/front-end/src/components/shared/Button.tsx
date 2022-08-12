476265e249657d55017644534f98234b1cf99aaa
Hari Chauhan <hari-c@hotmail.co.uk>
2022-08-12 17:11:45 +0100

    Make disabled button text more visible

--- packages/front-end/src/components/shared/Button.tsx
+++ packages/front-end/src/components/shared/Button.tsx
@@ -57,7 +57,7 @@ export const Button: React.FC<
       className={`border-black border-2  text-md px-2 py-1 ${
         color === "black" ? "bg-black text-white" : "bg-white text-black"
       } ${props.className ?? ""} ${
-        props.disabled ? "!bg-gray-300 !cursor-default" : ""
+        props.disabled ? "!bg-gray-300 !cursor-default !text-gray-600" : ""
       }`}
     />
   );
