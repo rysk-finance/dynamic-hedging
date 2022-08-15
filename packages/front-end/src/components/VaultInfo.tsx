84e591bfa9908887ac10ebbbb9a1b67a962b3f95
Hari Chauhan <hari-c@hotmail.co.uk>
2022-08-15 23:49:46 +0100

    Use existing date utils

--- packages/front-end/src/components/VaultInfo.tsx
+++ packages/front-end/src/components/VaultInfo.tsx
@@ -1,51 +1,32 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useContract } from "../hooks/useContract";
 import LPABI from "../abis/LiquidityPool.json";
 import { CHAINID, SCAN_URL } from "../config/constants";
 import { useVaultContext } from "../state/VaultContext";
 import { BigNumberDisplay } from "./BigNumberDisplay";
 import { Currency } from "../types";
-import moment from "moment";
-import { useWalletContext } from "../App";
+import { getClosestFridayToDate } from "../utils/getSuggestedExpiryDates";
 
-
-export const VaultInfo = () => { 
-
+export const VaultInfo = () => {
   const {
-    state: { currentEpoch, currentPricePerShare, userRyskBalance },
-  } = useVaultContext(); 
+    state: { currentEpoch, currentPricePerShare },
+  } = useVaultContext();
 
-  function getNextFriday(date = new Date()) {
-    const dateCopy = new Date(date.getTime());
+  const nextFriday = useMemo(() => getClosestFridayToDate(new Date()), []);
 
-    // TODO change based on epoch
-    // assuming Friday 11am UTC epoch
-    const nextDate = dateCopy.getDay() !== 5 || ( dateCopy.getDay() === 5 && dateCopy.getUTCHours() > 10 )  ? 
-      new Date(
-        dateCopy.setDate(
-          dateCopy.getDate() + ((7 - dateCopy.getDay() + 5) % 7 || 7),
-        ),
-      ) :
-      dateCopy
+  const [lpContract] = useContract({
+    contract: "liquidityPool",
+    ABI: LPABI,
+    readOnly: true,
+  });
 
-    return moment(nextDate).format('MMMM Do YYYY');
-  }
-
-  const [lpContract] = useContract({ contract: "liquidityPool", ABI: LPABI, readOnly: true });
-
-  
   return (
     <div className="pb-8 py-12 px-8">
       <div className="grid grid-cols-2">
-
         <div>
           <h4>DHV</h4>
-          <p className="mt-4">
-            Chain: Arbitrum
-          </p>
-          <p className="mt-4">
-            Current Epoch: {currentEpoch?.toString()}
-          </p>
+          <p className="mt-4">Chain: Arbitrum</p>
+          <p className="mt-4">Current Epoch: {currentEpoch?.toString()}</p>
           <p className="mt-4">
             DHV Share Price:{" "}
             <BigNumberDisplay
@@ -58,26 +39,26 @@ export const VaultInfo = () => {
           </p>
           <p className="mt-4">
             {/* TODO add next epoch start */}
-            Next Epoch Start: { getNextFriday(new Date()) } 11:00 UTC
+            Next Epoch Start: {nextFriday.toDateString()} 11:00 UTC
           </p>
         </div>
-
 
         <div>
           <h4>Addresses</h4>
           <p className="mt-4">
-            DHV: {" "} 
-            <a 
-              href={`${SCAN_URL[CHAINID.ARBITRUM_MAINNET]}/address/${lpContract?.address}`} 
+            DHV:{" "}
+            <a
+              href={`${SCAN_URL[CHAINID.ARBITRUM_MAINNET]}/address/${
+                lpContract?.address
+              }`}
               target="blank"
-              className="underline hover:font-medium"> 
-              { lpContract?.address } 
+              className="underline hover:font-medium"
+            >
+              {lpContract?.address}
             </a>
           </p>
         </div>
-
       </div>
-
     </div>
   );
 };
