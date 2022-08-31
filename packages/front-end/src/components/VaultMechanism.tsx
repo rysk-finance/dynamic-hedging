76c67687785952dffe80011f2df9c960c9070467
ugolino <dan.ugolini@gmail.com>
2022-09-01 00:16:59 +0200

    fix typo

--- packages/front-end/src/components/VaultMechanism.tsx
+++ packages/front-end/src/components/VaultMechanism.tsx
@@ -15,7 +15,7 @@ export const VaultMechanism = () => {
               </li>
               <li className="pb-2">
                 Your USDC deposit will be queued till the next epoch start. 
-                At this , you can also deposit additional USDC
+                At this stage, you can also deposit additional USDC
               </li>
               <li className="pb-2">
                 Once the new epoch starts your deposit will be converted to
