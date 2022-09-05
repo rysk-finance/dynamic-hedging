e363743568fbdf902ef7a6ebacb43b54e6776da5
ugolino <dan.ugolini@gmail.com>
2022-09-05 17:33:14 +0200

    fix merge

--- packages/front-end/src/config/constants.ts
+++ packages/front-end/src/config/constants.ts
@@ -19,6 +19,8 @@ export const BIG_NUMBER_DECIMALS: Record<Token, BigNumber> = {
   OPYN: BigNumber.from("100000000"),
   RYSK: BigNumber.from("1000000000000000000"),
 };
+// Ethers event polling interval
+export const DEFAULT_POLLING_INTERVAL = 20000;
 
 // Proportion added to approval transaction to account for price moving.
 const APPROVAL_MARGIN = 0.1;
@@ -37,6 +39,13 @@ export const IDToNetwork: Record<CHAINID, ETHNetwork> = {
   [CHAINID.LOCALHOST]: ETHNetwork.LOCALHOST,
 };
 
+export const RPC_URL_MAP: Record<CHAINID, string> = {
+  [CHAINID.ETH_MAINNET]: `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
+  [CHAINID.ARBITRUM_MAINNET]: `https://arbitrum-mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
+  [CHAINID.ARBITRUM_RINKEBY]: `https://arbitrum-rinkeby.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
+  [CHAINID.LOCALHOST]: "",
+};
+
 export const SUBGRAPH_URL = {
   [CHAINID.LOCALHOST]: "",
   [CHAINID.ETH_MAINNET]: "",
@@ -48,12 +57,9 @@ export const SUBGRAPH_URL = {
 export const SCAN_URL = {
   [CHAINID.LOCALHOST]: "",
   [CHAINID.ETH_MAINNET]: "",
-  [CHAINID.ARBITRUM_RINKEBY]:
-    "https://testnet.arbiscan.io",
-  [CHAINID.ARBITRUM_MAINNET]: 
-    "https://arbiscan.io"
-  ,
-}
+  [CHAINID.ARBITRUM_RINKEBY]: "https://testnet.arbiscan.io",
+  [CHAINID.ARBITRUM_MAINNET]: "https://arbiscan.io",
+};
 
 /**
  * Tokens and owners
@@ -160,4 +166,4 @@ export const ADDRESS_BOOK = {
 export const ORACLE_DISPUTE_PERIOD = 7200;
 export const ORACLE_LOCKING_PERIOD = 300;
 
-export const DHV_NAME = "DHV-USDC"
+export const DHV_NAME = "DHV-USDC";
