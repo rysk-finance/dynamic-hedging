448d6c889bf39cb6f80bee14ccb383dfe740b486
Hari Chauhan <hari-c@hotmail.co.uk>
2022-09-07 14:10:42 +0100

    Update create otc strangle script

--- packages/contracts/scripts/createOTCStrangle.ts
+++ packages/contracts/scripts/createOTCStrangle.ts
@@ -3,11 +3,13 @@ import { BigNumber, ethers } from "ethers"
 import hre from "hardhat"
 // This file doesn't exist in CI, only exists locally (in git ignore)
 import { arbitrumRinkeby } from "../contracts.json"
+import { abi as optionHandlerABI } from "../artifacts/contracts/AlphaOptionHandler.sol/AlphaOptionHandler.json"
+import { toWei } from "../utils/conversion-helper"
 
 const ADDRESS = "0xed9d4593a9BD1aeDBA8C5F9013EF3323FEC5e4dC"
 
 const RYSK_DECIMAL = BigNumber.from("1000000000000000000")
-const USDC_DECIMAL = BigNumber.from("1000000")
+const RYSK_EXP = 1e18
 
 type OrderBounds = {
 	callMinDelta: BigNumber
@@ -24,111 +26,45 @@ async function main() {
 			process.exit()
 		}
 
-		const optionHandler = await hre.ethers.getContractAt(
-			"OptionHandler",
-			arbitrumRinkeby.optionHandler
+		// const optionHandler = await hre.ethers.getContractAt(
+		// 	"OptionHandler",
+		// 	arbitrumRinkeby.optionHandler
+		// )
+
+		const signers = await hre.ethers.getSigners()
+		const alphaOptionHandler = new hre.ethers.Contract(
+			arbitrumRinkeby.optionHandler,
+			optionHandlerABI,
+			signers[0]
 		)
-
-		const priceFeed = await hre.ethers.getContractAt("PriceFeed", arbitrumRinkeby.priceFeed)
-
-		const pvFeed = await hre.ethers.getContractAt(
-			"PortfolioValuesFeed",
-			arbitrumRinkeby.portfolioValuesFeed
-		)
-
-		const liquidityPool = await hre.ethers.getContractAt(
-			"LiquidityPool",
-			arbitrumRinkeby.liquidityPool
-		)
-
-		const price = await priceFeed.getNormalizedRate(arbitrumRinkeby.WETH, arbitrumRinkeby.USDC)
-
-		const pvTransaction = await pvFeed.fulfill(
-			ethers.utils.formatBytes32String("1"),
-			arbitrumRinkeby.WETH,
-			arbitrumRinkeby.USDC,
-			BigNumber.from("0"),
-			BigNumber.from("0"),
-			BigNumber.from("0"),
-			BigNumber.from("0"),
-			BigNumber.from("0"),
-			price
-		)
-
-		await pvTransaction.wait()
-
-		const orderBounds: OrderBounds = await optionHandler.customOrderBounds()
 
 		const callOption = {
-			expiration: "1661068800",
+			expiration: "1663315200",
 			// Need to update to give a value in the orderBounds defined on optionHandler.
-			strike: BigNumber.from("2928").mul(RYSK_DECIMAL),
+			strike: BigNumber.from("2500").mul(RYSK_DECIMAL),
 			isPut: false,
 			underlying: arbitrumRinkeby.WETH,
 			strikeAsset: arbitrumRinkeby.USDC,
 			collateral: arbitrumRinkeby.USDC
 		}
 
+		const callQuote = BigNumber.from((7.45 * RYSK_EXP).toString())
+
 		const putOption = {
-			expiration: "1661068800",
+			expiration: "1663315200",
 			// Need to update to give a value in the orderBounds defined on optionHandler.
-			strike: BigNumber.from("491").mul(RYSK_DECIMAL),
+			strike: BigNumber.from("1000").mul(RYSK_DECIMAL),
 			isPut: true,
 			underlying: arbitrumRinkeby.WETH,
 			strikeAsset: arbitrumRinkeby.USDC,
 			collateral: arbitrumRinkeby.USDC
 		}
 
-		const orderAmount = RYSK_DECIMAL.mul(2)
+		const putQuote = BigNumber.from((7.45 * RYSK_EXP).toString())
 
-		const [callQuote, callDelta]: BigNumber[] = await liquidityPool.quotePriceWithUtilizationGreeks(
-			callOption,
-			orderAmount,
-			false
-		)
+		const orderAmount = BigNumber.from((1 * RYSK_EXP).toString())
 
-		const [putQuote, putDelta]: BigNumber[] = await liquidityPool.quotePriceWithUtilizationGreeks(
-			putOption,
-			orderAmount,
-			false
-		)
-
-		if (callDelta.lt(orderBounds.callMinDelta) || callDelta.gt(orderBounds.callMaxDelta)) {
-			console.log(
-				`Call option has invalid delta of ${ethers.utils.formatEther(
-					callDelta
-				)}.\n Bounds are ${ethers.utils.formatEther(
-					orderBounds.callMinDelta
-				)} - ${ethers.utils.formatEther(orderBounds.callMaxDelta)}`
-			)
-			process.exit()
-		}
-
-		if (putDelta.lt(orderBounds.putMinDelta) || putDelta.gt(orderBounds.putMaxDelta)) {
-			console.log(
-				`Put option has invalid delta of ${ethers.utils.formatEther(
-					putDelta
-				)}.\n Bounds are ${ethers.utils.formatEther(
-					orderBounds.putMinDelta
-				)} - ${ethers.utils.formatEther(orderBounds.putMaxDelta)}`
-			)
-			process.exit()
-		}
-
-		console.log(
-			"Call: delta -",
-			ethers.utils.formatEther(callDelta),
-			"quote: -",
-			ethers.utils.formatEther(callQuote)
-		)
-		console.log(
-			"Put: delta -",
-			ethers.utils.formatEther(putDelta),
-			"quote: -",
-			ethers.utils.formatEther(putQuote)
-		)
-
-		const orderTransaction = await optionHandler.createStrangle(
+		const orderTransaction = await alphaOptionHandler.createStrangle(
 			callOption,
 			putOption,
 			orderAmount,
@@ -137,7 +73,9 @@ async function main() {
 			putQuote,
 			BigNumber.from(1800),
 			// Update to order reciever address.
-			"0x939f39468b34E985d5Faa8d044569cfeC9E6CA69"
+			"0x939f39468b34E985d5Faa8d044569cfeC9E6CA69",
+			[toWei("100"), toWei("100")],
+			[toWei("100"), toWei("100")]
 		)
 
 		console.log(orderTransaction)
