7a6ac4649a2760367b733b84c4cfe940a901e168
Hari Chauhan <hari-c@hotmail.co.uk>
2022-09-12 14:26:24 +0100

    format

--- packages/front-end/src/components/dashboard/UserOptionsList.tsx
+++ packages/front-end/src/components/dashboard/UserOptionsList.tsx
@@ -12,9 +12,13 @@ import { RadioButtonSlider } from "../shared/RadioButtonSlider";
 import { BuyBack } from "./BuyBack";
 import { RFQ_FORM } from "../../config/links";
 import { useContract } from "../../hooks/useContract";
-import OpynController from '../../abis/OpynController.json'
+import OpynController from "../../abis/OpynController.json";
 import { toast } from "react-toastify";
-import { BIG_NUMBER_DECIMALS, DECIMALS, ZERO_ADDRESS } from "../../config/constants";
+import {
+  BIG_NUMBER_DECIMALS,
+  DECIMALS,
+  ZERO_ADDRESS,
+} from "../../config/constants";
 
 enum OptionState {
   OPEN = "Open",
@@ -23,8 +27,8 @@ enum OptionState {
 
 interface Position {
   id: string;
-  expiryTimestamp: string,
-  strikePrice: string,
+  expiryTimestamp: string;
+  strikePrice: string;
   expired: boolean;
   symbol: string;
   amount: string;
@@ -53,7 +57,6 @@ export const UserOptionsList = () => {
   const [listedOptionState, setListedOptionState] = useState(OptionState.OPEN);
 
   const parsePositions = (data: any) => {
-
     const positions: Position[] = [];
     const timeNow = Date.now() / 1000;
 
@@ -61,15 +64,13 @@ export const UserOptionsList = () => {
     data?.positions.forEach((position: any) => {
       const expired = timeNow > position.oToken.expiryTimestamp;
 
-      const otokenBalance = position.account.balances
-                            .filter( (item: any)  => 
-                              item.token.id === position.oToken.id
-                            )[0] 
-                            ? position.account.balances
-                              .filter( (item: any)  => 
-                                item.token.id === position.oToken.id 
-                              )[0].balance 
-                            : 0
+      const otokenBalance = position.account.balances.filter(
+        (item: any) => item.token.id === position.oToken.id
+      )[0]
+        ? position.account.balances.filter(
+            (item: any) => item.token.id === position.oToken.id
+          )[0].balance
+        : 0;
       // 1e8
       const totPremium =
         position.writeOptionsTransactions.length > 0
@@ -83,9 +84,9 @@ export const UserOptionsList = () => {
 
       // premium converted to 1e18
       const entryPrice =
-        otokenBalance > 0 && totPremium > 0 
-        ? totPremium * ( 10 ** (DECIMALS.OPYN - DECIMALS.USDC) ) / otokenBalance 
-        : 0;
+        otokenBalance > 0 && totPremium > 0
+          ? (totPremium * 10 ** (DECIMALS.OPYN - DECIMALS.USDC)) / otokenBalance
+          : 0;
 
       // TODO add current price and PNL
 
@@ -143,38 +144,38 @@ export const UserOptionsList = () => {
     }
   );
 
-
-  const [opynControllerContract, opynControllerContractCall] =
-    useContract({
-      contract: "OpynController",
-      ABI: OpynController,
-      readOnly: false
+  const [opynControllerContract, opynControllerContractCall] = useContract({
+    contract: "OpynController",
+    ABI: OpynController,
+    readOnly: false,
   });
 
-  const completeRedeem = useCallback(async (otokenId: string, amount: string) => {
+  const completeRedeem = useCallback(
+    async (otokenId: string, amount: string) => {
+      const args = {
+        actionType: 8,
+        owner: ZERO_ADDRESS,
+        secondAddress: account,
+        asset: otokenId,
+        vaultId: "0",
+        amount: amount,
+        index: "0",
+        data: ZERO_ADDRESS,
+      };
 
-    const args = {
-      actionType: 8,
-      owner: ZERO_ADDRESS,
-      secondAddress: account,
-      asset: otokenId,
-      vaultId: '0',
-      amount: amount,
-      index: '0',
-      data: ZERO_ADDRESS,
-    }
-
-    await opynControllerContractCall({
-      method: opynControllerContract?.operate,
-      args: [[args]],
-      onSubmit: () => {
-        // TODO add loader
-      },
-      onComplete: () => {
-        toast("✅ Order complete");
-      },
-    }); 
-  }, [account, opynControllerContract, opynControllerContractCall])
+      await opynControllerContractCall({
+        method: opynControllerContract?.operate,
+        args: [[args]],
+        onSubmit: () => {
+          // TODO add loader
+        },
+        onComplete: () => {
+          toast("✅ Order complete");
+        },
+      });
+    },
+    [account, opynControllerContract, opynControllerContractCall]
+  );
 
   return (
     positions && (
@@ -199,7 +200,9 @@ export const UserOptionsList = () => {
                           <div className="col-span-1">Side</div>
                           <div className="col-span-4">Option</div>
                           <div className="col-span-2 text-right">Size</div>
-                          <div className="col-span-2 text-right">Entry Price</div>
+                          <div className="col-span-2 text-right">
+                            Entry Price
+                          </div>
                           <div className="col-span-3 text-center">Actions</div>
                         </div>
 
@@ -207,8 +210,19 @@ export const UserOptionsList = () => {
                           {positions &&
                             positions
                               .filter((position) => !position.expired)
-                              .sort((a, b) => a.strikePrice.localeCompare(b.strikePrice) || Number(b.strikePrice) - Number(a.strikePrice))
-                              .sort((a, b) => a.expiryTimestamp.localeCompare(b.expiryTimestamp) || Number(b.expiryTimestamp) - Number(a.expiryTimestamp))
+                              .sort(
+                                (a, b) =>
+                                  a.strikePrice.localeCompare(b.strikePrice) ||
+                                  Number(b.strikePrice) - Number(a.strikePrice)
+                              )
+                              .sort(
+                                (a, b) =>
+                                  a.expiryTimestamp.localeCompare(
+                                    b.expiryTimestamp
+                                  ) ||
+                                  Number(b.expiryTimestamp) -
+                                    Number(a.expiryTimestamp)
+                              )
                               .map((position) => (
                                 <div key={position.id} className="w-full">
                                   <div className="grid grid-cols-12 py-2">
@@ -220,7 +234,10 @@ export const UserOptionsList = () => {
                                     </div>
                                     <div className="col-span-2 text-right">
                                       <NumberFormat
-                                        value={(Number(position.amount) / ( 10 ** DECIMALS.OPYN )).toFixed(2)}
+                                        value={(
+                                          Number(position.amount) /
+                                          10 ** DECIMALS.OPYN
+                                        ).toFixed(2)}
                                         displayType={"text"}
                                         decimalScale={2}
                                       />
@@ -234,15 +251,17 @@ export const UserOptionsList = () => {
                                       />
                                     </div>
                                     <div className="col-span-3 text-center">
-                                      <Button onClick={() => {
-                                        window.open(RFQ_FORM, "_blank")
-                                      }} >
-                                      RFQ to close position  
+                                      <Button
+                                        onClick={() => {
+                                          window.open(RFQ_FORM, "_blank");
+                                        }}
+                                      >
+                                        RFQ to close position
                                       </Button>
                                     </div>
                                   </div>
                                 </div>
-                              ))} 
+                              ))}
                         </div>
                       </div>
                     </div>
@@ -250,59 +269,74 @@ export const UserOptionsList = () => {
 
                   {listedOptionState === OptionState.EXPIRED && (
                     <div className="p-4">
-
-                        <div className="grid grid-cols-12 text-left text-lg pb-4">
-                          <div className="col-span-1">Side</div>
-                          <div className="col-span-4">Option</div>
-                          <div className="col-span-2 text-right">Size</div>
-                          <div className="col-span-2 text-right">Entry Price</div>
-                          <div className="col-span-3 text-center">Actions</div>
-                        </div>
-                        <div>
-                          {positions &&
-                            positions
-                              .filter((position) => position.expired)
-                              .sort((a, b) => a.strikePrice.localeCompare(b.strikePrice) || Number(b.strikePrice) - Number(a.strikePrice))
-                              .sort((a, b) => a.expiryTimestamp.localeCompare(b.expiryTimestamp) || Number(a.expiryTimestamp) - Number(b.expiryTimestamp))
-                              .map((position) => (
-                                <div key={position.id} className="w-full">
-                                  <div className="grid grid-cols-12">
-                                    <div className="col-span-1 text-green-700">
-                                      LONG
-                                    </div>
-                                    <div className="col-span-4">
-                                      {renameOtoken(position.symbol)}
-                                    </div>
-                                    <div className="col-span-2 text-right">
-                                      <NumberFormat
-                                        value={(Number(position.amount) / ( 10 ** DECIMALS.OPYN )).toFixed(2)}
-                                        displayType={"text"}
-                                        decimalScale={2}
-                                      />
-                                    </div>
-                                    <div className="col-span-2 text-right">
-                                      <NumberFormat
-                                        value={position.entryPrice}
-                                        displayType={"text"}
-                                        prefix="$"
-                                        decimalScale={2}
-                                      />
-                                    </div>
-                                    <div className="col-span-4">
-                                      <Button
-                                        onClick={() =>
-                                          completeRedeem(position.otokenId, position.amount)
-                                        }
-                                        className="w-full"
-                                      >
-                                        redeem
-                                      </Button>
-                                    </div>
+                      <div className="grid grid-cols-12 text-left text-lg pb-4">
+                        <div className="col-span-1">Side</div>
+                        <div className="col-span-4">Option</div>
+                        <div className="col-span-2 text-right">Size</div>
+                        <div className="col-span-2 text-right">Entry Price</div>
+                        <div className="col-span-3 text-center">Actions</div>
+                      </div>
+                      <div>
+                        {positions &&
+                          positions
+                            .filter((position) => position.expired)
+                            .sort(
+                              (a, b) =>
+                                a.strikePrice.localeCompare(b.strikePrice) ||
+                                Number(b.strikePrice) - Number(a.strikePrice)
+                            )
+                            .sort(
+                              (a, b) =>
+                                a.expiryTimestamp.localeCompare(
+                                  b.expiryTimestamp
+                                ) ||
+                                Number(a.expiryTimestamp) -
+                                  Number(b.expiryTimestamp)
+                            )
+                            .map((position) => (
+                              <div key={position.id} className="w-full">
+                                <div className="grid grid-cols-12">
+                                  <div className="col-span-1 text-green-700">
+                                    LONG
+                                  </div>
+                                  <div className="col-span-4">
+                                    {renameOtoken(position.symbol)}
+                                  </div>
+                                  <div className="col-span-2 text-right">
+                                    <NumberFormat
+                                      value={(
+                                        Number(position.amount) /
+                                        10 ** DECIMALS.OPYN
+                                      ).toFixed(2)}
+                                      displayType={"text"}
+                                      decimalScale={2}
+                                    />
+                                  </div>
+                                  <div className="col-span-2 text-right">
+                                    <NumberFormat
+                                      value={position.entryPrice}
+                                      displayType={"text"}
+                                      prefix="$"
+                                      decimalScale={2}
+                                    />
+                                  </div>
+                                  <div className="col-span-4">
+                                    <Button
+                                      onClick={() =>
+                                        completeRedeem(
+                                          position.otokenId,
+                                          position.amount
+                                        )
+                                      }
+                                      className="w-full"
+                                    >
+                                      redeem
+                                    </Button>
                                   </div>
                                 </div>
-                              ))}
-                        </div>
-
+                              </div>
+                            ))}
+                      </div>
                     </div>
                   )}
                 </>
