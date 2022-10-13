5ca34521d223c289c160a49d4958288e1584e28b
ugolino <dan.ugolini@gmail.com>
2022-10-13 17:56:04 -0500

    added buybacks

--- packages/front-end/src/components/VaultTrades.tsx
+++ packages/front-end/src/components/VaultTrades.tsx
@@ -9,14 +9,16 @@ import { optionSymbolFormat } from "../utils";
 const parseTrades = (data: any) => {
 
   const writeOptionsActions = data?.writeOptionsActions
+  const buybackOptionActions = data?.buybackOptionActions
   const rebalanceDeltaActions = data?.rebalanceDeltaActions
 
-  const allTrades = [...writeOptionsActions, ...rebalanceDeltaActions].map( trade => {
+  const allTrades = [...writeOptionsActions, ...buybackOptionActions, ...rebalanceDeltaActions].map( trade => {
 
-    const tradeType = trade.__typename === "WriteOptionsAction" ? "optionsTrade" : "deltaRebalance";
+    const tradeType = ["WriteOptionsAction","BuybackOptionAction"].includes(trade.__typename) ? "optionsTrade" : "deltaRebalance";
 
     return {
       id: trade.id,
+      typename: trade.__typename,
       tradeType: tradeType,
       optionSymbol: tradeType === "optionsTrade" 
                                   ? optionSymbolFormat(trade.otoken.isPut, trade.otoken.expiryTimestamp, trade.otoken.strikePrice) 
@@ -62,6 +64,19 @@ export const VaultTrades = () => {
         premium
         transactionHash
       }
+      buybackOptionActions(first: 1000, orderBy: timestamp, orderDirection: desc, where: {timestamp_gte: ${startTimestamp} }) {
+        id
+        timestamp
+        otoken {
+          symbol
+          strikePrice
+          isPut
+          expiryTimestamp
+        }
+        amount
+        premium
+        transactionHash
+      }
       rebalanceDeltaActions(first: 1000, orderBy: timestamp, orderDirection: desc, where: {timestamp_gte: ${startTimestamp} }) {
         id
         timestamp
@@ -85,12 +100,13 @@ export const VaultTrades = () => {
     <div className="pb-8 py-12 px-8">
 
       <div className="grid grid-cols-12 text-left text-lg pb-4 px-2">
-        <div className="col-span-2">Date</div>
+        <div className="col-span-3">Date</div>
         <div className="col-span-2">Trade Type</div>
         <div className="col-span-3">Product</div>
         <div className="col-span-1 text-right">Size</div>
-        <div className="col-span-2 text-right">Premium Received</div>
-        <div className="col-span-2 text-right">Delta Change</div>
+        <div className="col-span-1 text-right">Premium Received</div>
+        <div className="col-span-1 text-right">Premium Paid</div>
+        <div className="col-span-1 text-right">Delta Change</div>
       </div>
 
       { trades && 
@@ -109,11 +125,13 @@ export const VaultTrades = () => {
               rel="noreferrer"
               >
             <div className="grid grid-cols-12 py-2 text-black hover:bg-black hover:text-white px-2">
-              <div className="col-span-2">
+              <div className="col-span-3">
                 {moment.utc(trade.timestamp * 1000).format("DD-MMM-YY HH:mm")} UTC
               </div>
               <div className="col-span-2">
-                { trade.tradeType === "optionsTrade" ? "Short Option" : "Delta Rebalance" }
+                { trade.typename === "WriteOptionsAction" && "Short Options" }
+                { trade.typename === "BuybackOptionAction" && "Buyback Options" }
+                { trade.typename === "RebalanceDeltaAction" && "Delta Rebalance" }
               </div>
               <div className="col-span-3">
                 { trade.optionSymbol }
@@ -130,8 +148,8 @@ export const VaultTrades = () => {
                   />
                 }
               </div>
-              <div className="col-span-2 text-right">
-                { trade.premium && 
+              <div className="col-span-1 text-right">
+                { trade.premium && trade.typename === "WriteOptionsAction" && 
                   <NumberFormat
                     value={(
                       Number(trade.premium) /
@@ -143,7 +161,20 @@ export const VaultTrades = () => {
                   />
                 }
               </div>
-              <div className="col-span-2 text-right">
+              <div className="col-span-1 text-right">
+                { trade.premium && trade.typename === "BuybackOptionAction" &&
+                  <NumberFormat
+                    value={(
+                      Number(trade.premium) /
+                      10 ** DECIMALS.RYSK
+                    ).toFixed(2)}
+                    displayType={"text"}
+                    prefix={"$"}
+                    decimalScale={2}
+                  />
+                }
+              </div>
+              <div className="col-span-1 text-right">
                 { trade.deltaChange && 
                   <NumberFormat
                     value={(
