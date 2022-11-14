6c6ca3e9fcc89a0a8e74a2ebdaeae5f0cac4ea1b
Josh <55187183+JoshDAO@users.noreply.github.com>
2022-11-14 18:22:04 +0000

    successfully open long position and test

--- packages/contracts/contracts/hedging/GmxHedgingReactor.sol
+++ packages/contracts/contracts/hedging/GmxHedgingReactor.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.9;
 
+import "prb-math/contracts/PRBMathUD60x18.sol";
+import "prb-math/contracts/PRBMathSD59x18.sol";
+
 import "../PriceFeed.sol";
 
 import "../libraries/AccessControl.sol";
@@ -9,8 +12,12 @@ import "../libraries/SafeTransferLib.sol";
 
 import "../interfaces/ILiquidityPool.sol";
 import "../interfaces/IHedgingReactor.sol";
+import "../interfaces/IRouter.sol";
+import "../interfaces/IPositionRouter.sol";
 
 import "@openzeppelin/contracts/utils/math/Math.sol";
+
+import "hardhat/console.sol";
 
 /**
  *  @title A hedging reactor that will manage delta by opening or closing short or long perp positions using rage trade
@@ -19,6 +26,8 @@ import "@openzeppelin/contracts/utils/math/Math.sol";
  */
 
 contract GmxHedgingReactor is IHedgingReactor, AccessControl {
+	using PRBMathSD59x18 for int256;
+	using PRBMathUD60x18 for uint256;
 	/////////////////////////////////
 	/// immutable state variables ///
 	/////////////////////////////////
@@ -39,6 +48,8 @@ contract GmxHedgingReactor is IHedgingReactor, AccessControl {
 	/// @notice delta of the pool
 	int256 public internalDelta;
 
+	mapping(bytes32 => int256) public orderDeltaChange;
+
 	/////////////////////////////////////
 	/// governance settable variables ///
 	/////////////////////////////////////
@@ -49,6 +60,10 @@ contract GmxHedgingReactor is IHedgingReactor, AccessControl {
 	uint256 public healthFactor = 5_000;
 	/// @notice should change position also sync state
 	bool public syncOnChange;
+	/// @notice the GMX position router contract
+	IPositionRouter public gmxPositionRouter;
+	/// @notice the GMX Router contract
+	IRouter public router;
 
 	//////////////////////////
 	/// constant variables ///
@@ -68,18 +83,39 @@ contract GmxHedgingReactor is IHedgingReactor, AccessControl {
 	error IncorrectDeltaChange();
 	error InvalidTransactionNotEnoughMargin(int256 accountMarketValue, int256 totalRequiredMargin);
 
+	//////////////
+	/// events ///
+	//////////////
+
+	event CreateIncreasePosition(bytes32 positionKey);
+
+	///////////////
+	/// structs ///
+	///////////////
+
 	constructor(
+		address _gmxPositionRouter,
+		address _gmxRouter,
 		address _collateralAsset,
 		address _wethAddress,
 		address _parentLiquidityPool,
 		address _priceFeed,
 		address _authority
 	) AccessControl(IAuthority(_authority)) {
+		router = IRouter(_gmxRouter);
+		gmxPositionRouter = IPositionRouter(_gmxPositionRouter);
+		console.log(_gmxPositionRouter);
+		console.log(gmxPositionRouter.isLeverageEnabled());
+
+		router.approvePlugin(_gmxPositionRouter);
+		SafeTransferLib.safeApprove(ERC20(_collateralAsset), _gmxPositionRouter, MAX_UINT);
 		parentLiquidityPool = _parentLiquidityPool;
 		wETH = _wethAddress;
 		collateralAsset = _collateralAsset;
 		priceFeed = _priceFeed;
 	}
+
+	receive() external payable {}
 
 	///////////////
 	/// setters ///
@@ -101,6 +137,12 @@ contract GmxHedgingReactor is IHedgingReactor, AccessControl {
 	function setSyncOnChange(bool _syncOnChange) external {
 		_onlyGovernor();
 		syncOnChange = _syncOnChange;
+	}
+
+	function setPositionRouter(address _gmxPositionRouter) external {
+		_onlyGovernor();
+		gmxPositionRouter = IPositionRouter(_gmxPositionRouter);
+		router.approvePlugin(_gmxPositionRouter);
 	}
 
 	////////////////////////////////////////////
@@ -188,7 +230,69 @@ contract GmxHedgingReactor is IHedgingReactor, AccessControl {
         @return deltaChange The resulting difference in delta exposure
     */
 	function _changePosition(int256 _amount) internal returns (int256) {
+		if (_amount > 0) {
+			// enter long position
+			// if (internalDelta < 0) {
+			// 	// close short position before opening long
+			// }
+
+			uint256 amountIn = OptionsCompute.convertToDecimals(
+				(uint256(_amount).mul(getUnderlyingPrice(wETH, collateralAsset)) * healthFactor) / MAX_BIPS,
+				ERC20(collateralAsset).decimals()
+			);
+
+			SafeTransferLib.safeTransferFrom(collateralAsset, msg.sender, address(this), amountIn);
+			SafeTransferLib.safeApprove(ERC20(collateralAsset), address(router), amountIn);
+
+			address[] memory path = new address[](2);
+			path[0] = collateralAsset;
+			path[1] = wETH;
+			uint256 executionFee = gmxPositionRouter.minExecutionFee();
+			bytes32 positionKey = gmxPositionRouter.createIncreasePosition{ value: executionFee }(
+				path,
+				wETH,
+				amountIn,
+				amountIn.div(getUnderlyingPrice(wETH, collateralAsset)).mul(995e15),
+				uint256(_amount).mul(getUnderlyingPrice(wETH, collateralAsset)) * 1e12,
+				true,
+				getUnderlyingPrice(wETH, collateralAsset) * 1005e9, // mul by 1.005 e12 for slippage
+				executionFee,
+				"leverageisfun",
+				address(this)
+			);
+
+			emit CreateIncreasePosition(positionKey);
+			orderDeltaChange[positionKey] = _amount;
+		}
 		return 0;
+	}
+
+	function executeIncreasePosition(bytes32 positionKey) external {
+		gmxPositionRouter.executeIncreasePosition(positionKey, payable(address(this)));
+	}
+
+	function gmxPositionCallback(
+		bytes32 positionKey,
+		bool isExecuted,
+		bool isIncrease
+	) external {
+		if (isExecuted) {
+			internalDelta += orderDeltaChange[positionKey];
+		}
+	}
+
+	/**
+	 * @notice get the underlying price with just the underlying asset and strike asset
+	 * @param underlying   the asset that is used as the reference asset
+	 * @param _strikeAsset the asset that the underlying value is denominated in
+	 * @return the underlying price
+	 */
+	function getUnderlyingPrice(address underlying, address _strikeAsset)
+		internal
+		view
+		returns (uint256)
+	{
+		return PriceFeed(priceFeed).getNormalizedRate(underlying, _strikeAsset);
 	}
 
 	/// @dev keepers, managers or governors can access
