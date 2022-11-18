eb87a38e2cc3ae42b5c8a7b561963089de5ce3b3
ugolino <dan.ugolini@gmail.com>
2022-11-18 16:24:38 +0100

    fixed walletconnect

--- packages/front-end/src/App.tsx
+++ packages/front-end/src/App.tsx
@@ -50,6 +50,10 @@ import * as Fathom from "fathom-client";
 import { LegalDisclaimer } from "./components/LegalDisclaimer";
 import { MobileWarning } from "./components/MobileWarning";
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+window.Buffer = require('buffer/').Buffer;
+
+
 const walletConnect = walletConnectModule();
 const injectedWallets = injectedModule();
 
