9b30a88e634c8a5d437a73668dc13a657ebf9ba1
Josh <55187183+JoshDAO@users.noreply.github.com>
2022-12-22 14:36:27 +0000

    Arb sequencer check (#310)
    * create internal function to check sequencer status
    
    * create mock sequencer uptime feed
    
    * add tests for seuqencer down check
    
    * specify which PriceFeed contract on deployment and lint
    
    * add natspec and remove redundant memory var

--- packages/contracts/utils/alpha-system-deployer.ts
+++ packages/contracts/utils/alpha-system-deployer.ts
@@ -87,8 +87,13 @@ export async function deploySystem(
 	)) as OptionRegistry
 	const optionRegistry = _optionRegistry
 
-	const priceFeedFactory = await ethers.getContractFactory("PriceFeed")
-	const _priceFeed = (await priceFeedFactory.deploy(authority.address)) as PriceFeed
+	const sequencerUptimeFeedFactory = await ethers.getContractFactory("MockChainlinkSequencerFeed")
+	const sequencerUptimeFeed = await sequencerUptimeFeedFactory.deploy()
+	const priceFeedFactory = await ethers.getContractFactory("contracts/PriceFeed.sol:PriceFeed")
+	const _priceFeed = (await priceFeedFactory.deploy(
+		authority.address,
+		sequencerUptimeFeed.address
+	)) as PriceFeed
 	const priceFeed = _priceFeed
 	await priceFeed.addPriceFeed(ZERO_ADDRESS, usd.address, opynAggregator.address)
 	await priceFeed.addPriceFeed(weth.address, usd.address, opynAggregator.address)
