06a72391355706e0be6e3e3b28184a3dd2fad532
kjr217 <55159119+kjr217@users.noreply.github.com>
2023-01-02 11:55:19 +0000

    Feat/improve tests (#312)
    * feat: change vol feed to use forward price for sabr
    
    * chore: fix warnings
    
    * feat: fix foundry tests
    
    * feat: fix hardhat tests
    
    * feat: create otoken in exchange
    
    * fix: buyback bug
    
    * chore: fix sell and buy tests
    
    * chore: add in test suite checks
    
    * feat: bolter tests
    
    * feat: action tests and add collateral holding capabiliites
    
    * feat: solve double approve issue
    
    * chore: add test for full redeem flow
    
    * feat: interest rate fuzz test
    
    * feat: more tests and fee return var in pricer
    
    * feat: complete scenario test list
    
    * feat: add fee setup
    
    * feat: add fee logic and tests
    
    * chore: add todo
    
    * feat: add in fee edge case handling and close only option
    
    * feat: change fee to be capped
    
    * golf: remove redundant min checks

--- packages/contracts/contracts/libraries/OptionsCompute.sol
+++ packages/contracts/contracts/libraries/OptionsCompute.sol
@@ -195,4 +195,14 @@ library OptionsCompute {
 			optionSeries.isPut
 		);
 	}
+
+	function min(uint v1, uint v2) internal pure returns (uint) {
+		return v1 > v2 ? v2 : v1;
+	}
+	
+	function toInt256(uint256 value) internal pure returns (int256) {
+        // Note: Unsafe cast below is okay because `type(int256).max` is guaranteed to be positive
+        require(value <= uint256(type(int256).max), "SafeCast: value doesn't fit in an int256");
+        return int256(value);
+    }
 }
