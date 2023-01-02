6f16abbc41c5bfd6347a852d714de3e1a89c14ce
Josh <55187183+JoshDAO@users.noreply.github.com>
2023-01-02 14:07:20 +0000

    add new slippage variables and setters

--- packages/contracts/contracts/BeyondPricer.sol
+++ packages/contracts/contracts/BeyondPricer.sol
@@ -47,13 +47,22 @@ contract BeyondPricer is AccessControl, ReentrancyGuard {
 	/// dynamic variables ///
 	/////////////////////////
 
-
 	/////////////////////////////////////
 	/// governance settable variables ///
 	/////////////////////////////////////
 
 	uint256 bidAskIVSpread;
 	uint256 riskFreeRate;
+	uint256 slippageGradient;
+
+	// multiplier of slippageGradient for options < 10 delta
+	// reflects the cost of increased collateral used to back these kind of options relative to their price.
+	uint256 lowDeltaSlippageMultiplier;
+	// multiplier of slippageGradient for options between 10 and 25 delta
+	uint256 mediumDeltaSlippageMultiplier;
+	// multiplier of slippageGradient for options > 25 delta
+
+	uint256 highDeltaSlippageMultiplier;
 	//////////////////////////
 	/// constant variables ///
 	//////////////////////////
@@ -65,11 +74,11 @@ contract BeyondPricer is AccessControl, ReentrancyGuard {
 	/// structs && events ///
 	/////////////////////////
 
-
 	constructor(
 		address _authority,
 		address _protocol,
-		address _liquidityPool
+		address _liquidityPool,
+		uint256 _slippageGradient
 	) AccessControl(IAuthority(_authority)) {
 		protocol = Protocol(_protocol);
 		liquidityPool = ILiquidityPool(_liquidityPool);
@@ -82,22 +91,56 @@ contract BeyondPricer is AccessControl, ReentrancyGuard {
 	/// setters ///
 	///////////////
 
+	function setSlippageGradient(uint256 _slippageGradient) external {
+		_onlyGuardian();
+		slippageGradent = _slippageGradient;
+	}
+
+	function setLowDeltaSlippageMultiplier(uint256 _lowDeltaSlippageMultiplier) external {
+		_onlyGuardian();
+		lowDeltaSlippageMultiplier = _lowDeltaSlippageMultiplier;
+	}
+
+	function setMediumDeltaSlippageMultiplier(uint256 _mediumDeltaSlippageMultiplier) external {
+		_onlyGuardian();
+		mediumDeltaSlippageMultiplier = _mediumDeltaSlippageMultiplier;
+	}
+
+	function setHighDeltaSlippageMultiplier(uint256 _highDeltaSlippageMultiplier) external {
+		_onlyGuardian();
+		highDeltaSlippageMultiplier = _highDeltaSlippageMultiplier;
+	}
 
 	//////////////////////////////////////////////////////
 	/// access-controlled state changing functionality ///
 	//////////////////////////////////////////////////////
 
-
 	///////////////////////
 	/// complex getters ///
 	///////////////////////
 
-    function quoteOptionPrice(Types.OptionSeries memory _optionSeries, uint256 _amount, bool isSell) external view returns (uint256 totalPremium, int256 totalDelta) {
+	function quoteOptionPrice(
+		Types.OptionSeries memory _optionSeries,
+		uint256 _amount,
+		bool isSell
+	) external view returns (uint256 totalPremium, int256 totalDelta) {
 		uint256 underlyingPrice = _getUnderlyingPrice(underlyingAsset, strikeAsset);
-		uint256 iv = _getVolatilityFeed().getImpliedVolatility(_optionSeries.isPut, underlyingPrice,_optionSeries.strike, _optionSeries.expiration);
-		(uint256 premium, int256 delta) = OptionsCompute.quotePriceGreeks(_optionSeries, isSell, bidAskIVSpread, riskFreeRate, iv, underlyingPrice);
-        return (premium.mul(_amount) / 1e12, delta.mul(int256(_amount)));
-    }
+		uint256 iv = _getVolatilityFeed().getImpliedVolatility(
+			_optionSeries.isPut,
+			underlyingPrice,
+			_optionSeries.strike,
+			_optionSeries.expiration
+		);
+		(uint256 premium, int256 delta) = OptionsCompute.quotePriceGreeks(
+			_optionSeries,
+			isSell,
+			bidAskIVSpread,
+			riskFreeRate,
+			iv,
+			underlyingPrice
+		);
+		return (premium.mul(_amount) / 1e12, delta.mul(int256(_amount)));
+	}
 
 	///////////////////////////
 	/// non-complex getters ///
@@ -141,3 +184,15 @@ contract BeyondPricer is AccessControl, ReentrancyGuard {
 		return VolatilityFeed(protocol.volatilityFeed());
 	}
 }
+
+//////////////////////////
+/// internal functions ///
+//////////////////////////
+
+function _applySlippage(
+	Types.OptionSeries memory _optionSeries,
+	uint256 _amount,
+	bool isSell,
+	uint256 vanillaPremium,
+	int256 portfolioDelta
+) internal view returns (uint256 slippageMultiplier) {}
