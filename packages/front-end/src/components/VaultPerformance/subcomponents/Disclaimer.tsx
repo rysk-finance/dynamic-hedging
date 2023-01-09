9a93d3259abcd384f3a84600b73f112417bec985
Tim <timothy.dunphy@gmail.com>
2023-01-09 10:46:46 +0000

    More readable text length.

--- packages/front-end/src/components/VaultPerformance/subcomponents/Disclaimer.tsx
+++ packages/front-end/src/components/VaultPerformance/subcomponents/Disclaimer.tsx
@@ -2,7 +2,7 @@ import { DHV_NAME } from "src/config/constants";
 import { DISCORD_LINK } from "src/config/links";
 
 export const Disclaimer = () => (
-  <small className="pb-8 px-8 block w-400">
+  <small className="pb-8 px-8 block mx-[20%]">
     {`This chart shows the ${DHV_NAME} share price change since the third epoch (Alpha public launch). Data before this is the result of testing and is excluded. If you have any questions, feel free to `}
     <a href={DISCORD_LINK} target="_blank" rel="noopener noreferrer">
       {`reach out to us via our Discord.`}
