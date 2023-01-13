181bf76564c3ebb827151e7b3039b364de32e712
thegeronimo <thegeronimo@protonmail.com>
2023-01-13 15:44:17 -0500

    add RangeOrder Reactor deploy script
    use standard arbitrum rpc endpoints

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -138,14 +138,14 @@ module.exports = {
 			chainId: 1337
 		},
 		arbitrum: {
-			url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA}`,
+      url: 'https://arb1.arbitrum.io/rpc',
 			chainId: 42161,
 			saveDeployments: true,
 			accounts: process.env.MAINNET_PRIVATE_KEY ? [process.env.MAINNET_PRIVATE_KEY] : accounts,
 			gas: 500000000
 		},
 		arbitrumGoerli: {
-			url: `https://arbitrum-goerli.infura.io/v3/${process.env.INFURA}`,
+			url: `https://goerli-rollup.arbitrum.io/rpc`,
 			chainId: 421613,
 			saveDeployments: true,
 			accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : accounts,
