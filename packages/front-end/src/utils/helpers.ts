8f1df6749bf86a7b518b1e1a1f2f58f8bf2bb12d
Yassine <yassinesania@gmail.com>
2023-02-04 19:58:30 +0100

    fix(lint): Clean after rebase

--- packages/front-end/src/utils/helpers.ts
+++ packages/front-end/src/utils/helpers.ts
@@ -1,16 +1,8 @@
 import { BigNumber } from "ethers";
-import {
-  toWei,
-  genOptionTimeFromUnix,
-  fromWei,
-  tFormatEth,
-} from "./conversion-helper";
+import { toWei, genOptionTimeFromUnix, fromWei } from "./conversion-helper";
 import { LiquidityPool } from "../types/LiquidityPool";
-import { ERC20 } from "../types/ERC20";
-import { OptionRegistry } from "../types/OptionRegistry";
 import { PriceFeed } from "../types/PriceFeed";
 
-import bs from "black-scholes";
 import greeks from "greeks";
 import impliedVol from "implied-volatility";
 
@@ -60,57 +52,6 @@ export async function calculateOptionDeltaLocally(
   // TODO make sure this rounding is appropriate
   return toWei(parseFloat(localDelta.toString()).toFixed(5)).mul(
     amount.div(toWei("1"))
-  );
-}
-
-export async function getBlackScholesQuote(
-  liquidityPool: LiquidityPool,
-  optionRegistry: OptionRegistry,
-  collateralAsset: ERC20,
-  priceFeed: PriceFeed,
-  optionSeries: {
-    expiration: number;
-    strike: BigNumber;
-    isPut: boolean;
-    strikeAsset: string;
-    underlying: string;
-    collateral: string;
-  },
-  amount: BigNumber,
-  toBuy: boolean
-) {
-  const underlyingPrice = await priceFeed.getNormalizedRate(
-    optionSeries.underlying,
-    optionSeries.strikeAsset
-  );
-  const iv = await liquidityPool.getImpliedVolatility(
-    optionSeries.isPut,
-    underlyingPrice,
-    optionSeries.strike,
-    optionSeries.expiration
-  );
-  // const blockNum = await ethers.provider.getBlockNumber()
-  // const block = await ethers.provider.getBlock(blockNum)
-  // const { timestamp } = block
-
-  const timestamp = Math.round(+new Date() / 1000);
-  const timeToExpiration = genOptionTimeFromUnix(
-    Number(timestamp),
-    optionSeries.expiration
-  );
-
-  const priceNorm = fromWei(underlyingPrice);
-  const bidAskSpread = tFormatEth(await liquidityPool.bidAskIVSpread());
-
-  return (
-    bs.blackScholes(
-      priceNorm,
-      fromWei(optionSeries.strike),
-      timeToExpiration,
-      toBuy ? Number(fromWei(iv)) * (1 - Number(bidAskSpread)) : fromWei(iv),
-      parseFloat(rfr),
-      optionSeries.isPut ? "put" : "call"
-    ) * parseFloat(fromWei(amount))
   );
 }
 
