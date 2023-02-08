63812f3e5a912f65996fe4dbd1e29ce10d3a25ed
Tim <timothy.dunphy@gmail.com>
2023-02-08 11:12:34 +0000

    General refactoring, better typing, include netDhvExposure to calculations, check is option is buyable/sellable.

--- packages/front-end/src/utils/helpers.ts
+++ packages/front-end/src/utils/helpers.ts
@@ -1,93 +1,85 @@
-import { BigNumber } from "ethers";
-import { toWei, genOptionTimeFromUnix, fromWei } from "./conversion-helper";
-import { LiquidityPool } from "../types/LiquidityPool";
-import { PriceFeed } from "../types/PriceFeed";
+import type { ETHNetwork, ContractAddresses } from "src/types";
 
+import { getNetwork, readContract } from "@wagmi/core";
+import { BigNumber } from "ethers";
 import greeks from "greeks";
 import impliedVol from "implied-volatility";
 
+import addresses from "src/contracts.json";
+import { PriceFeedABI } from "src/abis/PriceFeed_ABI";
+import { LiquidityPoolABI } from "src/abis/LiquidityPool_ABI";
+import { fromWei, genOptionTimeFromUnix, toWei } from "./conversion-helper";
+
 const rfr = "0.03";
 
-export async function calculateOptionDeltaLocally(
-  liquidityPool: LiquidityPool,
-  priceFeed: PriceFeed,
+export const getContractAddress = (contractName: keyof ContractAddresses) => {
+  const { chain } = getNetwork();
+  const typedAddresses = addresses as Record<ETHNetwork, ContractAddresses>;
+  const network = (chain?.network as ETHNetwork) || (process.env.REACT_APP_NETWORK as ETHNetwork);
+
+  return typedAddresses[network][contractName] as HexString;
+};
+
+export const calculateOptionDeltaLocally = async (
   optionSeries: {
-    expiration: number;
+    expiration: BigNumber;
     isPut: boolean;
     strike: BigNumber;
-    strikeAsset: string;
-    underlying: string;
-    collateral: string;
+    strikeAsset: HexString;
+    underlying: HexString;
+    collateral: HexString;
   },
   amount: BigNumber,
   isShort: boolean
-) {
-  const priceQuote = await priceFeed.getNormalizedRate(
-    optionSeries.underlying,
-    optionSeries.strikeAsset
-  );
+): Promise<BigNumber> => {
+  const priceQuote = await readContract({
+    address: getContractAddress("priceFeed"),
+    abi: PriceFeedABI,
+    functionName: "getNormalizedRate",
+    args: [optionSeries.underlying, optionSeries.strikeAsset],
+  });
 
-  // const blockNum = await ethers.provider.getBlockNumber()
-  // const block = await ethers.provider.getBlock(blockNum)
-  // const { timestamp } = block
+  const vol = await readContract({
+    address: getContractAddress("liquidityPool"),
+    abi: LiquidityPoolABI,
+    functionName: "getImpliedVolatility",
+    args: [optionSeries.isPut, priceQuote, optionSeries.strike, optionSeries.expiration],
+  });
 
   const timestamp = Math.round(+new Date() / 1000);
-  const time = genOptionTimeFromUnix(timestamp, optionSeries.expiration);
-  const vol = await liquidityPool.getImpliedVolatility(
-    optionSeries.isPut,
-    priceQuote,
-    optionSeries.strike,
-    optionSeries.expiration
-  );
+  const time = genOptionTimeFromUnix(timestamp, optionSeries.expiration.toNumber());
+
   const opType = optionSeries.isPut ? "put" : "call";
-  let localDelta = greeks.getDelta(
-    fromWei(priceQuote),
-    fromWei(optionSeries.strike),
-    time,
-    fromWei(vol),
-    rfr,
-    opType
-  );
+  let localDelta = greeks.getDelta(fromWei(priceQuote), fromWei(optionSeries.strike), time, fromWei(vol), rfr, opType);
   localDelta = isShort ? -localDelta : localDelta;
   // TODO make sure this rounding is appropriate
-  return toWei(parseFloat(localDelta.toString()).toFixed(5)).mul(
-    amount.div(toWei("1"))
-  );
-}
+  return toWei(parseFloat(localDelta.toString()).toFixed(5)).mul(amount.div(toWei("1")));
+};
 
-export async function returnIVFromQuote(
+export const returnIVFromQuote = async (
   quote: number,
-  priceFeed: PriceFeed,
   optionSeries: {
-    expiration: number;
+    expiration: BigNumber;
     strike: BigNumber;
     isPut: boolean;
-    strikeAsset: string;
-    underlying: string;
-    collateral: string;
+    strikeAsset: HexString;
+    underlying: HexString;
+    collateral: HexString;
   }
-) {
-  const underlyingPrice = await priceFeed.getNormalizedRate(
-    optionSeries.underlying,
-    optionSeries.strikeAsset
-  );
+): Promise<number> => {
+  const underlyingPrice = await readContract({
+    address: getContractAddress("priceFeed"),
+    abi: PriceFeedABI,
+    functionName: "getNormalizedRate",
+    args: [optionSeries.underlying, optionSeries.strikeAsset],
+  });
 
   const priceNorm = fromWei(underlyingPrice);
 
   const timestamp = Math.round(+new Date() / 1000);
-  const timeToExpiration = genOptionTimeFromUnix(
-    Number(timestamp),
-    optionSeries.expiration
-  );
+  const timeToExpiration = genOptionTimeFromUnix(Number(timestamp), optionSeries.expiration.toNumber());
 
   const type = optionSeries.isPut ? "put" : "call";
 
-  return impliedVol.getImpliedVolatility(
-    quote,
-    priceNorm,
-    fromWei(optionSeries.strike),
-    timeToExpiration,
-    rfr,
-    type
-  );
-}
+  return impliedVol.getImpliedVolatility(quote, priceNorm, fromWei(optionSeries.strike), timeToExpiration, rfr, type);
+};
