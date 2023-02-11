3686dab5f524281d545cce658a1e402a9350003d
Josh <55187183+JoshDAO@users.noreply.github.com>
2023-02-12 09:30:54 +1100

    Feat/add gmx reactor event (#393)
    * add event emission on successful order execution
    
    * update test suite to check for event:
    
    * add draft test for failed execution
    
    * feat: make event trigger
    
    * feat: fix test and fix accounting bug
    
    * chore: fix import for ci
    
    ---------
    
    Co-authored-by: kjr217 <kjraja217@gmail.com>

--- packages/contracts/contracts/interfaces/IPositionRouter.sol
+++ packages/contracts/contracts/interfaces/IPositionRouter.sol
@@ -36,6 +36,12 @@ interface IPositionRouter {
 	function executeDecreasePosition(bytes32 _key, address payable _executionFeeReceiver)
 		external
 		returns (bool);
+	
+	function executeIncreasePositions(uint256, address payable) external;
+
+	function setPositionKeeper(address, bool) external;
+
+	function admin() external view returns (address);
 
 	function minExecutionFee() external view returns (uint256);
 
