b1b8116b7e84ddf6b85e98fb6c270bfe9f336552
Yassine <yassinesania@gmail.com>
2023-02-13 12:20:03 +0100

    feat(hooks): useOraclePrice
    Fetch asset price from opynOracle contract
    
    feat(hooks): useApproveTransfer
    
    Allows user to approve a certain amount to exchange address, plus
    showing allowance and if enough is approved
    
    feat(hooks): useMarginRequirement
    
    Allows to retrieve collateral requirement for selling option
    
    feat(hooks): useSellOperate
    
    Hook to mint and sell option to DHV

---
+++ packages/front-end/src/hooks/useSellOperate/index.tsx
@@ -0,0 +1,133 @@
+import { usePrepareContractWrite, useContractWrite, useAccount } from "wagmi";
+import { EMPTY_SERIES, ZERO_ADDRESS } from "../../config/constants";
+import { ETHNetwork, OptionSeries } from "../../types";
+import addresses from "../../contracts.json";
+import { getNetwork } from "@wagmi/core";
+import OptionExchangeABI from "../../abis/OptionExchange.json";
+import { AbiCoder } from "ethers/lib/utils";
+import { useState } from "react";
+import { BigNumber } from "ethers";
+
+const abiCode = new AbiCoder();
+
+const useSellOperate = (): [
+  ((overrideConfig?: undefined) => void) | undefined,
+  (value: BigNumber) => void,
+  (value: BigNumber) => void,
+  (value: Partial<OptionSeries>) => void
+] => {
+  // Global state
+  // TODO - this is repetitive code
+  const { chain } = getNetwork();
+  const { address } = useAccount();
+  const network = chain?.network as ETHNetwork;
+
+  const exchangeAddress = addresses[network].optionExchange;
+  const collateral = addresses[network].USDC;
+  const strike = addresses[network].USDC;
+  const underlying = addresses[network].WETH;
+
+  // Internal state
+  // note - set by user and it's the collateral they want to put down
+  const [margin, setMargin] = useState<BigNumber>(BigNumber.from("0"));
+  // note - amount of otokens to be minted
+  const [amount, setAmount] = useState<BigNumber>(BigNumber.from("0"));
+  // note - partial option series as assets are hardcoded above
+  const [optionSeries, setOptionSeries] = useState<Partial<OptionSeries>>();
+
+  // Setters
+  const updateMargin = (amount: BigNumber) => {
+    setMargin(amount);
+  };
+  const updateAmount = (amount: BigNumber) => {
+    setAmount(amount);
+  };
+  const updateOptionSeries = (optionSeries: Partial<OptionSeries>) => {
+    setOptionSeries(optionSeries);
+  };
+
+  // Contract write
+  const { config } = usePrepareContractWrite({
+    address: exchangeAddress as `0x${string}`, // TODO update after rebasing Tim's branch
+    abi: OptionExchangeABI,
+    functionName: "operate",
+    args: [
+      [
+        {
+          operation: 0, // 0 means opyn operation
+          operationQueue: [
+            {
+              actionType: 0, // 0 on an opyn operation means Open Vault which is represented by a vaultId
+              owner: address,
+              secondAddress: address,
+              asset: ZERO_ADDRESS,
+              vaultId: 1, // vaultId, // TODO vaultId, each short position the user holds will be held in a unique vaultId, we need to find an easy way to retrieve this
+              amount: BigNumber.from("0"),
+              optionSeries: EMPTY_SERIES,
+              index: 0,
+              data: abiCode.encode(["uint256"], [1]), // 1 here represents partially collateralised, 0 represents fully collateralised
+            },
+            {
+              actionType: 5, // 5 represents a Deposit Collateral action
+              owner: address,
+              secondAddress: exchangeAddress, // this can be set as the senderAddress or exchange address, if set to the exchange address then the user approval goes to the exchange, if set to the sender address then the user approval goes to the opyn margin pool
+              asset: collateral, // TODO proposedSeries.collateral
+              vaultId: 1, // vaultId, // TODO vault id to deposit collateral into
+              amount: margin,
+              optionSeries: EMPTY_SERIES,
+              index: 0,
+              data: ZERO_ADDRESS,
+            },
+            {
+              actionType: 1, // 1 represents a mint otoken operation (minting an option contract, this only works if there is enough collateral)
+              owner: address,
+              secondAddress: exchangeAddress, // most of the time this should be set to exchange address, this helps avoid an extra approval from the user on the otoken when selling to the dhv
+              asset: ZERO_ADDRESS, // TODO this should be the oToken that is being sold address
+              vaultId: 1, // TODO vaultId,
+              amount: amount, // amount needs to be in e8 decimals
+              optionSeries: EMPTY_SERIES,
+              index: 0,
+              data: ZERO_ADDRESS,
+            },
+          ],
+        },
+        {
+          operation: 1, // indicates a rysk operation
+          operationQueue: [
+            {
+              actionType: 2, // this is a sell action
+              owner: ZERO_ADDRESS,
+              secondAddress: address,
+              asset: ZERO_ADDRESS,
+              vaultId: 0, // i guess all dhv options are in this vault
+              amount: amount.mul(BigNumber.from("10000000000")).toString(), // amount needs to be in e18 decimals
+              optionSeries: {
+                ...optionSeries,
+                strikeAsset: strike,
+                collateral,
+                underlying,
+              },
+              index: 0,
+              data: "0x",
+            },
+          ],
+        },
+      ],
+    ],
+    enabled: optionSeries && amount.gt("0") && margin.gt("0"),
+    overrides: {
+      gasLimit: BigNumber.from("2500000"),
+    },
+  });
+
+  const { write } = useContractWrite(config);
+
+  return [
+    write, // send operate to mint and sell oToken
+    updateMargin, // user defined collateral for oToken
+    updateAmount, // amount of options to be minted
+    updateOptionSeries, // option series to be sold
+  ];
+};
+
+export default useSellOperate;
