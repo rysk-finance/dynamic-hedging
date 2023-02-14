03a5e67d8ee307219f227f4ae7749fd029a6736a
Tim <timothy.dunphy@gmail.com>
2023-02-14 10:30:28 +0000

    New date picker component for paginating available expiries.

---
+++ packages/front-end/src/components/optionsTrading/ExpiryDatePicker/components/ArrowButton.tsx
@@ -0,0 +1,13 @@
+import type { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+
+export const ArrowButton = (
+  props: DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >
+) => (
+  <button
+    {...props}
+    className="flex justify-center py-2 enabled:hover:bg-bone-dark ease-in-out duration-100 disabled:opacity-40"
+  />
+);
