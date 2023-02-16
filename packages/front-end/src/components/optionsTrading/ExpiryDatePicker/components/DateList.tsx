be6a2194b140ed04e7968cc1649c6ef48700e646
Tim <timothy.dunphy@gmail.com>
2023-02-16 13:27:50 +0000

    Show positions for the user on the expiry dates. Currently only works for longs, awaiting graph updates to get short position data.

--- packages/front-end/src/components/optionsTrading/ExpiryDatePicker/components/DateList.tsx
+++ packages/front-end/src/components/optionsTrading/ExpiryDatePicker/components/DateList.tsx
@@ -1,21 +1,53 @@
-import type { DateListProps } from "../types";
+import type { DateListProps, UserExpiryStatusNoTimestamp } from "../types";
 
 import dayjs from "dayjs";
 import { AnimatePresence, motion } from "framer-motion";
 
 import FadeInOutFixedDelay from "src/animation/FadeInOutFixedDelay";
+import { DownChevron, UpChevron } from "src/Icons";
+
+const getTitle = (hasBalance?: UserExpiryStatusNoTimestamp) => {
+  switch (true) {
+    case hasBalance?.isLong && hasBalance?.isShort:
+      return "You have open positions for this expiry.";
+
+    case hasBalance?.isLong:
+      return "You have open long positions for this expiry.";
+
+    case hasBalance?.isShort:
+      return "You have open short positions for this expiry.";
+
+    default:
+      return "";
+  }
+};
 
 export const DateList = ({
   expiryDates,
   visibleRange,
   expiryDate,
   handleExpirySelection,
+  balances,
 }: DateListProps) => (
   <ol className="grid grid-cols-4 col-span-10">
     <AnimatePresence initial={false} mode="popLayout">
       {expiryDates.map((timestamp, index) => {
         const datetime = dayjs.unix(timestamp);
         const [min, max] = visibleRange;
+        const hasBalance = balances?.reduce(
+          (acc, { isLong, isShort, timestamp }) => {
+            if (timestamp === datetime.unix()) {
+              acc.isLong = !acc.isLong ? isLong : acc.isLong;
+              acc.isShort = !acc.isShort ? isShort : acc.isShort;
+            }
+
+            return acc;
+          },
+          {
+            isLong: false,
+            isShort: false,
+          } as UserExpiryStatusNoTimestamp
+        );
 
         if (index >= min && index <= max) {
           return (
@@ -27,10 +59,27 @@ export const DateList = ({
               onClick={handleExpirySelection(timestamp)}
               {...FadeInOutFixedDelay}
             >
-              <button className="relative w-full py-3">
-                <time dateTime={datetime.format("YYYY-MM-DD")}>
+              <button
+                className="flex items-center justify-center w-full py-3"
+                title={getTitle(hasBalance)}
+              >
+                <UpChevron
+                  className={`min-w-6 h-6 stroke-green-500 ${
+                    !hasBalance?.isLong && "opacity-0"
+                  }`}
+                  strokeWidth={4}
+                />
+
+                <time className="mx-4" dateTime={datetime.format("YYYY-MM-DD")}>
                   {`${datetime.format("MMM DD")}`}
                 </time>
+
+                <DownChevron
+                  className={`min-w-6 h-6 stroke-red-500 ${
+                    !hasBalance?.isShort && "opacity-0"
+                  }`}
+                  strokeWidth={4}
+                />
               </button>
             </motion.li>
           );
--- packages/front-end/src/components/optionsTrading/ExpiryDatePicker/hooks/useExpiryDates.tsx
+++ packages/front-end/src/components/optionsTrading/ExpiryDatePicker/hooks/useExpiryDates.tsx
@@ -1,30 +1,42 @@
 import type { BigNumber } from "ethers";
-import type { ExpiryDateList, VisibleRange } from "../types";
 
+import type {
+  ExpiryDateList,
+  UserExpiryStatus,
+  UserPositions,
+  VisibleRange
+} from "../types";
+
+import { gql, useQuery } from "@apollo/client";
 import { captureException } from "@sentry/react";
 import { readContract } from "@wagmi/core";
 import dayjs from "dayjs";
+import { useAccount } from "wagmi";
 
 import { useCallback, useEffect, useState } from "react";
 
 import { OptionCatalogueABI } from "src/abis/OptionCatalogue_ABI";
-import { getContractAddress } from "src/utils/helpers";
 import { useOptionsTradingContext } from "src/state/OptionsTradingContext";
 import { OptionsTradingActionType } from "src/state/types";
+import { getContractAddress } from "src/utils/helpers";
 
 /**
  * Hook to fetch all expiry dates from the option catalogue and parse them into a list of unix time stamps.
  * Currently selected expiry date is set into the options trading context.
+ * Also queries the graph for the users balances - currently only works for long positions.
  *
  * @property expiryDate - The currently selected unix time stamp.
  * @property expiryDates - A list of all available unix time stamps.
  * @property visibleRange - A tuple representing the visible index range of the expiryDates list.
  * @property handleExpirySelection - Handler function for selecting an option expiry.
  * @property scrollExpiries - A function to move the visible index of expiry dates.
+ * @property balances - A dict of the user balances, displaying if they are long/short and their expiry.
  *
  * @returns [number | null, ExpiryDateList, VisibleRange, (date: number) => () => void, (direction: 1 | -1) => () => void]
  */
 export const useExpiryDates = () => {
+  const { address } = useAccount();
+
   const {
     state: { expiryDate },
     dispatch,
@@ -58,8 +70,8 @@ export const useExpiryDates = () => {
         });
 
         if (expiries) {
-          const expiryTimestamps = expiries.reduce(
-            (expiryDates: ExpiryDateList, expiration: BigNumber) => {
+          const expiryTimestamps = expiries
+            .reduce((expiryDates: ExpiryDateList, expiration: BigNumber) => {
               const now = dayjs().unix();
               const timestamp = expiration.toNumber();
 
@@ -68,9 +80,8 @@ export const useExpiryDates = () => {
               }
 
               return expiryDates;
-            },
-            []
-          );
+            }, [])
+            .sort((a, b) => a - b);
 
           setExpiryDates(expiryTimestamps);
           setExpiryDate(expiryTimestamps[0]);
@@ -85,11 +96,47 @@ export const useExpiryDates = () => {
     fetchExpirations();
   }, [setExpiryDate]);
 
+  const { data } = useQuery<UserPositions>(
+    gql`
+      query ($address: String) {
+        account(id: $address) {
+          balances {
+            token {
+              expiryTimestamp
+            }
+            balance
+          }
+        }
+      }
+    `,
+    {
+      onError: (err) => {
+        captureException(err);
+        console.error(err);
+      },
+      variables: {
+        address: address?.toLowerCase(),
+      },
+      skip: !address,
+    }
+  );
+
+  const balances: UserExpiryStatus[] | undefined = data?.account?.balances.map(
+    ({ balance, token }) => {
+      return {
+        isLong: Number(balance) > 0,
+        isShort: Number(balance) < 0,
+        timestamp: Number(token.expiryTimestamp),
+      };
+    }
+  );
+
   return [
     expiryDate,
     expiryDates,
     visibleRange,
     handleExpirySelection,
     scrollExpiries,
+    balances,
   ] as const;
 };
