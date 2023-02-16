af7cf24ecd28821e796e63da8df5fcc0ce9d642c
Tim <timothy.dunphy@gmail.com>
2023-02-16 13:27:50 +0000

    Show positions for the user on the expiry dates. Currently only works for longs, awaiting graph updates to get short position data.

--- packages/front-end/src/components/optionsTrading/ExpiryDatePicker/types.ts
+++ packages/front-end/src/components/optionsTrading/ExpiryDatePicker/types.ts
@@ -1,9 +1,32 @@
 export type ExpiryDateList = number[];
 export type VisibleRange = [number, number];
 
+export interface UserPositions {
+  account: {
+    __typename: string;
+    balances: {
+      balance: string;
+      token: {
+        expiryTimestamp: string;
+        __typename: string;
+      };
+      __typename: string;
+    }[];
+  };
+}
+
+export interface UserExpiryStatus {
+  isLong: boolean;
+  isShort: boolean;
+  timestamp: number;
+}
+
+export type UserExpiryStatusNoTimestamp = Omit<UserExpiryStatus, "timestamp">;
+
 export interface DateListProps {
   expiryDates: ExpiryDateList;
   visibleRange: VisibleRange;
   expiryDate: number | null;
   handleExpirySelection: (date: number) => VoidFunction;
+  balances?: UserExpiryStatus[];
 }
