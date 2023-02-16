ef7c6c9ab3204f7826eaf1ff8991e51c71366d18
Tim <timothy.dunphy@gmail.com>
2023-02-16 13:56:25 +0000

    Clean up semantics.

--- packages/front-end/src/components/optionsTrading/OptionsTradingContent.tsx
+++ packages/front-end/src/components/optionsTrading/OptionsTradingContent.tsx
@@ -5,29 +5,15 @@ import { AssetPriceInfo } from "./AssetPriceInfo";
 
 export const OptionsTradingContent = () => {
   return (
-    <>
-      <div className="col-start-1 col-end-17 -mt-16">
-        <AssetPriceInfo />
-        <div className="border-2 border-black">
-          <div className="w-full">
-            <div className="">
-              <ExpiryDatePicker />
-            </div>
-          </div>
-          <div className="flex grow items-stretch">
-            <div className="grow flex-col rounded-br-lg">
-              <div className="grow border-black rounded-br-lg">
-                <OptionsTable />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="grow rounded-br-lg flex flex-col">
-          <div>
-            <Purchase />
-          </div>
-        </div>
+    <section className="col-start-1 col-end-17 -mt-16">
+      <AssetPriceInfo />
+
+      <div className="relative border-2 border-black">
+        <ExpiryDatePicker />
+        <OptionsTable />
       </div>
-    </>
+
+      <Purchase />
+    </section>
   );
 };
