9e24b30cdac14a62db3d15d86521d4fe8ac804e2
Josh <55187183+JoshDAO@users.noreply.github.com>
2023-02-17 11:53:38 +1100

    write gelato multicall contract

---
+++ packages/contracts/infrastructure/gelato/VaultCollateralMulticall.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.9;
+
+import "../../contracts/OptionRegistry.sol";
+import "../../contracts/packages/opyn/core/Controller.sol";
+
+contract VaultCollateralMulticall {
+	address executorAddress;
+	OptionRegistry optionRegistry;
+
+	error invalidMsgSender();
+
+	constructor(address _executorAddress, address _optionRegistry) {
+		executorAddress = _executorAddress;
+		optionRegistry = OptionRegistry(_optionRegistry);
+	}
+
+	function adjustVaults(uint256[] vaultIds) external {
+		if (msg.sender != executorAddress) {
+			revert invalidMsgSender();
+		}
+
+		uint256 i = 0;
+		for (i; i < vaultIds.length; i++) {
+			try optionRegistry.adjustCollateral(vaultIds[i]) {} catch {}
+		}
+	}
+}
--- packages/contracts/infrastructure/gelato/VaultCollateralResolver.sol
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.9;
-
-import "../../contracts/OptionRegistry.sol";
-import "../../contracts/packages/opyn/core/Controller.sol";
