f101ed7eb31d507e4c8f912acf4ca400f900cb25
Yassine <yassinesania@gmail.com>
2023-02-21 13:01:30 +0100

    feat(hooks): Get token address with static call
    This is the token address that needs to be passed in case of shorting.

---
+++ packages/front-end/src/hooks/useOToken/index.tsx
@@ -0,0 +1,46 @@
+import { useProvider } from "wagmi";
+import { getContractAddress } from "../../utils/helpers";
+import { OptionExchangeABI } from "../../abis/OptionExchange_ABI";
+import { BigNumber, ethers } from "ethers";
+
+const useOToken = () => {
+  // Global state
+  const provider = useProvider();
+
+  // Addresses
+  const optionExchangeAddress = getContractAddress("optionExchange");
+  const usdcAddress = getContractAddress("USDC");
+  const wethAddress = getContractAddress("WETH");
+  const strikeAsset = usdcAddress;
+  const underlying = wethAddress;
+  const collateral = usdcAddress;
+
+  // Contracts
+  const optionExchange = new ethers.Contract(
+    optionExchangeAddress,
+    OptionExchangeABI,
+    provider
+  );
+
+  // Contract read
+  const getOToken = async (
+    expiration: string,
+    strike: BigNumber,
+    isPut: boolean
+  ) => {
+    return optionExchange.callStatic.createOtoken({
+      strikeAsset,
+      collateral,
+      underlying,
+      expiration: expiration,
+      strike: strike,
+      isPut: isPut,
+    });
+  };
+
+  return [
+    getOToken, // retrieve oToken address
+  ];
+};
+
+export default useOToken;
