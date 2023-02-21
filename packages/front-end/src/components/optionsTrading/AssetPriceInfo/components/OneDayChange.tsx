cee98f75db246c2cceafe667dbc1bd0b7e639abe
Tim <timothy.dunphy@gmail.com>
2023-02-21 14:02:31 +0000

    Add count-up and block height.

--- packages/front-end/src/components/optionsTrading/AssetPriceInfo/components/OneDayChange.tsx
+++ packages/front-end/src/components/optionsTrading/AssetPriceInfo/components/OneDayChange.tsx
@@ -1,5 +1,10 @@
+import type { RefObject } from "react";
+
 import type { OneDayChangeProps } from "../types";
 
+import CountUp from "react-countup";
+
+import { easeOutCubic } from "src/animation/easing";
 import { toTwoDecimalPlaces as round } from "src/utils/rounding";
 
 export const OneDayChange = ({ low, change, high }: OneDayChangeProps) => {
@@ -8,7 +13,6 @@ export const OneDayChange = ({ low, change, high }: OneDayChangeProps) => {
   const barEndColorPosition =
     change < 0 ? "bg-red-900 mr-auto" : "bg-green-900 ml-auto";
   const widthChunk = change * (50 / (Math.ceil(Math.abs(change) / 5) * 5));
-  
 
   return (
     <div className="flex flex-col justify-center w-1/3 px-4">
@@ -19,7 +23,22 @@ export const OneDayChange = ({ low, change, high }: OneDayChangeProps) => {
 
       <div className="flex justify-between font-dm-mono">
         <p className="text-red-500">{`$ ${low}`}</p>
-        <p className="text-gray-600">{`${round(change)} %`}</p>
+        <CountUp
+          decimals={2}
+          duration={0.3}
+          easingFn={easeOutCubic}
+          end={round(change) || 0}
+          preserveValue
+          suffix=" %"
+          useEasing
+        >
+          {({ countUpRef }) => (
+            <p
+              className="text-gray-600"
+              ref={countUpRef as RefObject<HTMLHeadingElement>}
+            />
+          )}
+        </CountUp>
         <p className="text-green-500">{`$ ${high}`}</p>
       </div>
 
