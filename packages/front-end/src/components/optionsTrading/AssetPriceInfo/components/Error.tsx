80f3d8e1a8e658db03e60b6c83c8ce20cdd000f3
Tim <timothy.dunphy@gmail.com>
2023-02-21 13:59:57 +0000

    Better semantics and media queries.

--- packages/front-end/src/components/optionsTrading/AssetPriceInfo/components/Error.tsx
+++ packages/front-end/src/components/optionsTrading/AssetPriceInfo/components/Error.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import FadeInOutFixedDelay from "src/animation/FadeInOutFixedDelay";
 
 export const Error = () => (
-  <motion.div className="w-full m-auto" {...FadeInOutFixedDelay}>
+  <motion.em className="w-full m-auto not-italic px-8" {...FadeInOutFixedDelay}>
     {`We're having some trouble fetching Ethereum price data. Please hold tight and we should have it soon!`}
-  </motion.div>
+  </motion.em>
 );
