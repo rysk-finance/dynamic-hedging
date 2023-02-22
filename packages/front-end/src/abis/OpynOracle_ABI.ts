1650405167d20b399b58fd44dc3530ebf8519c04
Yassine <yassinesania@gmail.com>
2023-02-22 14:42:49 +0100

    fix(review): Tim
    No functional changes
    
    - creating ts files for ABIs
    - Adjust WAGMI param
    types
    - useContractRead for useMarginRequirement

---
+++ packages/front-end/src/abis/OpynOracle_ABI.ts
@@ -0,0 +1,21 @@
+export const OpynOracleABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_asset",
+        "type": "address"
+      }
+    ],
+    "name": "getPrice",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+] as const;
