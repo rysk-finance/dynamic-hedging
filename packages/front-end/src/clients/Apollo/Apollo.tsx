d8dda8280b00cec816d27c321bebefb80b11425c
Tim <timothy.dunphy@gmail.com>
2023-03-03 14:51:53 +0000

    Extract Apollo client to allow exporting.

--- packages/front-end/src/clients/Apollo/Apollo.tsx
+++ packages/front-end/src/clients/Apollo/Apollo.tsx
@@ -8,50 +8,50 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 
-const ApolloProvider = ({ children }: PropsWithChildren<unknown>) => {
-  const ryskSubgraph = new HttpLink({
-    uri: process.env.REACT_APP_SUBGRAPH_URL,
-  });
+const ryskSubgraph = new HttpLink({
+  uri: process.env.REACT_APP_SUBGRAPH_URL,
+});
 
-  const opynSubgraph = new HttpLink({
-    uri: process.env.REACT_APP_OPYN_SUBGRAPH_URL,
-  });
+const opynSubgraph = new HttpLink({
+  uri: process.env.REACT_APP_OPYN_SUBGRAPH_URL,
+});
 
-  const client = new ApolloClient({
-    link: ApolloLink.split(
-      (operation) => operation.getContext().clientName === "opyn",
-      opynSubgraph,
-      ryskSubgraph
-    ),
-    cache: new InMemoryCache({
-      typePolicies: {
-        Query: {
-          fields: {
-            writeOptionsActions: {
-              keyArgs: false,
-              merge(existing = [], incoming) {
-                return [...existing, ...incoming];
-              },
+export const RyskApolloClient = new ApolloClient({
+  link: ApolloLink.split(
+    (operation) => operation.getContext().clientName === "opyn",
+    opynSubgraph,
+    ryskSubgraph
+  ),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          writeOptionsActions: {
+            keyArgs: false,
+            merge(existing = [], incoming) {
+              return [...existing, ...incoming];
             },
-            buybackOptionActions: {
-              keyArgs: false,
-              merge(existing = [], incoming) {
-                return [...existing, ...incoming];
-              },
+          },
+          buybackOptionActions: {
+            keyArgs: false,
+            merge(existing = [], incoming) {
+              return [...existing, ...incoming];
             },
-            rebalanceDeltaActions: {
-              keyArgs: false,
-              merge(existing = [], incoming) {
-                return [...existing, ...incoming];
-              },
+          },
+          rebalanceDeltaActions: {
+            keyArgs: false,
+            merge(existing = [], incoming) {
+              return [...existing, ...incoming];
             },
           },
         },
       },
-    }),
-  });
+    },
+  }),
+});
 
-  return <Provider client={client}>{children}</Provider>;
+const ApolloProvider = ({ children }: PropsWithChildren<unknown>) => {
+  return <Provider client={RyskApolloClient}>{children}</Provider>;
 };
 
 export default ApolloProvider;
