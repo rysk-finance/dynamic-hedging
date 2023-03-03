800b85aa802b93b92b1e2db556d9558afc41448d
Tim <timothy.dunphy@gmail.com>
2023-03-03 15:09:53 +0000

    Extract core functionality to allow for vanilla function and hook form.

--- packages/front-end/src/hooks/useTenderlySimulator/index.tsx
+++ packages/front-end/src/hooks/useTenderlySimulator/index.tsx
@@ -8,6 +8,41 @@ const TENDERLY_USER = process.env.REACT_APP_TENDERLY_USER;
 const TENDERLY_PROJECT = process.env.REACT_APP_TENDERLY_PROJECT;
 const TENDERLY_ACCESS_KEY = process.env.REACT_APP_TENDERLY_ACCESS_KEY;
 
+export const fetchSimulation = async (
+  from: string = "0x",
+  to: string = "0x",
+  input: string = "0x",
+  gas: number = 0,
+  gas_price: number = 0,
+  value: number = 0
+): Promise<SimulationResponse> => {
+  const response = await fetch(
+    `${TENDERLY_API}/${TENDERLY_USER}/project/${TENDERLY_PROJECT}/simulate`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        /* Simulation Configuration */
+        save: true, // if true simulation is saved and shows up in the dashboard
+        save_if_fails: true, // if true, reverting simulations show up in the dashboard
+        simulation_type: "full", // full or quick (full is default)
+        network_id: process.env.REACT_APP_CHAIN_ID, // network to simulate on
+        /* Standard EVM Transaction object */
+        from,
+        to,
+        input,
+        gas,
+        gas_price,
+        value,
+      }),
+      headers: {
+        "X-Access-Key": TENDERLY_ACCESS_KEY as string,
+      },
+    }
+  );
+
+  return await response.json();
+};
+
 const useTenderlySimulator = ({ to }: UseTenderlySimulatorConfig) => {
   // global state
   const { address } = useAccount();
@@ -18,37 +53,13 @@ const useTenderlySimulator = ({ to }: UseTenderlySimulatorConfig) => {
 
   const simulate = async (
     data: string,
-    gas: number,
-    gasPrice: number,
-    value: number
+    gas: number = 0,
+    gasPrice: number = 0,
+    value: number = 0
   ): Promise<SimulationResponse | void> => {
     setLoading(true);
     try {
-      const response = await fetch(
-        `${TENDERLY_API}/${TENDERLY_USER}/project/${TENDERLY_PROJECT}/simulate`,
-        {
-          method: "POST",
-          body: JSON.stringify({
-            /* Simulation Configuration */
-            save: true, // if true simulation is saved and shows up in the dashboard
-            save_if_fails: true, // if true, reverting simulations show up in the dashboard
-            simulation_type: "full", // full or quick (full is default)
-            network_id: process.env.REACT_APP_CHAIN_ID, // network to simulate on
-            /* Standard EVM Transaction object */
-            from: address,
-            to: to,
-            input: data,
-            gas: gas,
-            gas_price: gasPrice,
-            value: value,
-          }),
-          headers: {
-            "X-Access-Key": TENDERLY_ACCESS_KEY as string,
-          },
-        }
-      );
-
-      return await response.json();
+      return fetchSimulation(address, to, data, gas, gasPrice, value);
     } catch (e: any) {
       captureException(e);
       setError(e.message);
