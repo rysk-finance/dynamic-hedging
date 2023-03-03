9c8057c5ed014a049c0ea886e8aefa702e4a8403
Tim <timothy.dunphy@gmail.com>
2023-03-03 15:09:24 +0000

    New icons.

--- packages/front-end/src/Icons/index.ts
+++ packages/front-end/src/Icons/index.ts
@@ -1,7 +1,17 @@
+import { Close } from "./Close";
 import { DownChevron } from "./DownChevron";
 import { LeftArrow } from "./LeftArrow";
+import { Loading } from "./Loading";
 import { RightArrow } from "./RightArrow";
 import { RyskFavicon } from "./RyskFavicon";
 import { UpChevron } from "./UpChevron";
 
-export { DownChevron, LeftArrow, RightArrow, RyskFavicon, UpChevron };
+export {
+  Close,
+  DownChevron,
+  LeftArrow,
+  Loading,
+  RightArrow,
+  RyskFavicon,
+  UpChevron,
+};
