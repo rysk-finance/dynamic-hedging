20c6310b3c4dd2ce7498496e7388c0528e6f5b02
Josh <55187183+JoshDAO@users.noreply.github.com>
2023-03-07 22:05:55 +1100

    write gelato multicall contract (#416)
    * resolve rebase conflicts
    
    * Revert "resolve rebase conflicts"
    
    This reverts commit b54a58c3adb5e1a3f951ee5a6e5269c543e7a5d5.
    
    * write gelato multicall contract
    
    * add test suite for multicall
    
    * optimise for loop for gas
    
    * check LP balance in test suite'
    
    * add multicall for checkVaultHealth
    
    * add deploy script for multicall contract
    
    * add setter for executor address
    
    * add arbitrum to hardhat-etherscan config
    
    * add hardhat-run script to package.json
    
    * build and test gelato resolver for opyn pricer
    
    * fix test name

--- packages/contracts/contracts/interfaces/AggregatorV3Interface.sol
+++ packages/contracts/contracts/interfaces/AggregatorV3Interface.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.6.0;
+pragma solidity >=0.8.0;
 
 interface AggregatorV3Interface {
 	function decimals() external view returns (uint8);
--- packages/contracts/contracts/interfaces/IOracle.sol
+++ packages/contracts/contracts/interfaces/IOracle.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.9;
+pragma solidity >=0.8.0;
 
 interface IOracle {
 	function getPrice(address _asset) external view returns (uint256);
+
+	function getExpiryPrice(address _asset, uint256 _expiryTimestamp)
+		external
+		view
+		returns (uint256, bool);
+
+	function isLockingPeriodOver(address _asset, uint256 _expiryTimestamp)
+		external
+		view
+		returns (bool);
 }
