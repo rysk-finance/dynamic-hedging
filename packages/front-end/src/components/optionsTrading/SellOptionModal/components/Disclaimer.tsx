1abfc190dc1ba8ecae19782574e5b855755a9efa
Tim <timothy.dunphy@gmail.com>
2023-03-07 13:46:36 +0000

    Copy changes.

--- packages/front-end/src/components/optionsTrading/SellOptionModal/components/Disclaimer.tsx
+++ packages/front-end/src/components/optionsTrading/SellOptionModal/components/Disclaimer.tsx
@@ -1,5 +1,5 @@
 export const Disclaimer = () => (
   <small className="block text-gray-600 p-4">
-    {`You are about to sell your position. Please ensure this is what you want because the action is irreversible.`}
+    {`You are about to sell some or all of your position. Please ensure this is what you want because the action is irreversible.`}
   </small>
 );
