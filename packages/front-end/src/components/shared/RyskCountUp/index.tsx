77e90e936391b5560e8b4c67f29e2df2ddc27694
Tim <timothy.dunphy@gmail.com>
2023-03-08 12:30:00 +0000

    Set up new count up component.

---
+++ packages/front-end/src/components/shared/RyskCountUp/index.tsx
@@ -0,0 +1,42 @@
+import type { RyskCountUpProps } from "./types";
+
+import { useEffect, useState } from "react";
+import { CountUp } from "use-count-up";
+
+import { RyskCountUpDecimals } from "./constants";
+
+export const RyskCountUp = ({
+  value = 0,
+  fallback = "-",
+  format = "USD",
+}: RyskCountUpProps) => {
+  const [end, setEnd] = useState(0);
+  const [key, setKey] = useState(crypto.randomUUID());
+  const [start, setStart] = useState(0);
+
+  useEffect(() => {
+    if (value) {
+      setStart(end);
+      setEnd(value);
+      setKey(crypto.randomUUID());
+    }
+  }, [value]);
+
+  if (!value) {
+    return <>{fallback}</>;
+  }
+
+  return (
+    <CountUp
+      decimalPlaces={RyskCountUpDecimals[format]}
+      decimalSeparator={"."}
+      duration={0.3}
+      easing="easeOutCubic"
+      end={end}
+      isCounting={Boolean(value)}
+      key={key}
+      start={start}
+      thousandsSeparator={","}
+    />
+  );
+};
