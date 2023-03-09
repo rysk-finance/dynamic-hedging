204c6d48d0325fcc4d350a77744c292a887bbc8f
Tim <timothy.dunphy@gmail.com>
2023-03-09 14:42:20 +0000

    Temp hack to get the domain to show in the profile modal.

--- packages/front-end/src/clients/WalletProvider/components/Avatar.tsx
+++ packages/front-end/src/clients/WalletProvider/components/Avatar.tsx
@@ -1,13 +1,30 @@
 import type { AvatarComponent } from "@rainbow-me/rainbowkit";
 
 import { hexToCSSFilter } from "hex-to-css-filter";
+import { useEffect } from "react";
+
+import { useGlobalContext } from "src/state/GlobalContext";
 
 const CustomAvatar: AvatarComponent = ({ address, ensImage, size }) => {
+  const {
+    state: { unstoppableDomain },
+  } = useGlobalContext();
+
   const borderRadius = size / 2;
   const iconSize = size * 0.65;
 
   const addressToColor = `#${address.slice(2, 8)}`;
   const colorToCSS = hexToCSSFilter(addressToColor).filter.slice(0, -1);
+
+  // Using this anti-pattern hack until RainbowKit provides
+  // a way to customise the profile name in the modal.
+  useEffect(() => {
+    const profile = document.getElementById("rk_profile_title");
+
+    if (unstoppableDomain && profile) {
+      profile.innerHTML = unstoppableDomain;
+    }
+  }, [unstoppableDomain]);
 
   return ensImage ? (
     <img
