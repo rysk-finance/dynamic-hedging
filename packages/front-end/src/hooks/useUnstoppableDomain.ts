c4f4f741cf2241ca3d6bbbb8ce8eccc82122c275
Tim <timothy.dunphy@gmail.com>
2023-03-09 14:37:13 +0000

    New hook to set domain into state.

---
+++ packages/front-end/src/hooks/useUnstoppableDomain.ts
@@ -0,0 +1,42 @@
+import { captureException } from "@sentry/react";
+import { Resolution } from "@unstoppabledomains/resolution";
+import { getAccount } from "@wagmi/core";
+import { useEffect } from "react";
+
+import { useGlobalContext } from "src/state/GlobalContext";
+import { ActionType } from "src/state/types";
+
+export const useUnstoppableDomain = () => {
+  const { dispatch } = useGlobalContext();
+
+  const { address } = getAccount();
+
+  useEffect(() => {
+    const getDomain = async () => {
+      try {
+        if (address) {
+          const resolution = new Resolution();
+
+          const domain = await resolution.reverse(address);
+
+          if (domain) {
+            dispatch({
+              type: ActionType.SET_UNSTOPPABLE_DOMAIN,
+              unstoppableDomain:
+                domain.length > 24 ? `${domain.substring(0, 24)}...` : domain,
+            });
+          }
+        }
+      } catch (error) {
+        captureException(error);
+
+        dispatch({
+          type: ActionType.SET_UNSTOPPABLE_DOMAIN,
+          unstoppableDomain: null,
+        });
+      }
+    };
+
+    getDomain();
+  }, [address]);
+};
