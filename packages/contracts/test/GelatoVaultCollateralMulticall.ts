ddf6c95013d067d1710c9b3cb5a5500bc0585b9e
kjr217 <kjraja217@gmail.com>
2023-03-21 18:54:44 +0000

    fix: R4 R5

--- packages/contracts/test/GelatoVaultCollateralMulticall.ts
+++ packages/contracts/test/GelatoVaultCollateralMulticall.ts
@@ -115,9 +115,9 @@ describe("Gelato Options registry Vault Health", function () {
 			params: [
 				{
 					forking: {
-						jsonRpcUrl: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY}`,
-						chainId: 42161,
-						blockNumber: 36000000
+						chainId: 1,
+						jsonRpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY}`,
+						blockNumber: 14290000
 					}
 				}
 			]
