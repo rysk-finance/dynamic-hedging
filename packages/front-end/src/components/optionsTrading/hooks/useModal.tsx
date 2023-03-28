89e43b40475a7de03b71b4369cd43c8c622bd47e
Tim <timothy.dunphy@gmail.com>
2023-03-28 12:57:11 +0100

    Generalise state with enum to allow keying multiple modal types.

--- packages/front-end/src/components/optionsTrading/hooks/useModal.tsx
+++ packages/front-end/src/components/optionsTrading/hooks/useModal.tsx
@@ -3,17 +3,25 @@ import { useSearchParams } from "react-router-dom";
 
 import { useGlobalContext } from "src/state/GlobalContext";
 import { useOptionsTradingContext } from "src/state/OptionsTradingContext";
-import { OptionsTradingActionType } from "src/state/types";
+import {
+  OptionChainModalActions,
+  OptionsTradingActionType,
+} from "src/state/types";
 
 /**
- * Hook to check query params and determine if
- * the sell modal should be visible.
+ * Hook that checks query params and state to determine if
+ * the modal should be visible.
+ *
+ * The buy/sell modal are keyed from the `selectedOption`
+ * state variable.
+ *
+ * The close long position modal is keyed from query params.
  *
  * Returns a boolean value for the modal state.
  *
  * @returns [boolean]
  */
-export const useCloseModal = () => {
+export const useModal = () => {
   const [searchParams] = useSearchParams();
 
   const {
@@ -23,7 +31,7 @@ export const useCloseModal = () => {
   } = useGlobalContext();
 
   const {
-    state: { optionChainModalOpen },
+    state: { optionChainModalOpen, selectedOption },
     dispatch,
   } = useOptionsTradingContext();
 
@@ -37,11 +45,13 @@ export const useCloseModal = () => {
       if (hasSellRef && hasUserPosition) {
         dispatch({
           type: OptionsTradingActionType.SET_OPTION_CHAIN_MODAL_VISIBLE,
-          visible: true,
+          visible: OptionChainModalActions.CLOSE,
         });
+      } else if (selectedOption) {
+        // do stuff here
       }
     }
-  }, [activeExpiry, searchParams]);
+  }, [activeExpiry, searchParams, selectedOption]);
 
   return [optionChainModalOpen] as const;
 };
