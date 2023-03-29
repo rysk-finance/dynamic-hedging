55ffc0f06313d3ba25d643ffe8b3eab94975720d
Tim <timothy.dunphy@gmail.com>
2023-03-29 17:12:41 +0100

    New Icon assets.

--- packages/front-end/src/Icons/index.ts
+++ packages/front-end/src/Icons/index.ts
@@ -1,19 +1,23 @@
 import { Close } from "./Close";
 import { DownChevron } from "./DownChevron";
+import { Ether } from "./Ether";
 import { LeftArrow } from "./LeftArrow";
 import { Loading } from "./Loading";
 import { Question } from "./Question";
 import { RightArrow } from "./RightArrow";
 import { RyskFavicon } from "./RyskFavicon";
 import { UpChevron } from "./UpChevron";
+import { USDC } from "./USDC";
 
 export {
   Close,
   DownChevron,
+  Ether,
   LeftArrow,
   Loading,
   Question,
   RightArrow,
   RyskFavicon,
   UpChevron,
+  USDC,
 };
