e8e1460709edee28c2829ec4a23598c38a5daa77
Tim <timothy.dunphy@gmail.com>
2023-03-29 16:51:49 +0100

    Simplify component to use media queries and a better message.

--- packages/front-end/src/components/MobileWarning.tsx
+++ packages/front-end/src/components/MobileWarning.tsx
@@ -1,43 +1,10 @@
-import { useCallback, useEffect, useState } from "react";
-
-const OVERFLOW_HIDDEN_CLASS = "overflow-hidden";
-const MIN_WIDTH = 768;
-
 export const MobileWarning = () => {
-  const [isActive, setIsActive] = useState(false);
-
-  const resizeHandler = useCallback(() => {
-    if (window.innerWidth < MIN_WIDTH && !isActive) {
-      setIsActive(true);
-    } else if (window.innerWidth >= MIN_WIDTH && isActive) {
-      setIsActive(false);
-    }
-  }, [isActive]);
-
-  useEffect(() => {
-    resizeHandler();
-    window.addEventListener("resize", resizeHandler);
-
-    return () => window.removeEventListener("resize", resizeHandler);
-  }, [resizeHandler]);
-
-  useEffect(() => {
-    if (isActive !== null) {
-      if (!isActive) {
-        document.body.classList.remove(OVERFLOW_HIDDEN_CLASS);
-      } else if (!document.body.classList.contains(OVERFLOW_HIDDEN_CLASS)) {
-        document.body.classList.add(OVERFLOW_HIDDEN_CLASS);
-      }
-    }
-  }, [isActive]);
-
-  return isActive ? (
-    <div className="fixed w-screen h-screen z-[10000] bg-bone flex flex-col items-center justify-center p-4">
+  return (
+    <div className="fixed w-full h-full z-[10000] bg-bone flex md:hidden flex-col items-center justify-center p-4">
       <img src={"/logo.png"} alt="logo" className="w-20 mb-8" />
       <p className="text-center">
-        Rysk doesn&apos;t support mobile currently. Please visit back on desktop
-        to continue.
+        {`Unfortunately, Rysk does not currently support mobile devices. We are working hard to rectify this. Please come back via a desktop device for now.`}
       </p>
     </div>
-  ) : null;
+  );
 };
