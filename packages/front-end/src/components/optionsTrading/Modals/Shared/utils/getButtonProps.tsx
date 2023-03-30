9b1f37c6e7f7936d0fa86f57a879ad8727d40eb7
Tim <timothy.dunphy@gmail.com>
2023-03-30 14:20:42 +0100

    Make transaction key optional.

--- packages/front-end/src/components/optionsTrading/Modals/Shared/utils/getButtonProps.tsx
+++ packages/front-end/src/components/optionsTrading/Modals/Shared/utils/getButtonProps.tsx
@@ -5,34 +5,33 @@ import { capitalise } from "src/utils/caseConvert";
  * Switch function to get the modal button props.
  *
  * @param action - Action for the modal. One of "buy" or "sell".
- * @param dataPending - Boolean value to determine if the transaction or a fetch is pending.
+ * @param transactionPending - Boolean value to determine if the transaction or a fetch is pending.
  * @param isApproved - Boolean value to determine if the transaction is approved.
  * @param handleApprove - Function to handle approval for the transaction.
- * @param handleTransaction - Function to handle the transaction.
+ * @param handleTransaction - Function to handle the transaction - optional.
  */
 export const getButtonProps = (
   action: "buy" | "sell",
-  dataPending: boolean,
+  transactionPending: boolean,
   isApproved: boolean,
   handleApprove: () => Promise<void>,
-  handleTransaction: () => Promise<void>
+  handleTransaction?: () => Promise<void>
 ) => {
   switch (true) {
-    case dataPending:
+    case transactionPending:
       return {
         children: (
           <Loading className="h-8 mx-auto animate-spin text-gray-600" />
         ),
         key: "pending",
-        onClick: () => {},
         title: "Transaction pending.",
       };
 
-    case isApproved:
+    case isApproved && handleTransaction:
       return {
         children: capitalise(action),
         key: action,
-        onClick: handleTransaction,
+        onClick: handleTransaction as () => Promise<void>,
         title: `Click to ${action}.`,
       };
 
