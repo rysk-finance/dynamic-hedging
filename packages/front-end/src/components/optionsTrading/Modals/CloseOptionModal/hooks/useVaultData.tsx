9623c07f7ee92691e278cb0c58833018b916fd94
Yassine <yassinesania@gmail.com>
2023-04-04 12:40:02 +0200

    feat(sell): Support closing short

---
+++ packages/front-end/src/components/optionsTrading/Modals/CloseOptionModal/hooks/useVaultData.tsx
@@ -0,0 +1,36 @@
+import { useAccount } from "wagmi";
+import { gql, useQuery } from "@apollo/client";
+import { VaultQueryData } from "../../Shared/types";
+import { BigNumber } from "ethers";
+import { fromOpynToNumber } from "../../../../../utils/conversion-helper";
+
+export const useVaultData = (vaultId: string | null) => {
+  // Global state
+  const { address } = useAccount();
+
+  const { data } = useQuery<VaultQueryData>(
+    gql`
+      query ($vaultId: ID!) {
+        vault(id: $vaultId) {
+          id
+          collateralAmount
+          shortAmount
+        }
+      }
+    `,
+    {
+      variables: {
+        vaultId: `${address?.toLowerCase()}-${vaultId}`,
+      },
+      skip: !vaultId || !address,
+    }
+  );
+
+  const collateralAmount = BigNumber.from(data?.vault?.collateralAmount || 0);
+
+  const collateralPerOption = collateralAmount.gt(0)
+    ? collateralAmount.div(fromOpynToNumber(data?.vault?.shortAmount || 1))
+    : BigNumber.from(0);
+
+  return [collateralAmount, collateralPerOption];
+};
--- packages/front-end/src/components/optionsTrading/Modals/Shared/utils/transactions.ts
+++ packages/front-end/src/components/optionsTrading/Modals/Shared/utils/transactions.ts
@@ -2,7 +2,7 @@ import { BigNumber as BigNumberType } from "ethers";
 
 import type { UserVaults } from "src/state/types";
 import type { OptionSeries } from "src/types";
-import type { AddressesRequired } from "../types";
+import type { AddressesRequired, AddressesRequiredVaultSell } from "../types";
 
 import {
   getContract,
@@ -11,11 +11,11 @@ import {
   waitForTransaction,
   writeContract,
 } from "@wagmi/core";
-import { BigNumber, utils } from "ethers";
+import { BigNumber, utils, ethers } from "ethers";
 
+import { erc20ABI } from "src/abis/erc20_ABI";
 import { NewControllerABI } from "src/abis/NewController_ABI";
 import { OptionExchangeABI } from "src/abis/OptionExchange_ABI";
-import { erc20ABI } from "src/abis/erc20_ABI";
 import {
   EMPTY_SERIES,
   GAS_MULTIPLIER,
@@ -336,4 +336,91 @@ export const setOperator = async (isOperator: boolean) => {
   await waitForTransaction({ hash, confirmations: 1 });
 
   return hash;
+};
+
+export const vaultSell = async (
+  addresses: AddressesRequiredVaultSell,
+  amount: BigNumberType,
+  refresh: () => void,
+  collateralAmount: BigNumberType
+) => {
+  const txData = [
+    {
+      operation: OperationType.RyskAction,
+      operationQueue: [
+        {
+          actionType: BigNumber.from(RyskActionType.BuyOption),
+          owner: ZERO_ADDRESS,
+          secondAddress: addresses.user,
+          asset: addresses.token,
+          vaultId: BigNumber.from(0),
+          amount: amount,
+          optionSeries: EMPTY_SERIES,
+          indexOrAcceptablePremium: BigNumber.from(0),
+          data: "0x" as HexString,
+        },
+      ],
+    },
+    {
+      operation: OperationType.OpynAction,
+      operationQueue: [
+        {
+          actionType: BigNumber.from(OpynActionType.BurnShortOption),
+          owner: addresses.user,
+          secondAddress: addresses.user,
+          asset: addresses.token,
+          vaultId: addresses.vaultId,
+          amount: amount.div(ethers.utils.parseUnits("1", 10)),
+          optionSeries: EMPTY_SERIES,
+          indexOrAcceptablePremium: BigNumber.from(0),
+          data: "0x" as HexString,
+        },
+        {
+          actionType: BigNumber.from(OpynActionType.WithdrawCollateral),
+          owner: addresses.user,
+          secondAddress: addresses.user,
+          asset: addresses.collateralAsset,
+          vaultId: addresses.vaultId,
+          amount: collateralAmount,
+          optionSeries: EMPTY_SERIES,
+          indexOrAcceptablePremium: BigNumber.from(0),
+          data: "0x" as HexString,
+        },
+      ],
+    },
+  ];
+
+  const config = await prepareWriteContract({
+    address: addresses.exchange,
+    abi: OptionExchangeABI,
+    functionName: "operate",
+    args: [txData],
+    overrides: {
+      gasLimit: BigNumber.from("3000000"),
+    },
+  });
+
+  if (config.request.data) {
+    const simulationResponse = await fetchSimulation(
+      addresses.user,
+      addresses.exchange,
+      config.request.data
+    );
+
+    if (simulationResponse.simulation.status) {
+      config.request.gasLimit = BigNumber.from(
+        simulationResponse.simulation.gas_used
+      );
+
+      const { hash, wait } = await writeContract(config);
+
+      await wait(1);
+
+      refresh();
+
+      return hash;
+    } else {
+      throw new Error("Tenderly simulation failed.");
+    }
+  }
 };
