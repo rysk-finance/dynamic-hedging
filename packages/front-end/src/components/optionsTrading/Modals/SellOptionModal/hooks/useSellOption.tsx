ed15532cb34eda391f5413013c4c99cf4a94e254
Tim <timothy.dunphy@gmail.com>
2023-04-05 09:58:49 +0100

    Option selling hooks and utils.

---
+++ packages/front-end/src/components/optionsTrading/Modals/SellOptionModal/hooks/useSellOption.tsx
@@ -0,0 +1,206 @@
+import type { Addresses } from "../../Shared/types";
+import type { PositionDataState } from "../types";
+
+import { captureException } from "@sentry/react";
+import { readContract } from "@wagmi/core";
+import dayjs from "dayjs";
+import { BigNumber } from "ethers";
+import { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+
+import { NewMarginCalculatorABI } from "src/abis/NewMarginCalculator_ABI";
+import { DECIMALS } from "src/config/constants";
+import { useGlobalContext } from "src/state/GlobalContext";
+import { useOptionsTradingContext } from "src/state/OptionsTradingContext";
+import { CollateralAmount } from "src/state/types";
+import {
+  fromRyskToNumber,
+  tFormatUSDC,
+  toOpyn,
+  toRysk,
+  toUSDC,
+  toWei,
+  truncate,
+} from "src/utils/conversion-helper";
+import { getContractAddress } from "src/utils/helpers";
+import { useAllowance } from "../../Shared/hooks/useAllowance";
+import { getBalancesAsInteger } from "../../Shared/utils/getBalancesAsInteger";
+import { getQuote } from "../../Shared/utils/getQuote";
+
+const multipliers = {
+  [CollateralAmount["1.5x"]]: 15,
+  [CollateralAmount["2x"]]: 20,
+  [CollateralAmount["3x"]]: 30,
+};
+
+export const useSellOption = (amountToSell: string) => {
+  // Global state.
+  const {
+    state: {
+      collateralPreferences,
+      ethPrice,
+      options: { activeExpiry },
+    },
+  } = useGlobalContext();
+
+  const {
+    state: { selectedOption },
+  } = useOptionsTradingContext();
+
+  // Collateral type.
+  const USDCCollateral = collateralPreferences.type === "USDC";
+
+  // Addresses.
+  const { address } = useAccount();
+  const USDCAddress = getContractAddress("USDC");
+  const WETHAddress = getContractAddress("WETH");
+  const collateralAddress = USDCCollateral ? USDCAddress : WETHAddress;
+  const exchangeAddress = getContractAddress("optionExchange");
+  const marginCalculatorAddress = getContractAddress("OpynNewCalculator");
+
+  // User allowance state for the collateral asset.
+  const [allowance, setAllowance] = useAllowance(collateralAddress, address);
+
+  // User position state.
+  const [purchaseData, setPurchaseData] = useState<PositionDataState>({
+    callOrPut: selectedOption?.callOrPut,
+    collateral: 0,
+    expiry: dayjs.unix(Number(activeExpiry)).format("DDMMMYY"),
+    fee: 0,
+    now: dayjs().format("MMM DD, YYYY HH:mm A"),
+    premium: 0,
+    quote: 0,
+    remainingBalanceUSDC: 0,
+    remainingBalanceWETH: 0,
+    requiredApproval: "",
+    strike: selectedOption?.strikeOptions?.strike,
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  // Get user position price data.
+  useEffect(() => {
+    const setPriceData = async (amount: number) => {
+      setLoading(true);
+
+      try {
+        const { USDC: balanceUSDCInt, WETH: balanceWETHInt } =
+          await getBalancesAsInteger(address);
+
+        if (amount > 0 && ethPrice && selectedOption) {
+          const strike = selectedOption.strikeOptions.strike;
+
+          const { totalFees, totalPremium } = await getQuote(
+            Number(activeExpiry),
+            toRysk(strike.toString()),
+            selectedOption.callOrPut === "put",
+            toWei(amount.toString()),
+            selectedOption.buyOrSell === "sell",
+            collateralPreferences.type
+          );
+
+          const fee = tFormatUSDC(totalFees) / Number(amountToSell);
+          const premium = tFormatUSDC(totalPremium) / Number(amountToSell);
+          const quote = tFormatUSDC(totalPremium.sub(totalFees));
+
+          const _getCollateralAmount = async () => {
+            const requiredCollateral = await readContract({
+              address: marginCalculatorAddress,
+              abi: NewMarginCalculatorABI,
+              functionName: "getNakedMarginRequired",
+              args: [
+                WETHAddress,
+                USDCAddress,
+                collateralAddress,
+                toOpyn(amountToSell),
+                toOpyn(strike.toString()),
+                toOpyn(ethPrice.toString()),
+                BigNumber.from(activeExpiry),
+                BigNumber.from(USDCCollateral ? DECIMALS.USDC : DECIMALS.RYSK),
+                selectedOption.callOrPut === "put",
+              ],
+            });
+
+            if (collateralPreferences.amount === CollateralAmount["full"]) {
+              if (USDCCollateral) {
+                return Number(strike) * Number(amount);
+              } else {
+                return Number(amount);
+              }
+            } else {
+              const multipliedCollateral = requiredCollateral
+                .mul(multipliers[collateralPreferences.amount])
+                .div(10);
+              const formatted = USDCCollateral
+                ? tFormatUSDC(multipliedCollateral)
+                : fromRyskToNumber(multipliedCollateral.toString());
+
+              return Math.min(formatted, strike * Number(amountToSell));
+            }
+          };
+
+          const collateral = await _getCollateralAmount();
+
+          const remainingBalanceUSDC =
+            balanceUSDCInt + quote - (USDCCollateral ? collateral : 0);
+          const remainingBalanceWETH = USDCCollateral
+            ? balanceWETHInt
+            : balanceWETHInt - collateral;
+
+          const requiredApproval = String(truncate(collateral * 1.05, 4));
+          const approved = (
+            USDCCollateral ? toUSDC(requiredApproval) : toRysk(requiredApproval)
+          ).lte(allowance.amount);
+
+          setPurchaseData({
+            callOrPut: selectedOption.callOrPut,
+            collateral,
+            expiry: dayjs.unix(Number(activeExpiry)).format("DDMMMYY"),
+            fee,
+            now: dayjs().format("MMM DD, YYYY HH:mm A"),
+            premium,
+            quote,
+            remainingBalanceUSDC,
+            remainingBalanceWETH,
+            requiredApproval,
+            strike,
+          });
+          setAllowance((currentState) => ({ ...currentState, approved }));
+        } else {
+          setPurchaseData({
+            callOrPut: selectedOption?.callOrPut,
+            collateral: 0,
+            expiry: dayjs.unix(Number(activeExpiry)).format("DDMMMYY"),
+            fee: 0,
+            now: dayjs().format("MMM DD, YYYY HH:mm A"),
+            premium: 0,
+            quote: 0,
+            remainingBalanceUSDC: balanceUSDCInt,
+            remainingBalanceWETH: balanceWETHInt,
+            requiredApproval: "",
+            strike: selectedOption?.strikeOptions?.strike,
+          });
+          setAllowance((currentState) => ({
+            ...currentState,
+            approved: false,
+          }));
+        }
+
+        setLoading(false);
+      } catch (error) {
+        captureException(error);
+        setLoading(false);
+      }
+    };
+
+    setPriceData(Number(amountToSell));
+  }, [amountToSell, allowance.amount, collateralPreferences, ethPrice]);
+
+  const addresses: Addresses = {
+    exchange: exchangeAddress,
+    token: collateralAddress,
+    user: address,
+  };
+
+  return [addresses, allowance, setAllowance, purchaseData, loading] as const;
+};
--- packages/front-end/src/components/optionsTrading/Modals/Shared/utils/transactions.ts
+++ packages/front-end/src/components/optionsTrading/Modals/Shared/utils/transactions.ts
@@ -1,21 +1,34 @@
-import type { BigNumber as BigNumberType } from "ethers";
+import { BigNumber as BigNumberType } from "ethers";
 
+import type { UserVaults } from "src/state/types";
 import type { OptionSeries } from "src/types";
 import type { AddressesRequired } from "../types";
 
-import { prepareWriteContract, writeContract } from "@wagmi/core";
-import { BigNumber } from "ethers";
-import { erc20ABI } from "src/abis/erc20_ABI";
+import {
+  getContract,
+  getProvider,
+  prepareWriteContract,
+  waitForTransaction,
+  writeContract,
+} from "@wagmi/core";
+import { BigNumber, utils } from "ethers";
+
 import { NewControllerABI } from "src/abis/NewController_ABI";
 import { OptionExchangeABI } from "src/abis/OptionExchange_ABI";
+import { erc20ABI } from "src/abis/erc20_ABI";
 import {
   EMPTY_SERIES,
   GAS_MULTIPLIER,
   ZERO_ADDRESS,
 } from "src/config/constants";
 import OperationType from "src/enums/OperationType";
+import {
+  OpenVaultCollateralType,
+  OpynActionType,
+} from "src/enums/OpynActionType";
 import RyskActionType from "src/enums/RyskActionType";
 import { fetchSimulation } from "src/hooks/useTenderlySimulator";
+import { fromWeiToOpyn } from "src/utils/conversion-helper";
 import { getContractAddress } from "src/utils/helpers";
 
 export const approveAllowance = async (
@@ -41,65 +54,9 @@ export const approveAllowance = async (
         Math.ceil(simulationResponse.simulation.gas_used * GAS_MULTIPLIER)
       );
 
-      const { hash, wait } = await writeContract(config);
+      const { hash } = await writeContract(config);
 
-      await wait(1);
-
-      return hash;
-    } else {
-      throw new Error("Tenderly simulation failed.");
-    }
-  }
-};
-
-export const closeLong = async (
-  addresses: AddressesRequired,
-  amount: BigNumberType,
-  refresh: () => void
-) => {
-  const txData = [
-    {
-      operation: OperationType.RyskAction,
-      operationQueue: [
-        {
-          actionType: BigNumber.from(RyskActionType.CloseOption),
-          owner: ZERO_ADDRESS,
-          secondAddress: addresses.user,
-          asset: addresses.token,
-          vaultId: BigNumber.from(0),
-          amount,
-          optionSeries: EMPTY_SERIES,
-          indexOrAcceptablePremium: BigNumber.from(0),
-          data: "0x" as HexString,
-        },
-      ],
-    },
-  ];
-
-  const config = await prepareWriteContract({
-    address: addresses.exchange,
-    abi: OptionExchangeABI,
-    functionName: "operate",
-    args: [txData],
-  });
-
-  if (config.request.data) {
-    const simulationResponse = await fetchSimulation(
-      addresses.user,
-      addresses.exchange,
-      config.request.data
-    );
-
-    if (simulationResponse.simulation.status) {
-      config.request.gasLimit = BigNumber.from(
-        Math.ceil(simulationResponse.simulation.gas_used * GAS_MULTIPLIER)
-      );
-
-      const { hash, wait } = await writeContract(config);
-
-      await wait(2);
-
-      refresh();
+      await waitForTransaction({ hash, confirmations: 2 });
 
       return hash;
     } else {
@@ -163,9 +120,196 @@ export const buy = async (
         Math.ceil(simulationResponse.simulation.gas_used * GAS_MULTIPLIER)
       );
 
-      const { hash, wait } = await writeContract(config);
+      const { hash } = await writeContract(config);
 
-      await wait(2);
+      await waitForTransaction({ hash, confirmations: 2 });
+
+      refresh();
+
+      return hash;
+    } else {
+      throw new Error("Tenderly simulation failed.");
+    }
+  }
+};
+
+export const closeLong = async (
+  addresses: AddressesRequired,
+  amount: BigNumberType,
+  refresh: () => void
+) => {
+  const txData = [
+    {
+      operation: OperationType.RyskAction,
+      operationQueue: [
+        {
+          actionType: BigNumber.from(RyskActionType.CloseOption),
+          owner: ZERO_ADDRESS,
+          secondAddress: addresses.user,
+          asset: addresses.token,
+          vaultId: BigNumber.from(0),
+          amount,
+          optionSeries: EMPTY_SERIES,
+          indexOrAcceptablePremium: BigNumber.from(0),
+          data: "0x" as HexString,
+        },
+      ],
+    },
+  ];
+
+  const config = await prepareWriteContract({
+    address: addresses.exchange,
+    abi: OptionExchangeABI,
+    functionName: "operate",
+    args: [txData],
+  });
+
+  if (config.request.data) {
+    const simulationResponse = await fetchSimulation(
+      addresses.user,
+      addresses.exchange,
+      config.request.data
+    );
+
+    if (simulationResponse.simulation.status) {
+      config.request.gasLimit = BigNumber.from(
+        Math.ceil(simulationResponse.simulation.gas_used * GAS_MULTIPLIER)
+      );
+
+      const { hash } = await writeContract(config);
+
+      await waitForTransaction({ hash, confirmations: 2 });
+
+      refresh();
+
+      return hash;
+    } else {
+      throw new Error("Tenderly simulation failed.");
+    }
+  }
+};
+
+export const sell = async (
+  addresses: AddressesRequired,
+  amount: BigNumberType,
+  optionSeries: OptionSeries,
+  refresh: () => void,
+  vaults: UserVaults,
+  collateral: BigNumberType
+) => {
+  // Get oToken address.
+  const provider = getProvider();
+  const contract = getContract({
+    address: addresses.exchange,
+    abi: OptionExchangeABI,
+    signerOrProvider: provider,
+  });
+
+  // OptionExchangeABI is missing `callStatic` so the return type is wrong, hence the conversion to unknown.
+  const oTokenAddress = (await contract.callStatic.createOtoken({
+    strikeAsset: getContractAddress("USDC"),
+    collateral: addresses.token,
+    underlying: getContractAddress("WETH"),
+    expiration: optionSeries.expiration,
+    strike: optionSeries.strike,
+    isPut: optionSeries.isPut,
+  })) as unknown as HexString;
+
+  const vaultKey = oTokenAddress.toLowerCase() as HexString;
+  const hasVault = Boolean(vaults[vaultKey]);
+
+  // Get vaultId from global state or assign next available.
+  const vaultId = hasVault
+    ? BigNumber.from(vaults[vaultKey])
+    : BigNumber.from(++vaults.length);
+
+  const openVaultData = {
+    actionType: BigNumber.from(OpynActionType.OpenVault),
+    owner: addresses.user,
+    secondAddress: addresses.user,
+    asset: ZERO_ADDRESS,
+    vaultId,
+    amount: BigNumber.from(0),
+    optionSeries: EMPTY_SERIES,
+    indexOrAcceptablePremium: BigNumber.from(0),
+    data: utils.hexZeroPad(
+      utils.hexlify([OpenVaultCollateralType.Partially]),
+      32
+    ) as HexString,
+  };
+
+  const requiredData = [
+    {
+      actionType: BigNumber.from(OpynActionType.DepositCollateral),
+      owner: addresses.user,
+      secondAddress: addresses.exchange,
+      asset: addresses.token,
+      vaultId,
+      amount: collateral,
+      optionSeries: EMPTY_SERIES,
+      indexOrAcceptablePremium: BigNumber.from(0),
+      data: ZERO_ADDRESS,
+    },
+    {
+      actionType: BigNumber.from(OpynActionType.MintShortOption),
+      owner: addresses.user,
+      secondAddress: addresses.exchange,
+      asset: oTokenAddress,
+      vaultId,
+      amount: fromWeiToOpyn(amount),
+      optionSeries: EMPTY_SERIES,
+      indexOrAcceptablePremium: BigNumber.from(0),
+      data: ZERO_ADDRESS,
+    },
+  ];
+
+  const txData = [
+    {
+      operation: OperationType.OpynAction,
+      operationQueue: hasVault
+        ? requiredData
+        : [openVaultData, ...requiredData],
+    },
+    {
+      operation: OperationType.RyskAction,
+      operationQueue: [
+        {
+          actionType: BigNumber.from(RyskActionType.SellOption),
+          owner: ZERO_ADDRESS,
+          secondAddress: addresses.user,
+          asset: ZERO_ADDRESS,
+          vaultId: BigNumber.from(0),
+          amount,
+          optionSeries,
+          indexOrAcceptablePremium: BigNumber.from(0),
+          data: ZERO_ADDRESS,
+        },
+      ],
+    },
+  ];
+
+  const config = await prepareWriteContract({
+    address: addresses.exchange,
+    abi: OptionExchangeABI,
+    functionName: "operate",
+    args: [txData],
+  });
+
+  if (config.request.data) {
+    const simulationResponse = await fetchSimulation(
+      addresses.user,
+      addresses.exchange,
+      config.request.data
+    );
+
+    if (simulationResponse.simulation.status) {
+      config.request.gasLimit = BigNumber.from(
+        Math.ceil(simulationResponse.simulation.gas_used * GAS_MULTIPLIER)
+      );
+
+      const { hash } = await writeContract(config);
+
+      await waitForTransaction({ hash, confirmations: 2 });
 
       refresh();
 
@@ -184,9 +328,9 @@ export const setOperator = async (isOperator: boolean) => {
     args: [getContractAddress("optionExchange"), !isOperator],
   });
 
-  const { hash, wait } = await writeContract(config);
+  const { hash } = await writeContract(config);
 
-  await wait(1);
+  await waitForTransaction({ hash, confirmations: 1 });
 
   return hash;
 };
