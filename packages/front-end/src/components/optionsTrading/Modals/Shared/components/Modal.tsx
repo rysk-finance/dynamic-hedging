6c29b3b0e2b6757feb160408beba467942922e35
Tim <timothy.dunphy@gmail.com>
2023-04-09 12:15:18 +0100

    Close modal on light box click.

--- packages/front-end/src/components/optionsTrading/Modals/Shared/components/Modal.tsx
+++ packages/front-end/src/components/optionsTrading/Modals/Shared/components/Modal.tsx
@@ -1,21 +1,39 @@
-import type { PropsWithChildren } from "react";
+import type { MouseEvent, PropsWithChildren } from "react";
 
 import { motion } from "framer-motion";
 
 import FadeInOut from "src/animation/FadeInOut";
 import FadeInUpDelayed from "src/animation/FadeInUpDelayed";
+import { useOptionsTradingContext } from "src/state/OptionsTradingContext";
+import { OptionsTradingActionType } from "src/state/types";
 
-export const Modal = ({ children }: PropsWithChildren) => (
-  <motion.div
-    className="fixed inset-0 z-50 w-screen h-screen grid grid-cols-12 bg-bone-light/80"
-    {...FadeInOut(0.1)}
-  >
+export const Modal = ({ children }: PropsWithChildren) => {
+  const { dispatch } = useOptionsTradingContext();
+
+  const handleLightBoxClick = () => {
+    dispatch({
+      type: OptionsTradingActionType.RESET,
+    });
+  };
+
+  const handleModalClick = (event: MouseEvent) => event.stopPropagation();
+
+  return (
     <motion.div
-      aria-modal="true"
-      className="flex flex-col col-span-8 col-start-3 lg:col-span-6 lg:col-start-4 xl:col-span-4 xl:col-start-5 my-auto border-black border-2 rounded-2xl bg-bone-light bg-[url('./assets/white-ascii-50.png')] bg-center overflow-hidden"
-      {...FadeInUpDelayed(0.3)}
+      className="fixed inset-0 z-50 w-screen h-screen grid grid-cols-12 bg-bone-light/80 cursor-pointer"
+      onClick={handleLightBoxClick}
+      title="Click to close the modal."
+      {...FadeInOut(0.1)}
     >
-      {children}
+      <motion.div
+        aria-modal="true"
+        className="flex flex-col col-span-8 col-start-3 lg:col-span-6 lg:col-start-4 xl:col-span-4 xl:col-start-5 my-auto border-black border-2 rounded-2xl bg-bone-light bg-[url('./assets/white-ascii-50.png')] bg-center overflow-hidden cursor-default"
+        onClick={handleModalClick}
+        title=""
+        {...FadeInUpDelayed(0.3)}
+      >
+        {children}
+      </motion.div>
     </motion.div>
-  </motion.div>
-);
+  );
+};
