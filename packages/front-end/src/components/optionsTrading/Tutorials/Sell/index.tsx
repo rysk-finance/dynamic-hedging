f506f7b650f6e6f89a57a9a34298565a6c6b438e
Tim <timothy.dunphy@gmail.com>
2023-04-14 15:46:24 +0100

    Copy updates from Ugolino.

--- packages/front-end/src/components/optionsTrading/Tutorials/Sell/index.tsx
+++ packages/front-end/src/components/optionsTrading/Tutorials/Sell/index.tsx
@@ -29,27 +29,27 @@ export const SellModalTutorial = () => {
         {
           disableBeacon: true,
           target: "#sell-symbol",
-          content: `Here you can see the asset you have selected to buy. This is represented as "underlying asset - expiry date - strike price - option flavor".`,
+          content: `Here you can see the option you have selected to sell. This is represented as "underlying asset - expiry date - strike price - option type (put or call)".`,
         },
         {
           target: "#sell-collateral",
           content:
-            "These filters allow you to specify the type of collateral you wish to use and how much you will add.",
+            "To sell an option you will be required to post collateral. These filters allow you to specify the type of collateral you wish to use and how much you will add.",
         },
         {
           target: "#sell-price-per-option",
           content:
-            "These prices represent the cost per option. The premium and fees are displayed separately for transparency, and you can also see how the price is impacted based on order size.",
+            "These prices represent the total amount you receive per option. The premium and fees are displayed separately for transparency, and you can also see how the premium is impacted by slippage based on order size.",
         },
         {
           target: "#sell-collateral-required",
           content:
-            "This price represents the total amount of collateral you must provide to complete the transaction.",
+            "This price represents the total amount of collateral you must provide to sell the options.",
         },
         {
           target: "#sell-total-price",
           content:
-            "This price represents the total amount in USDC you will receive after fees.",
+            "This price represents the total amount in USDC you will receive as premium after fees.",
         },
         {
           target: "#sell-balances",
@@ -59,17 +59,17 @@ export const SellModalTutorial = () => {
         {
           target: "#sell-input",
           content:
-            "You can use this input to specify how many contracts you wish to sell. We allow denominations as small as 0.01.",
+            "You can use this input to specify how many contracts you wish to sell. You can sell denominations as small as 0.01.",
         },
         {
           target: "#sell-button",
           content:
-            "You can click here to approve your spending and complete your purchase. If you do not already have your wallet connected, this button can handle that. Transactions will require two confirmations to be considered complete, and the interface will automatically close once your transaction is complete.",
+            "You can click here to approve your spending for collateral required and complete your sell. If you do not already have your wallet connected, this button can handle that. Transactions will require two confirmations to be considered complete, and the interface will automatically close once your transaction is complete.",
         },
         {
           target: "#quick-switch",
           content:
-            "This handy button will allow you to quickly toggle between buying and selling for the specified strike price and option flavor.",
+            "This handy button will allow you to quickly toggle between buying and selling for the selected option.",
         },
       ]}
       styles={styles}
