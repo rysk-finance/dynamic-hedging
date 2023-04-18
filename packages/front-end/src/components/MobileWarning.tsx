5580c9ad3ac2cab235fc4d83b8ee2816525c11dc
Tim <timothy.dunphy@gmail.com>
2023-04-18 15:54:03 +0100

    Animated logo on load.

--- packages/front-end/src/components/MobileWarning.tsx
+++ packages/front-end/src/components/MobileWarning.tsx
@@ -1,7 +1,12 @@
 export const MobileWarning = () => {
   return (
     <div className="fixed w-full h-full z-[10000] bg-bone flex md:hidden flex-col items-center justify-center p-4">
-      <img src={"/logo.png"} alt="logo" className="w-20 mb-8" />
+      <img
+        alt="Rysk logo"
+        className="w-20 mb-8"
+        src={"/logo-animated.gif"}
+        title="Rysk: Uncorrelated Returns"
+      />
       <p className="text-center">
         {`Unfortunately, Rysk does not currently support mobile devices. We are working hard to rectify this. Please come back via a desktop device for now.`}
       </p>
