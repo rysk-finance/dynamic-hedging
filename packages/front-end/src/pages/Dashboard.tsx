44facf89e04a8c8108227263c6243837d5923e5d
Yassine <yassinesania@gmail.com>
2023-04-25 15:51:58 +0200

    fix(state): Remove double state context provider
    It was reinitialising state to empty.

--- packages/front-end/src/pages/Dashboard.tsx
+++ packages/front-end/src/pages/Dashboard.tsx
@@ -1,10 +1,5 @@
 import { DashboardContent } from "../components/dashboard";
-import { GlobalContextProvider } from "../state/GlobalContext";
 
 export const Dashboard = () => {
-  return (
-    <GlobalContextProvider>
-      <DashboardContent />
-    </GlobalContextProvider>
-  );
+  return <DashboardContent />;
 };
