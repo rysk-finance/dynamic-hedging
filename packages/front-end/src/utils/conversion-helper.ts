05191695e6ac4c3bff704710675baf0510e8c086
Yassine <yassinesania@gmail.com>
2023-04-25 15:50:16 +0200

    style(copy): Adjust option name to standard

--- packages/front-end/src/utils/conversion-helper.ts
+++ packages/front-end/src/utils/conversion-helper.ts
@@ -105,11 +105,11 @@ export const parseTokenAmount = (value: BigNumberish, decimals: number) =>
   BigNumber.from(value).mul(BigNumber.from(10).pow(BigNumber.from(decimals)));
 
 export const renameOtoken = (string: string) => {
-  const isPut = string.slice(-1) === "P" ? "Put" : "Call";
+  const isPut = string.slice(-1) === "P" ? "P" : "C";
   const expiry = string.substring(string.indexOf("-") + 1).split("-")[0];
-  const strike = `$${string.split("-").pop()?.slice(0, -1)}`;
+  const strike = `${string.split("-").pop()?.slice(0, -1)}`;
 
-  return "ETH " + expiry + " " + strike + " " + isPut;
+  return "ETH-" + expiry + "-" + strike + "-" + isPut;
 };
 
 export const baseRyskToUsdc = (value: BigNumber) => {
