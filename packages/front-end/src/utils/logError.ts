0e297aa14ac3c9c98cd4d0b75a90aec31dc2a92b
Tim <timothy.dunphy@gmail.com>
2023-05-09 13:33:10 +0100

    Better Sentry logging.
    * Don't collect IP addresses.
    
    * Include users wallet address as username if available.
    
    * Always console.error for better breadcrumbs.

--- packages/front-end/src/utils/logError.ts
+++ packages/front-end/src/utils/logError.ts
@@ -1,9 +1,15 @@
-import { captureException } from "@sentry/react";
+import { captureException, setUser } from "@sentry/react";
+import { getAccount } from "@wagmi/core";
 
 export const logError = (error: unknown) => {
-  if (process.env.NODE_ENV === "development") {
-    console.error(error);
-  } else {
+  console.error(error);
+
+  if (process.env.NODE_ENV !== "development") {
+    const { address } = getAccount();
+    setUser({
+      ip_address: "0.0.0.0",
+      username: address ? address.toLowerCase() : undefined,
+    });
     captureException(error);
   }
 };
