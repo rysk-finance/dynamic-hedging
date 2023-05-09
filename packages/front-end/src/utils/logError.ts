4224b4caf2098835f91c4372b93e817cf64a9bbb
Tim <timothy.dunphy@gmail.com>
2023-05-09 16:31:28 +0100

    Set user earlier on.

--- packages/front-end/src/utils/logError.ts
+++ packages/front-end/src/utils/logError.ts
@@ -1,15 +1,9 @@
-import { captureException, setUser } from "@sentry/react";
-import { getAccount } from "@wagmi/core";
+import { captureException } from "@sentry/react";
 
 export const logError = (error: unknown) => {
   console.error(error);
 
   if (process.env.NODE_ENV !== "development") {
-    const { address } = getAccount();
-    setUser({
-      ip_address: "0.0.0.0",
-      username: address ? address.toLowerCase() : undefined,
-    });
     captureException(error);
   }
 };
