b57407ac19b070d03c6f5893cb7a77e05bac2783
Tim <timothy.dunphy@gmail.com>
2023-05-11 11:22:47 +0100

    Better UI whilst waiting for loading and add explainer tab.

--- packages/front-end/src/components/dashboard/OptionsTable/OptionsTable.tsx
+++ packages/front-end/src/components/dashboard/OptionsTable/OptionsTable.tsx
@@ -40,7 +40,7 @@ export const UserOptions = () => {
           content: (
             <>
               <AnimatePresence initial={false} mode="wait">
-                {(loading || error) && (
+                {!activePositions && (loading || error) && (
                   <LoadingOrError
                     key="loading-or-error"
                     error={error}
@@ -77,7 +77,7 @@ export const UserOptions = () => {
           content: (
             <>
               <AnimatePresence initial={false} mode="wait">
-                {(loading || error) && (
+                {!inactivePositions && (loading || error) && (
                   <LoadingOrError
                     key="loading-or-error"
                     error={error}
@@ -104,6 +104,31 @@ export const UserOptions = () => {
                 {isDisconnected && <Disconnected />}
               </AnimatePresence>
             </>
+          ),
+        },
+        {
+          label: "RYSK.Help",
+          content: (
+            <div className="p-4">
+              <h2 className="font-medium text-xl pb-4">
+                {"Understanding your positions."}
+              </h2>
+              <p className="pb-2">
+                {
+                  "From inside this section, you can visualise the state of your positions and perform some simple management actions."
+                }
+              </p>
+              <p className="pb-2">
+                {
+                  "If you wish to modify the health of your collateral for short positions, you can click the collateral value button, and an interface will appear. You can add or remove collateral from any position as you please."
+                }
+              </p>
+              <p>
+                {
+                  "The status column shows additional information or actions about your positions. If your position has been closed, settled, redeemed or has  expired, you will be shown here. If you wish to close your position, a button will appear to provide you with the necessary interfaces needed. In some cases, you will see that a position is currently untradeable. This can occur when the option is outside of a delta range of 10-70 or within one day to expiration. All of these circumstances aim to protect overall risk to the DHV."
+                }
+              </p>
+            </div>
           ),
         },
       ]}
