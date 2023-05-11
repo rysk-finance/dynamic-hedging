973b5e4f2e21e26c67df37dff47f818d1b20a1ce
Tim <timothy.dunphy@gmail.com>
2023-05-11 10:13:26 +0100

    Catch message where error is an object with key.

--- packages/front-end/src/utils/logError.ts
+++ packages/front-end/src/utils/logError.ts
@@ -1,9 +1,13 @@
 import { captureException } from "@sentry/react";
 
-export const logError = (error: unknown) => {
+export const logError = (error: any) => {
   console.error(error);
 
   if (process.env.NODE_ENV !== "development") {
-    captureException(error);
+    if (typeof error === "object" && "message" in error) {
+      captureException(error.message);
+    } else {
+      captureException(error);
+    }
   }
 };
