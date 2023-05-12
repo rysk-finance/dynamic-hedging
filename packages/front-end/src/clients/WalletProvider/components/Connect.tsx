96d667bf06517440d93632c0e18819dd0fc6130d
Tim <timothy.dunphy@gmail.com>
2023-05-12 11:51:29 +0100

    Show balances in UI.

--- packages/front-end/src/clients/WalletProvider/components/Connect.tsx
+++ packages/front-end/src/clients/WalletProvider/components/Connect.tsx
@@ -1,6 +1,7 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { AnimatePresence, motion } from "framer-motion";
 
+import { WETH, USDC, Ether } from "src/Icons";
 import FadeInOutFixedDelay from "src/animation/FadeInOutFixedDelay";
 import { useGlobalContext } from "src/state/GlobalContext";
 
@@ -16,8 +17,10 @@ const buttonProps = {
 
 const Connect = () => {
   const {
-    state: { unstoppableDomain },
+    state: { balances, unstoppableDomain },
   } = useGlobalContext();
+
+  const hasBalances = Object.values(balances).some((balance) => balance > 0);
 
   return (
     <Custom>
@@ -55,21 +58,51 @@ const Connect = () => {
             )}
 
             {connected && !chain.unsupported && (
-              <motion.button
-                onClick={openAccountModal}
-                title={`Connected to ${chain.name}.`}
-                key="connected"
-                {...buttonProps}
-              >
-                {chain.iconUrl && (
-                  <img
-                    alt={chain.name ?? "Chain icon"}
-                    src={chain.iconUrl}
-                    className="w-4 h-4 m-0 mr-2"
-                  />
+              <>
+                {hasBalances && (
+                  <motion.div
+                    className="hidden xl:flex items-center justify-center h-11 bg-black text-white mr-1 px-4"
+                    key="balances"
+                    layout="position"
+                    {...FadeInOutFixedDelay}
+                  >
+                    <span className="flex items-center">
+                      <WETH className="w-5 h-5 mr-1" />
+                      <p className="mr-2 pr-2 border-r border-white">
+                        {balances.WETH}
+                      </p>
+                    </span>
+                    <span className="flex items-center">
+                      <USDC className="w-5 h-5 mr-1" />
+                      <p className="mr-2 pr-2 border-r border-white">
+                        {balances.USDC}
+                      </p>
+                    </span>
+                    <span className="flex items-center">
+                      <div className="flex items-center justify-center w-5 h-5 mr-1 bg-white rounded-full">
+                        <Ether className="w-4 h-4" />
+                      </div>
+                      <p>{balances.ETH}</p>
+                    </span>
+                  </motion.div>
                 )}
-                {unstoppableDomain || account.ensName || account.displayName}
-              </motion.button>
+
+                <motion.button
+                  onClick={openAccountModal}
+                  title={`Connected to ${chain.name}.`}
+                  key="connected"
+                  {...buttonProps}
+                >
+                  {chain.iconUrl && (
+                    <img
+                      alt={chain.name ?? "Chain icon"}
+                      src={chain.iconUrl}
+                      className="w-4 h-4 m-0 mr-2"
+                    />
+                  )}
+                  {unstoppableDomain || account.ensName || account.displayName}
+                </motion.button>
+              </>
             )}
           </AnimatePresence>
         );
