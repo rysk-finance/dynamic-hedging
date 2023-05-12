217812958df81d99c05b23895268ec3752095a8f
Tim <timothy.dunphy@gmail.com>
2023-05-12 17:01:59 +0100

    Disable and show error message if capital is insufficient for closing a short. Also refactor state and UI to be more consistent with architecture.

--- packages/front-end/src/components/optionsTrading/Modals/CloseShortOptionModal/components/Pricing.tsx
+++ packages/front-end/src/components/optionsTrading/Modals/CloseShortOptionModal/components/Pricing.tsx
@@ -1,10 +1,41 @@
 import type { PricingProps } from "../types";
 
-import { RyskCountUp } from "src/components/shared/RyskCountUp";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
-export const Pricing = ({ positionData }: PricingProps) => {
-  const { fee, now, premium, quote, remainingBalance, slippage, title } =
-    positionData;
+import FadeInOutQuick from "src/animation/FadeInOutQuick";
+import { RyskCountUp } from "src/components/shared/RyskCountUp";
+import { getContractAddress } from "src/utils/helpers";
+
+export const Pricing = ({
+  collateralAddress,
+  remainingCollateral,
+  positionData,
+}: PricingProps) => {
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const {
+    fee,
+    hasRequiredCapital,
+    now,
+    premium,
+    quote,
+    remainingBalance,
+    slippage,
+    title,
+  } = positionData;
+
+  const isWeth = collateralAddress === getContractAddress("WETH");
+
+  useEffect(() => {
+    if (!hasRequiredCapital && quote) {
+      setErrorMessage("Insufficient balance to cover collateral.");
+    } else if (remainingBalance <= 0 && quote) {
+      setErrorMessage("Final balance cannot be negative.");
+    } else {
+      setErrorMessage("");
+    }
+  }, [positionData]);
 
   return (
     <div className="flex flex-col">
@@ -64,11 +95,30 @@ export const Pricing = ({ positionData }: PricingProps) => {
             {` USDC`}
           </p>
         </span>
-      </div>
 
-      <small className="flex flex-col pb-4 text-center leading-6 text-gray-600">
-        {`Last updated: ${now}`}
-      </small>
+        <span className="flex" id="collateral-balance">
+          <p className="mr-auto">{`Collateral after:`}</p>
+          <p className="font-medium">
+            <RyskCountUp value={remainingCollateral} />
+            {isWeth ? " WETH" : " USDC"}
+          </p>
+        </span>
+
+        <AnimatePresence mode="wait">
+          {errorMessage && (
+            <motion.small
+              className="block leading-6 text-red-500 text-right"
+              {...FadeInOutQuick}
+            >
+              {errorMessage}
+            </motion.small>
+          )}
+        </AnimatePresence>
+
+        <small className="flex flex-col pt-2 text-center leading-6 text-gray-600">
+          {`Last updated: ${now}`}
+        </small>
+      </div>
     </div>
   );
 };
