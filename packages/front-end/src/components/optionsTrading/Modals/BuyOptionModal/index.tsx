3f462cca9b3b0b4c9c823f539f56d282537cd97d
Tim <timothy.dunphy@gmail.com>
2023-05-23 13:47:19 +0100

    Remove unnecessary prop.

--- packages/front-end/src/components/optionsTrading/Modals/BuyOptionModal/index.tsx
+++ packages/front-end/src/components/optionsTrading/Modals/BuyOptionModal/index.tsx
@@ -156,7 +156,6 @@ export const BuyOptionModal = () => {
             transactionPending ||
             loading
           }
-          requiresConnection
           id="buy-button"
           {...getButtonProps(
             "buy",
