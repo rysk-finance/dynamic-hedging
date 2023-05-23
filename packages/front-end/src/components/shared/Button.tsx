d3792444811fb2925ab323443e24db16ef59a173
Tim <timothy.dunphy@gmail.com>
2023-05-23 13:47:04 +0100

    Check that the network is supported.

--- packages/front-end/src/components/shared/Button.tsx
+++ packages/front-end/src/components/shared/Button.tsx
@@ -1,9 +1,9 @@
 import type { HTMLMotionProps } from "framer-motion";
 
-import { useConnectModal } from "@rainbow-me/rainbowkit";
+import { useConnectModal, useChainModal } from "@rainbow-me/rainbowkit";
 import { motion } from "framer-motion";
 import { useCallback } from "react";
-import { useAccount } from "wagmi";
+import { useAccount, useNetwork } from "wagmi";
 
 import { useGlobalContext } from "../../state/GlobalContext";
 import { ActionType } from "../../state/types";
@@ -19,7 +19,9 @@ export const Button = ({
   ...props
 }: ButtonProps) => {
   const { openConnectModal } = useConnectModal();
+  const { openChainModal } = useChainModal();
   const { isDisconnected } = useAccount();
+  const { chain } = useNetwork();
 
   const { dispatch } = useGlobalContext();
 
@@ -37,22 +39,41 @@ export const Button = ({
     });
   }, [dispatch]);
 
-  if (requiresConnection && isDisconnected) {
+  const getDynamicProps = () => {
+    switch (true) {
+      case isDisconnected:
+        return {
+          children: "Click to connect",
+          onClick: () => {
+            if (isDisconnected && openConnectModal) {
+              openConnectModal();
+            }
+          },
+          title: "Click to connect a wallet.",
+        };
+
+      case chain?.unsupported:
+        return {
+          children: "Wrong network",
+          onClick: () => {
+            if (chain?.unsupported && openChainModal) {
+              openChainModal();
+            }
+          },
+          title: "Click to switch networks.",
+        };
+    }
+  };
+
+  if (requiresConnection && (isDisconnected || chain?.unsupported)) {
     return (
       <motion.button
         className={`border-black border-2 text-md px-2 py-1 !bg-black text-white ${props.className}`}
         id={props.id}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-        onClick={() => {
-          if (isDisconnected && openConnectModal) {
-            openConnectModal();
-          }
-        }}
-        title="Click to connect a wallet."
-      >
-        {`Click to connect`}
-      </motion.button>
+        {...getDynamicProps()}
+      />
     );
   }
   return (
