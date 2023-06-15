1626c1d8e746f749ce6b2a88c019eebd4523627a
Tim <timothy.dunphy@gmail.com>
2023-06-15 13:47:28 +0100

    RYSK-102: Fallback if country is undefined.

--- packages/front-end/src/components/optionsTrading/Modals/Shared/components/Disclaimer.tsx
+++ packages/front-end/src/components/optionsTrading/Modals/Shared/components/Disclaimer.tsx
@@ -11,7 +11,9 @@ export const Disclaimer = ({ children }: PropsWithChildren) => {
 
   const color = blocked ? "text-red-500" : "text-gray-600";
   const message = blocked
-    ? `Trading is not available for people or entities in ${country} and other restricted jurisdictions. Learn more in our Terms of Use.`
+    ? `Trading is not available for people or entities in ${
+        country || "your country"
+      } and other restricted jurisdictions. Learn more in our Terms of Use.`
     : children;
 
   return <small className={`block text-center p-4 ${color}`}>{message}</small>;
