ceb86b0da3de60f76ec416b6c75cdf8e376acffa
Tim <timothy.dunphy@gmail.com>
2023-06-20 17:28:40 +0100

    RYSK-4: Implement V1 of user stats.

--- packages/front-end/src/components/optionsTrading/index.tsx
+++ packages/front-end/src/components/optionsTrading/index.tsx
@@ -2,54 +2,59 @@ import { AnimatePresence, LayoutGroup } from "framer-motion";
 
 import { OptionChainModalActions } from "src/state/types";
 
-import { AssetPriceInfo } from "./AssetPriceInfo";
 import { ActiveExpiryAlerts } from "./ActiveExpiryAlerts";
+import { AssetPriceInfo } from "./AssetPriceInfo";
 import { Chain } from "./Chain";
 import { ExpiryDatePicker } from "./ExpiryDatePicker";
 import { Filters } from "./Filters/Filters";
-import { useModal } from "./hooks/useModal";
 import { BuyOptionModal } from "./Modals/BuyOptionModal";
 import { CloseOptionModal } from "./Modals/CloseOptionModal";
+import { CloseShortOptionModal } from "./Modals/CloseShortOptionModal";
 import { OperatorModal } from "./Modals/OperatorModal";
 import { SellOptionModal } from "./Modals/SellOptionModal";
-import { CloseShortOptionModal } from "./Modals/CloseShortOptionModal";
 import { BuyModalTutorial } from "./Tutorials/Buy";
 import { OptionChainTutorial } from "./Tutorials/Chain";
 import { SellModalTutorial } from "./Tutorials/Sell";
+import { UserStats } from "./UserStats";
+import { useModal } from "./hooks/useModal";
 
 export const OptionsTradingContent = () => {
   const [modalType] = useModal();
 
   return (
-    <section className="col-start-1 col-end-17 -mt-16">
-      <BuyModalTutorial />
-      <OptionChainTutorial />
-      <SellModalTutorial />
+    <>
+      <section className="col-start-1 col-end-17 -mt-16">
+        <BuyModalTutorial />
+        <OptionChainTutorial />
+        <SellModalTutorial />
 
-      <AssetPriceInfo />
+        <AssetPriceInfo />
 
-      <LayoutGroup>
-        <div className="relative border-2 border-black">
-          <ExpiryDatePicker />
-          <Filters />
-          <ActiveExpiryAlerts />
-          <Chain />
-        </div>
-      </LayoutGroup>
+        <LayoutGroup>
+          <div className="relative border-2 border-black">
+            <ExpiryDatePicker />
+            <Filters />
+            <ActiveExpiryAlerts />
+            <Chain />
+          </div>
+        </LayoutGroup>
 
-      <AnimatePresence mode="wait">
-        {modalType === OptionChainModalActions.BUY && (
-          <BuyOptionModal key="buy" />
-        )}
-        {modalType === OptionChainModalActions.CLOSE && <CloseOptionModal />}
-        {modalType === OptionChainModalActions.CLOSE_SHORT && (
-          <CloseShortOptionModal />
-        )}
-        {modalType === OptionChainModalActions.OPERATOR && <OperatorModal />}
-        {modalType === OptionChainModalActions.SELL && (
-          <SellOptionModal key="sell" />
-        )}
-      </AnimatePresence>
-    </section>
+        <AnimatePresence mode="wait">
+          {modalType === OptionChainModalActions.BUY && (
+            <BuyOptionModal key="buy" />
+          )}
+          {modalType === OptionChainModalActions.CLOSE && <CloseOptionModal />}
+          {modalType === OptionChainModalActions.CLOSE_SHORT && (
+            <CloseShortOptionModal />
+          )}
+          {modalType === OptionChainModalActions.OPERATOR && <OperatorModal />}
+          {modalType === OptionChainModalActions.SELL && (
+            <SellOptionModal key="sell" />
+          )}
+        </AnimatePresence>
+      </section>
+
+      <UserStats />
+    </>
   );
 };
