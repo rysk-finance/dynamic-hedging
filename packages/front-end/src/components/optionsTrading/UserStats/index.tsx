49bc45d01c277755dad0510eba7e90f452f4d72c
Tim <timothy.dunphy@gmail.com>
2023-06-20 17:38:44 +0100

    RYSK-4: Only render once chain data is loaded into state.

--- packages/front-end/src/components/optionsTrading/UserStats/index.tsx
+++ packages/front-end/src/components/optionsTrading/UserStats/index.tsx
@@ -8,6 +8,7 @@ import { Card } from "./components/Card";
 export const UserStats = () => {
   const {
     state: {
+      options: { data },
       userStats: { allTimePnL, delta },
     },
   } = useGlobalContext();
@@ -16,38 +17,40 @@ export const UserStats = () => {
 
   return (
     <AnimatePresence mode="wait">
-      <motion.section
-        className="grid grid-cols-4 col-start-1 col-end-17 gap-8 mt-8"
-        key="user-stats"
-        {...FadeInUpDelayed(0.3)}
-      >
-        <Card
-          explainer="Total P/L for all open and closed positions."
-          symbol="$"
-          title="P/L"
-          value={allTimePnL}
-        />
-        <Card
-          explainer="Total delta for all open positions."
-          symbol="Δ"
-          title="Delta"
-          value={delta}
-        />
-        <Card
-          disabled
-          explainer="Total gamma for all open positions."
-          symbol="Γ"
-          title="Gamma"
-          value={0}
-        />
-        <Card
-          disabled
-          explainer="Total theta for all open positions."
-          symbol="θ"
-          title="Theta"
-          value={0}
-        />
-      </motion.section>
+      {Object.values(data).length ? (
+        <motion.section
+          className="grid grid-cols-4 col-start-1 col-end-17 gap-8 mt-8"
+          key="user-stats"
+          {...FadeInUpDelayed(0.3)}
+        >
+          <Card
+            explainer="Total P/L for all open and closed positions."
+            symbol="$"
+            title="P/L"
+            value={allTimePnL}
+          />
+          <Card
+            explainer="Total delta for all open positions."
+            symbol="Δ"
+            title="Delta"
+            value={delta}
+          />
+          <Card
+            disabled
+            explainer="Total gamma for all open positions."
+            symbol="Γ"
+            title="Gamma"
+            value={0}
+          />
+          <Card
+            disabled
+            explainer="Total theta for all open positions."
+            symbol="θ"
+            title="Theta"
+            value={0}
+          />
+        </motion.section>
+      ) : null}
     </AnimatePresence>
   );
 };
