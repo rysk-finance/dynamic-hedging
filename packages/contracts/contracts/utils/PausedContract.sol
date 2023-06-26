94fab52e0d628cd7a609e17ae3b933032d536b81
kjr217 <kjraja217@gmail.com>
2023-06-26 15:58:54 +0100

    chore: do pause contract api properly

--- packages/contracts/contracts/utils/PausedContract.sol
+++ packages/contracts/contracts/utils/PausedContract.sol
@@ -3,8 +3,15 @@
 pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract PausedContract is Pausable {
+contract PausedContract {
+    OptionExchangePaused optionExchange;
 	constructor() {
+		optionExchange = new OptionExchangePaused();
+	}
+
+}
+contract OptionExchangePaused is Pausable {
+    constructor() {
 		_pause();
 	}
 }
