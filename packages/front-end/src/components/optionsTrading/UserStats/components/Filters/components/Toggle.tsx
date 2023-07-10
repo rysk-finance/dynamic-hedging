451643936e2a789e491917885345c9937fd6eda6
Tim <tim@rightly.co.uk>
2023-07-10 17:51:33 +0100

    RYSK-50: Branded colors.

--- packages/front-end/src/components/optionsTrading/UserStats/components/Filters/components/Toggle.tsx
+++ packages/front-end/src/components/optionsTrading/UserStats/components/Filters/components/Toggle.tsx
@@ -6,7 +6,7 @@ export const Toggle = ({ children, isLeft }: ToggleProps) => (
     <div className="relative w-8 h-4 ml-2 p-1 bg-bone-dark rounded-full shadow-[inset_0_0_10px_rgba(0,0,0,0.2)]">
       <div
         className={`absolute ${
-          isLeft ? "left-[0.25rem] bg-green-900" : "left-[1.25rem] bg-red-900"
+          isLeft ? "left-[0.25rem] bg-cyan" : "left-[1.25rem] bg-gray-600"
         } h-2 w-2 rounded-full ease-in-out duration-200`}
       />
     </div>
