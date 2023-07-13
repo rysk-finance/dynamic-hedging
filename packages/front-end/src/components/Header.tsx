08579c575b9ff6ea45f6077ce3c0ceeb62397b89
Tim <timothy.dunphy@gmail.com>
2023-07-13 13:12:26 +0100

    RYSK-237: Add closeable banner to nav.

--- packages/front-end/src/components/Header.tsx
+++ packages/front-end/src/components/Header.tsx
@@ -1,8 +1,10 @@
 import { LayoutGroup, motion } from "framer-motion";
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 import { Connect } from "src/clients/WalletProvider/components/Connect";
 import { AppPaths } from "../config/appPaths";
+import { Close } from "src/Icons";
 
 const links = [
   { id: "header-vault", path: AppPaths.VAULT, label: "Vault" },
@@ -13,37 +15,63 @@ const links = [
 export const Header = () => {
   const { pathname } = useLocation();
 
+  const [visible, setVisible] = useState(true);
+
+  const handleClose = () => setVisible(false);
+
   return (
-    <div className="fixed w-full h-24 t-0 flex items-center px-16 justify-between border-b-2 border-black bg-bone z-20">
-      <Link to={AppPaths.VAULT}>
-        <img
-          alt="Rysk logo"
-          className="w-20"
-          src={"/logo-animated.gif"}
-          title="Rysk: Uncorrelated Returns"
-        />
-      </Link>
+    <header className="sticky top-0 z-50 w-full bg-bone">
+      <nav className="flex items-center px-16 justify-between border-b-2 border-black">
+        <Link to={AppPaths.VAULT}>
+          <img
+            alt="Rysk logo"
+            className="h-20 py-4"
+            src={"/logo-animated.gif"}
+            title="Rysk: Uncorrelated Returns"
+          />
+        </Link>
 
-      <div className="flex items-center">
-        <LayoutGroup>
-          {/* <motion.div className="mr-4" layout="position">
-            {links.map(({ id, path, label }) => (
-              <Link
-                key={path}
-                id={id}
-                to={path}
-                className={`mr-2 border-none bg-transparent p-2 underline-offset-2 ${
-                  pathname === path ? "underline" : ""
-                }`}
-              >
-                {label}
-              </Link>
-            ))}
-          </motion.div> */}
+        <div className="flex items-center">
+          <LayoutGroup>
+            {/* <motion.div className="mr-4" layout="position">
+              {links.map(({ id, path, label }) => (
+                <Link
+                  key={path}
+                  id={id}
+                  to={path}
+                  className={`mr-2 border-none bg-transparent p-2 underline-offset-2 ${
+                    pathname === path ? "underline" : ""
+                  }`}
+                >
+                  {label}
+                </Link>
+              ))}
+            </motion.div> */}
 
-          <Connect />
-        </LayoutGroup>
-      </div>
-    </div>
+            <Connect />
+          </LayoutGroup>
+        </div>
+      </nav>
+
+      {visible && (
+        <b className="relative flex justify-center px-16 border-b-2 border-black font-normal">
+          <span className="mx-auto py-3 text-sm lg:text-base">
+            {`Rysk Finance is using native USDC. To swap your bridged USDC.e, you can `}
+            <a
+              className="text-cyan-dark-compliant underline"
+              href="https://app.1inch.io/#/42161/simple/swap/USDC.e/USDC"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              {`click here.`}
+            </a>
+          </span>
+
+          <button className="p-3" onClick={handleClose}>
+            <Close />
+          </button>
+        </b>
+      )}
+    </header>
   );
 };
