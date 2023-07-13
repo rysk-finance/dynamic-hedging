68bcd99c8a0484741d8b48380571d6c33aff83b3
Tim <timothy.dunphy@gmail.com>
2023-07-13 13:12:04 +0100

    RYSK-237: New cyan color for contrast compliant text.

--- packages/front-end/src/clients/WalletProvider/components/Disclaimer.tsx
+++ packages/front-end/src/clients/WalletProvider/components/Disclaimer.tsx
@@ -7,11 +7,11 @@ const Disclaimer: DisclaimerComponent = ({ Text, Link }) => {
     <Text>
       {`You have read and understand, and do hereby agree to the `}
       <Link href={TERMS_LINK}>
-        <small className="text-xs text-cyan-dark">{`Rysk user terms of service`}</small>
+        <small className="text-xs text-cyan-dark-compliant">{`Rysk user terms of service`}</small>
       </Link>
       {` and acknowledge that you have read and understand the `}
       <Link href={PP_LINK}>
-        <small className="text-xs text-cyan-dark">{`Rysk privacy policy.`}</small>
+        <small className="text-xs text-cyan-dark-compliant">{`Rysk privacy policy.`}</small>
       </Link>
     </Text>
   );
