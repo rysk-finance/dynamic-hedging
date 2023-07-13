ef1fcbdfa0904de3be886fe22707a8e6a6665eab
Tim <timothy.dunphy@gmail.com>
2023-07-13 13:25:44 +0100

    RYSK-237: Animate banner and keep bold for attention.

--- packages/front-end/src/components/Header.tsx
+++ packages/front-end/src/components/Header.tsx
@@ -1,10 +1,11 @@
-import { LayoutGroup, motion } from "framer-motion";
+import { AnimatePresence, LayoutGroup, motion } from "framer-motion";
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 import { Connect } from "src/clients/WalletProvider/components/Connect";
 import { AppPaths } from "../config/appPaths";
 import { Close } from "src/Icons";
+import FadeInDown from "src/animation/FadeInDownDelayed";
 
 const links = [
   { id: "header-vault", path: AppPaths.VAULT, label: "Vault" },
@@ -21,7 +22,7 @@ export const Header = () => {
 
   return (
     <header className="sticky top-0 z-50 w-full bg-bone">
-      <nav className="flex items-center px-16 justify-between border-b-2 border-black">
+      <nav className="relative flex items-center px-16 justify-between border-b-2 border-black bg-bone z-50">
         <Link to={AppPaths.VAULT}>
           <img
             alt="Rysk logo"
@@ -53,25 +54,30 @@ export const Header = () => {
         </div>
       </nav>
 
-      {visible && (
-        <b className="relative flex justify-center px-16 border-b-2 border-black font-normal">
-          <span className="mx-auto py-3 text-sm lg:text-base">
-            {`Rysk Finance is using native USDC. To swap your bridged USDC.e, you can `}
-            <a
-              className="text-cyan-dark-compliant underline"
-              href="https://app.1inch.io/#/42161/simple/swap/USDC.e/USDC"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              {`click here.`}
-            </a>
-          </span>
+      <AnimatePresence mode="wait">
+        {visible && (
+          <motion.b
+            className="relative flex justify-center px-16 border-b-2 border-black"
+            {...FadeInDown(0.3)}
+          >
+            <span className="mx-auto py-3 text-sm lg:text-base">
+              {`Rysk Finance is using native USDC. To swap your bridged USDC.e, you can `}
+              <a
+                className="text-cyan-dark-compliant underline"
+                href="https://app.1inch.io/#/42161/simple/swap/USDC.e/USDC"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                {`click here.`}
+              </a>
+            </span>
 
-          <button className="p-3" onClick={handleClose}>
-            <Close />
-          </button>
-        </b>
-      )}
+            <button className="p-3" onClick={handleClose}>
+              <Close />
+            </button>
+          </motion.b>
+        )}
+      </AnimatePresence>
     </header>
   );
 };
