0e30c3a8234a8698c5c9b358060ed0869fff5ade
Tim <timothy.dunphy@gmail.com>
2023-07-14 17:26:09 +0100

    fix-vault-network-load: Refactor to multi-call.

--- packages/front-end/src/components/vault/VaultStateManagement.tsx
+++ packages/front-end/src/components/vault/VaultStateManagement.tsx
@@ -1,99 +1,74 @@
-import { BigNumber } from "ethers";
-import { useCallback, useEffect } from "react";
-import { toast } from "react-toastify";
+import { useEffect } from "react";
 import { useAccount } from "wagmi";
+import { readContracts } from "@wagmi/core";
 
-import LPABI from "../../abis/LiquidityPool.json";
-import { useContract } from "../../hooks/useContract";
-import { useUserPosition } from "../../hooks/useUserPosition";
+import { LiquidityPoolABI } from "src/abis/LiquidityPool_ABI";
 import { VaultActionType } from "../../state/types";
 import { useVaultContext } from "../../state/VaultContext";
+import { getContractAddress } from "src/utils/helpers";
+
+const liquidityPoolContract = {
+  address: getContractAddress("liquidityPool"),
+  abi: LiquidityPoolABI,
+} as const;
 
 export const VaultStateManagment = () => {
   const { address } = useAccount();
   const { dispatch } = useVaultContext();
-  const { updatePosition } = useUserPosition();
-
-  const [lpContract] = useContract<{
-    DepositEpochExecuted: [];
-    WithdrawalEpochExecuted: [];
-  }>({
-    contract: "liquidityPool",
-    ABI: LPABI,
-    readOnly: true,
-    events: {
-      DepositEpochExecuted: async () => {
-        toast("✅ The deposit epoch was advanced");
-        EpochListener();
-      },
-      WithdrawalEpochExecuted: async () => {
-        // Assuming that epochs get advanced separately, we have
-        // two separate toast messages. If not we can remove one listener.
-        toast("✅ The withdraw epoch was advanced");
-        EpochListener();
-      },
-    },
-    isListening: {
-      DepositEpochExecuted: true,
-      WithdrawalEpochExecuted: true,
-    },
-  });
-
-  const getEpochData = useCallback(async () => {
-    if (lpContract) {
-      const depositEpoch: BigNumber = await lpContract.depositEpoch();
-      const withdrawalEpoch: BigNumber = await lpContract.withdrawalEpoch();
-      const depositEpochPricePerShare: BigNumber =
-        await lpContract.depositEpochPricePerShare(depositEpoch.sub(1));
-      const withdrawalEpochPricePerShare: BigNumber =
-        await lpContract.withdrawalEpochPricePerShare(withdrawalEpoch.sub(1));
-      return {
-        depositEpoch,
-        withdrawalEpoch,
-        depositEpochPricePerShare,
-        withdrawalEpochPricePerShare,
-      };
-    }
-  }, [lpContract]);
-
-  const EpochListener = useCallback(async () => {
-    const epochData = await getEpochData();
-    dispatch({
-      type: VaultActionType.SET,
-      data: { ...epochData },
-    });
-    if (address) {
-      updatePosition(address);
-    }
-  }, [dispatch, getEpochData, updatePosition, address]);
-
-  const getUserRyskBalance = useCallback(async () => {
-    if (lpContract && address) {
-      const balance: BigNumber = await lpContract.balanceOf(address);
-      return balance;
-    }
-  }, [lpContract, address]);
 
   useEffect(() => {
     const getInfo = async () => {
-      const epochData = await getEpochData();
-      const balance = await getUserRyskBalance();
-      if (epochData || balance) {
+      if (address) {
+        const [userDHVBalance, depositEpoch, withdrawalEpoch] =
+          await readContracts({
+            contracts: [
+              {
+                ...liquidityPoolContract,
+                functionName: "balanceOf",
+                args: [address],
+              },
+              {
+                ...liquidityPoolContract,
+                functionName: "depositEpoch",
+              },
+              {
+                ...liquidityPoolContract,
+                functionName: "withdrawalEpoch",
+              },
+            ],
+          });
+
+        const [withdrawPricePerShare, withdrawalPricePerShare] =
+          await readContracts({
+            contracts: [
+              {
+                ...liquidityPoolContract,
+                functionName: "depositEpochPricePerShare",
+                args: [depositEpoch.sub(1)],
+              },
+              {
+                ...liquidityPoolContract,
+                functionName: "withdrawalEpochPricePerShare",
+                args: [withdrawalEpoch.sub(1)],
+              },
+            ],
+          });
+
         dispatch({
           type: VaultActionType.SET,
           data: {
-            depositEpoch: epochData?.depositEpoch,
-            withdrawalEpoch: epochData?.withdrawalEpoch,
-            withdrawPricePerShare: epochData?.depositEpochPricePerShare,
-            withdrawalPricePerShare: epochData?.withdrawalEpochPricePerShare,
-            userDHVBalance: balance,
+            depositEpoch,
+            withdrawalEpoch,
+            withdrawPricePerShare,
+            withdrawalPricePerShare,
+            userDHVBalance,
           },
         });
       }
     };
 
     getInfo();
-  }, [dispatch, getEpochData, getUserRyskBalance]);
+  }, []);
 
   return null;
 };
