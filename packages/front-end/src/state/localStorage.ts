9e790461d71474667ac032b9e21c1f79b182af34
Tim <timothy.dunphy@gmail.com>
2023-07-17 15:22:32 +0100

    RYSK-26: Add utils for handling sets.

--- packages/front-end/src/state/localStorage.ts
+++ packages/front-end/src/state/localStorage.ts
@@ -2,6 +2,7 @@ export enum LocalStorageKeys {
   ACTIVE_POSITIONS_FILTERS_COMPACT = "rysk-apfc",
   ACTIVE_POSITIONS_FILTERS_HIDE_EXPIRED = "rysk-apfhe",
   ACTIVE_POSITIONS_FILTERS_SORTING = "rysk-apfs",
+  OPTION_CHAIN_FILTERS = "rysk-ocf",
   INACTIVE_POSITIONS_FILTERS_COMPACT = "rysk-ipfc",
   TRADING_PREFERENCES = "rysk-tp",
 }
@@ -20,3 +21,22 @@ export const setLocalStorageObject = (
   localStorageKey: `${LocalStorageKeys}`,
   data: Record<string, string | boolean | number>
 ) => localStorage.setItem(localStorageKey, JSON.stringify(data));
+
+export const getLocalStorageSet = <StorageObjectType = Set<string>>(
+  localStorageKey: `${LocalStorageKeys}`
+) => {
+  const storageObject = localStorage.getItem(localStorageKey);
+
+  return (
+    storageObject ? new Set(JSON.parse(storageObject)) : new Set()
+  ) as StorageObjectType;
+};
+
+export const setLocalStorageSet = (
+  localStorageKey: `${LocalStorageKeys}`,
+  data: Set<string | boolean | number>
+) => {
+  const asArray = Array.from(data);
+
+  localStorage.setItem(localStorageKey, JSON.stringify(asArray));
+};
