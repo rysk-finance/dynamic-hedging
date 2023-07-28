59637b30f1427e87efe8e629f5441d242975b04b
Tim <tim@rightly.co.uk>
2023-07-28 10:32:24 +0100

    production: Patch double value.

--- packages/front-end/src/components/VaultPerformance/subcomponents/Stats.tsx
+++ packages/front-end/src/components/VaultPerformance/subcomponents/Stats.tsx
@@ -1,7 +1,6 @@
 export const Stats = ({ cumulativeYield = 0 }: { cumulativeYield: number }) => (
   <div className="pb-8 py-12 px-8 flex flex-col lg:flex-row h-full justify-around">
     <h4 className="mb-2 text-xl">{`Historical Returns: ${cumulativeYield}%`}</h4>
-    <h4 className="mb-2 text-xl">{`Historical Returns: Soon™️`}</h4>
     <h4 className="mb-2 text-xl">{`Annualized Returns: Soon™️`}</h4>
   </div>
 );
