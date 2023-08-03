326c6e30db2082848d28f6ab34f2d5dc053114d1
Giacomo Brunetti <25010047+thundering-silence@users.noreply.github.com>
2023-08-03 11:41:44 +0200

    RYSK-305: full stops

--- packages/front-end/src/config/errors/erc20Errors.ts
+++ packages/front-end/src/config/errors/erc20Errors.ts
@@ -1,8 +1,8 @@
 export const ERC20_ERRORS = {
   "execution reverted: ERC20: transfer amount exceeds balance":
-    "Transfer amount exceeds balance",
+    "Transfer amount exceeds balance.",
   "execution reverted: ERC20: transfer amount exceeds allowance":
-    "Transfer amount exceeds allowance",
+    "Transfer amount exceeds allowance.",
 } as const;
 
 export const ERC20_KEYS = Object.keys(ERC20_ERRORS) as Array<
