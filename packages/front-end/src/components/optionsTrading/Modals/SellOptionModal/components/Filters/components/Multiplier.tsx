e5a9ad63e10192b8c02b01e64f768eef1dd736df
Tim <timothy.dunphy@gmail.com>
2023-08-04 17:27:35 +0100

    RYSK-255: Switch multiplier to input.

--- packages/front-end/src/components/optionsTrading/Modals/SellOptionModal/components/Filters/components/Multiplier.tsx
+++ packages/front-end/src/components/optionsTrading/Modals/SellOptionModal/components/Filters/components/Multiplier.tsx
@@ -1,3 +1,5 @@
+import type { ChangeEvent } from "react";
+
 import { useEffect, useState } from "react";
 import { useDebounce } from "use-debounce";
 
@@ -12,6 +14,21 @@ export const Multiplier = () => {
 
   const [multiplier, setMultiplier] = useState(collateralPreferences.amount);
   const [debouncedMultiplier] = useDebounce(multiplier, 300);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const amount = event.currentTarget.value;
+    const decimals = amount.split(".");
+    const rounded =
+      decimals.length > 1
+        ? `${decimals[0]}.${decimals[1].slice(0, 1)}`
+        : event.currentTarget.value;
+
+    setMultiplier(parseFloat(rounded));
+  };
+
+  const handleInputBlur = () => {
+    if (multiplier < 1.1) setMultiplier(1.1);
+  };
 
   useEffect(() => {
     dispatch({
@@ -38,17 +55,24 @@ export const Multiplier = () => {
   return (
     <div className="flex h-11 bg-white border border-gray-600 w-fit rounded-full">
       <button
-        className="py-2.5 px-4 w-11 border-r border-gray-600/40 rounded-l-full disabled:cursor-not-allowed disabled:bg-gray-600/30"
+        className="py-2.5 px-4 w-11 border-r border-gray-600/40 rounded-l-full disabled:cursor-not-allowed disabled:bg-gray-600/10"
         disabled={multiplier <= 1.1 || collateralPreferences.full}
         onClick={handleMultiplierChange("sub")}
       >
         {`-`}
       </button>
-      <span className="text-center py-2.5 px-2 w-16 number-input-hide-arrows">
-        {`${multiplier}x`}
-      </span>
+      <input
+        className="text-center py-2.5 px-2 w-16 number-input-hide-arrows disabled:cursor-not-allowed"
+        disabled={collateralPreferences.full}
+        inputMode="numeric"
+        onBlur={handleInputBlur}
+        onChange={handleInputChange}
+        step={0.1}
+        type="number"
+        value={multiplier}
+      />
       <button
-        className="py-2.5 px-4 w-11 border-l border-gray-600/40 rounded-r-full disabled:cursor-not-allowed disabled:bg-gray-600/30 "
+        className="py-2.5 px-4 w-11 border-l border-gray-600/40 rounded-r-full disabled:cursor-not-allowed disabled:bg-gray-600/10 "
         disabled={collateralPreferences.full}
         onClick={handleMultiplierChange("add")}
       >
