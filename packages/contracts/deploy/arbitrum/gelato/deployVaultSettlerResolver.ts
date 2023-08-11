bf58b0b9e7f1eb773a553017dfb94d12057c71b4
Josh <55187183+JoshDAO@users.noreply.github.com>
2023-08-11 12:54:42 +0100

    update deploy addresses

--- packages/contracts/deploy/arbitrum/gelato/deployVaultSettlerResolver.ts
+++ packages/contracts/deploy/arbitrum/gelato/deployVaultSettlerResolver.ts
@@ -1,16 +1,19 @@
 import hre, { ethers } from "hardhat"
 
+const multicallAddress = "0xA07C27805B17F4E5f6ab696A9Acf223B4aA78B60"
+const pvFeedAddress = "0x7f9d820CFc109686F2ca096fFA93dd497b91C073"
+
 const main = async () => {
 	const [deployer] = await ethers.getSigners()
 	console.log("Deploying contracts with the account:", deployer.address)
 
 	const resolverFactory = await ethers.getContractFactory("DeltaSettlerResolver")
-	const resolver = await resolverFactory.deploy()
+	const resolver = await resolverFactory.deploy(multicallAddress, pvFeedAddress)
 
 	try {
 		await hre.run("verify:verify", {
 			address: resolver.address,
-			constructorArguments: []
+			constructorArguments: [multicallAddress, pvFeedAddress]
 		})
 
 		console.log("resolver verified")
