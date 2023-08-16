094517d9ffb5705cbbded6011ffa8dd0775f9d6d
Tim <timothy.dunphy@gmail.com>
2023-08-16 18:12:15 +0100

    RYSK-299: New strategy icons.

---
+++ packages/front-end/src/Icons/Strategy/index.ts
@@ -0,0 +1,6 @@
+import { LongStraddle } from "./LongStraddle";
+import { LongStrangle } from "./LongStrangle";
+import { ShortStraddle } from "./ShortStraddle";
+import { ShortStrangle } from "./ShortStrangle";
+
+export { LongStraddle, LongStrangle, ShortStraddle, ShortStrangle };
