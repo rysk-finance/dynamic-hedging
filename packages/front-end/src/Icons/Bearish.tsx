80d3a16e507c937002f3f5666360a002bba3d19a
Tim <timothy.dunphy@gmail.com>
2023-08-17 10:57:09 +0100

    RYSK-299: Bullish and Bearish icons.

---
+++ packages/front-end/src/Icons/Bearish.tsx
@@ -0,0 +1,19 @@
+import type { SVGProps } from "react";
+
+export const Bearish = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M2.25 6L9 12.75l4.286-4.286a11.948 11.948 0 014.306 6.43l.776 2.898m0 0l3.182-5.511m-3.182 5.51l-5.511-3.181"
+    />
+  </svg>
+);
