924cc1af31e532d36567d2ecf95f90d12699aeb4
Tim <timothy.dunphy@gmail.com>
2023-08-17 11:09:57 +0100

    RYSK-299: Tweak layout.

--- packages/front-end/src/components/optionsTrading/Filters/components/Preferences/index.tsx
+++ packages/front-end/src/components/optionsTrading/Filters/components/Preferences/index.tsx
@@ -50,7 +50,7 @@ export const Preferences = () => {
   return (
     <>
       <button
-        className="ml-auto px-8 py-3 border-black border-l-2 ease-in-out duration-100 hover:bg-bone-light"
+        className="ml-auto px-8 py-2 border-black border-l-2 ease-in-out duration-100 hover:bg-bone-light"
         onClick={toggleModal}
       >
         <Cog className="h-7 w-7" />
