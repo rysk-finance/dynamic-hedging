66f7000bc96d70293eb1ec5b1dd9b2bf6c10e483
Tim <timothy.dunphy@gmail.com>
2023-08-23 13:43:09 +0100

    RYSK-300: Move info to it's won component.

---
+++ packages/front-end/src/components/optionsTrading/Modals/LongStraddleModal/components/Info.tsx
@@ -0,0 +1,25 @@
+import type { InfoProps } from "../types";
+
+import { Link } from "src/Icons";
+import { STRATEGY_LINKS } from "src/config/links";
+import { Symbol } from "../../Shared/components/Symbol";
+
+export const Info = ({ positionData }: InfoProps) => (
+  <Symbol {...positionData} strategyName="Long Straddle">
+    <p className="flex text-center text-sm justify-center">
+      {`Outlook: Neutral | Profit: Unlimited | Risk: Defined`}
+    </p>
+
+    <p className="flex text-center text-sm justify-center">
+      <a
+        className="flex !text-cyan-dark-compliant py-3"
+        href={STRATEGY_LINKS.LONG_STRADDLE}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Link className="w-4 h-4 mr-2 my-0.5" />
+        {`Learn more about long straddles.`}
+      </a>
+    </p>
+  </Symbol>
+);
