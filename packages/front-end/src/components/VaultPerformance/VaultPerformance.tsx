9675723a79685d31e0b3ca5d584b78bc54a30f2f
Tim Dunphy <54942744+MeanBoyCousin@users.noreply.github.com>
2023-08-25 17:22:45 +0100

    Update packages/front-end/src/components/VaultPerformance/VaultPerformance.tsx
    Co-authored-by: Dan <dan.ugolini@gmail.com>

--- packages/front-end/src/components/VaultPerformance/VaultPerformance.tsx
+++ packages/front-end/src/components/VaultPerformance/VaultPerformance.tsx
@@ -47,7 +47,7 @@ export const VaultPerformance = () => {
         : 0;
 
       // Values need replacing with API/Chain data.
-      const ethPrices = [1892.21, 1877.3, 1845.48, 1842.73, 1653.45, 1657.41];
+      const ethPrices = [1892.21, 1877.3, 1845.48, 1842.73, 1653.45, 1647.60];
 
       const adjustedChartData = pricePerShares.map((pricePoint, index) => {
         const pricePointGrowth = parseFloat(pricePoint.growthSinceFirstEpoch);
