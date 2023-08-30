ca78563290d8f25237ce46d94ea23c38ff7a65da
Tim <timothy.dunphy@gmail.com>
2023-08-30 11:45:58 +0100

    sentry-updates: Add fallback values for nullish params.

--- packages/front-end/src/utils/conversion-helper.ts
+++ packages/front-end/src/utils/conversion-helper.ts
@@ -3,7 +3,7 @@ import type { Dayjs } from "dayjs";
 import { BigNumber, BigNumberish, utils } from "ethers";
 import { BIG_NUMBER_DECIMALS } from "../config/constants";
 
-export const formatEth = (x: BigNumberish) => Number(utils.formatEther(x));
+export const formatEth = (x: BigNumberish = 0) => Number(utils.formatEther(x));
 export function truncate(num: number, places: number = 3): number {
   return (
     Math.trunc(Math.round(num * Math.pow(10, places))) / Math.pow(10, places)
@@ -11,7 +11,7 @@ export function truncate(num: number, places: number = 3): number {
 }
 export const tFormatEth = (x: BigNumberish, places: number = 3): number =>
   truncate(formatEth(x), places);
-export const toWei = (x: string) => utils.parseEther(x);
+export const toWei = (x: string = "0") => utils.parseEther(x);
 export const call = false,
   put = true;
 export const MAX_BPS = BigNumber.from(10000);
@@ -22,21 +22,22 @@ export const SECONDS_IN_SIXTY_DAYS = SECONDS_IN_DAY * 60;
 export const SECONDS_IN_YEAR = SECONDS_IN_DAY * 365.25;
 export const genOptionTime = (now: Dayjs, future: Dayjs) =>
   (future.unix() - now.unix()) / SECONDS_IN_YEAR;
-export const fromWei = (x: BigNumberish) => utils.formatEther(x);
-export const fromWeiToOpyn = (x: BigNumberish) =>
+export const fromWei = (x: BigNumberish = 0) => utils.formatEther(x);
+export const fromWeiToOpyn = (x: BigNumberish = 0) =>
   utils.parseUnits(utils.formatEther(x), 8);
-export const fromWeiToInt = (x: BigNumberish) => Number(utils.formatEther(x));
-export const fromUSDC = (x: BigNumberish) => utils.formatUnits(x, 6);
+export const fromWeiToInt = (x: BigNumberish = 0) =>
+  Number(utils.formatEther(x));
+export const fromUSDC = (x: BigNumberish = 0) => utils.formatUnits(x, 6);
 export const tFormatUSDC = (x: BigNumberish, places: number = 3) =>
   truncate(Number(fromUSDC(x)), places);
 export const fmtExpiration = (x: number) => toWei(x.toString());
-export const toUSDC = (x: string) => utils.parseUnits(x ? x : "0", 6);
-export const toOpyn = (x: string) => utils.parseUnits(x ? x : "0", 8);
-export const toRysk = (x: string) => utils.parseUnits(x ? x : "0", 18);
-export const toWeiFromUSDC = (x: string) => utils.parseUnits(x ? x : "0", 12);
-export const fromWeiToUSDC = (x: string) =>
+export const toUSDC = (x: string = "0") => utils.parseUnits(x, 6);
+export const toOpyn = (x: string = "0") => utils.parseUnits(x, 8);
+export const toRysk = (x: string = "0") => utils.parseUnits(x, 18);
+export const toWeiFromUSDC = (x: string = "0") => utils.parseUnits(x, 12);
+export const fromWeiToUSDC = (x: string = "0") =>
   utils.parseUnits(utils.formatEther(x), 6);
-export const fromOpyn = (x: BigNumberish) => utils.formatUnits(x, 8);
+export const fromOpyn = (x: BigNumberish = 0) => utils.formatUnits(x, 8);
 export const fromOpynHumanised = (x?: BigNumberish) => {
   if (!x) return undefined;
 
@@ -44,9 +45,9 @@ export const fromOpynHumanised = (x?: BigNumberish) => {
 };
 export const fromOpynToNumber = (x: BigNumberish) => Number(fromOpyn(x));
 export const fromOpynNoDecimal = (x: BigNumberish) => fromOpyn(x).split(".")[0];
-export const fromRysk = (x: string) => utils.formatUnits(x, 18);
+export const fromRysk = (x: string = "0") => utils.formatUnits(x, 18);
 export const fromRyskToNumber = (x: string) => Number(fromRysk(x));
-export const fromE27toInt = (value: BigNumberish) =>
+export const fromE27toInt = (value: BigNumberish = 0) =>
   parseFloat(utils.formatUnits(value, 27));
 export const getDiffSeconds = (now: Dayjs, future: Dayjs) =>
   future.unix() - now.unix();
