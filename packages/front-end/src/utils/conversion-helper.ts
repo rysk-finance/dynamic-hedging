cdd4e21fd94cabddd8e2e29b90ea9bf534d8fc4f
Tim <timothy.dunphy@gmail.com>
2023-09-04 15:13:10 +0100

    clean-up: Remove conversion utils.

--- packages/front-end/src/utils/conversion-helper.ts
+++ packages/front-end/src/utils/conversion-helper.ts
@@ -1,8 +1,11 @@
-import type { BigNumberish } from "ethers";
+import { BigNumberish, utils } from "ethers";
 
-import { utils } from "ethers";
+const SECONDS_IN_DAY = 86400;
+export const SECONDS_IN_WEEK = SECONDS_IN_DAY * 7;
+export const SECONDS_IN_SIXTY_DAYS = SECONDS_IN_DAY * 60;
+export const SECONDS_IN_YEAR = SECONDS_IN_DAY * 365.25;
 
-export const formatEth = (x: BigNumberish = 0) => Number(utils.formatEther(x));
+const formatEth = (x: BigNumberish = 0) => Number(utils.formatEther(x));
 export function truncate(num: number, places: number = 3): number {
   return (
     Math.trunc(Math.round(num * Math.pow(10, places))) / Math.pow(10, places)
@@ -11,15 +14,9 @@ export function truncate(num: number, places: number = 3): number {
 export const tFormatEth = (x: BigNumberish, places: number = 3): number =>
   truncate(formatEth(x), places);
 export const toWei = (x: string = "0") => utils.parseEther(x);
-export const SECONDS_IN_DAY = 86400;
-export const SECONDS_IN_WEEK = SECONDS_IN_DAY * 7;
-export const SECONDS_IN_SIXTY_DAYS = SECONDS_IN_DAY * 60;
-export const SECONDS_IN_YEAR = SECONDS_IN_DAY * 365.25;
 export const fromWei = (x: BigNumberish = 0) => utils.formatEther(x);
 export const fromWeiToOpyn = (x: BigNumberish = 0) =>
   utils.parseUnits(utils.formatEther(x), 8);
-export const fromWeiToUSDC = (x: BigNumberish = 0) =>
-  utils.parseUnits(utils.formatEther(x), 6);
 export const fromWeiToInt = (x: BigNumberish = 0) =>
   Number(utils.formatEther(x));
 export const fromUSDC = (x: BigNumberish = 0) => utils.formatUnits(x, 6);
@@ -30,7 +27,7 @@ export const toOpyn = (x: string = "0") => utils.parseUnits(x, 8);
 export const toRysk = (x: string = "0") => utils.parseUnits(x, 18);
 export const fromOpyn = (x: BigNumberish = 0) => utils.formatUnits(x, 8);
 export const fromOpynToNumber = (x: BigNumberish) => Number(fromOpyn(x));
-export const fromRysk = (x: string = "0") => utils.formatUnits(x, 18);
+const fromRysk = (x: string = "0") => utils.formatUnits(x, 18);
 export const fromRyskToNumber = (x: string) => Number(fromRysk(x));
 export const fromE27toInt = (value: BigNumberish = 0) =>
   parseFloat(utils.formatUnits(value, 27));
