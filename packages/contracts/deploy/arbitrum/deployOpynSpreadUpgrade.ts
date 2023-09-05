7d454fd550f7325a5d27b8900d541e1f814a4d7a
kjr217 <kjraja217@gmail.com>
2023-09-05 21:00:03 +0100

    chore: add comments on deployment steps

--- packages/contracts/deploy/arbitrum/deployOpynSpreadUpgrade.ts
+++ packages/contracts/deploy/arbitrum/deployOpynSpreadUpgrade.ts
@@ -5,6 +5,21 @@ import { DeployFunction } from "hardhat-deploy/types"
 import { getAddresses } from "../utils/opynAddress"
 import { NewMarginCalculator } from "../../types"
 
+
+// Mainnet wont use a script for the most part but the steps are split as follows:
+
+// 1. Entire system pause - Both opyn full pause and rysk exchange pause
+// 2. Margin Calculator deployment
+// 3. Set Spot Shocks and Upper Bound values for all combinations of products on the newly deployed Margin Calculator
+// 4. Set the fee and fee recipient on the newly deployed Margin Calculator
+// 5. Transfer ownership of the margin calculator to the governor multisig
+// 6. Set Margin calculator on address book
+// 7. Deploy new Controller contract
+// 8. Call setController on the address book using the newly deployed contract 
+// 9. Call initialize on the new Controller Implementation contract to make sure no one else can call it
+// 10. Call refreshConfiguration on the Controller Proxy contract and ensure that the proxy is pointing to the correct addresses
+// 11. Open some trades, create some of the new spreads etc.
+
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 	console.log(`Executing opyn upgrade to ${hre.network.name}. Hit ctrl + c to abort`)
     const { deployer } = await getNamedAccounts()
