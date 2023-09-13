ec4729eab95bcf0138a18d30be2d8ab9ad459020
Tim <timothy.dunphy@gmail.com>
2023-09-13 13:39:29 +0100

    RYSK-336: Update hook state.

--- packages/front-end/src/components/optionsTrading/Modals/CloseOptionModal/types.ts
+++ packages/front-end/src/components/optionsTrading/Modals/CloseOptionModal/types.ts
@@ -2,12 +2,17 @@ import type { BigNumber } from "ethers";
 
 import type { CallOrPut } from "src/state/types";
 
+export type CloseLongOperation = "close" | "sell";
+
 export interface PositionDataState {
   acceptablePremium: BigNumber;
   callOrPut?: CallOrPut;
   expiry: string;
   fee: number;
+  maxCloseSize?: number;
   now: string;
+  operation: CloseLongOperation;
+  orderTooBig: boolean;
   premium: number;
   quote: number;
   remainingBalance: number;
