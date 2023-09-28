45125b1cfc8826b1b9d0e2dc8e893abc264b4969
Tim <timothy.dunphy@gmail.com>
2023-09-28 11:47:50 +0100

    RYSK-393: Allow steps on button.

--- packages/front-end/src/components/optionsTrading/Modals/Shared/utils/getButtonProps.tsx
+++ packages/front-end/src/components/optionsTrading/Modals/Shared/utils/getButtonProps.tsx
@@ -6,14 +6,14 @@ import { capitalise } from "src/utils/caseConvert";
  *
  * @param action - Action for the modal. One of "buy" or "sell".
  * @param transactionPending - Boolean value to determine if the transaction or a fetch is pending.
- * @param isApproved - Boolean value to determine if the transaction is approved.
+ * @param isApproved - Boolean value(s) to determine if the transaction is approved.
  * @param handleApprove - Function to handle approval for the transaction.
  * @param handleTransaction - Function to handle the transaction - optional.
  */
 export const getButtonProps = (
   action: "buy" | "close" | "deposit" | "open" | "sell" | "withdraw",
   transactionPending: boolean,
-  isApproved: boolean,
+  isApproved: boolean | [boolean, boolean],
   handleApprove: () => Promise<void>,
   handleTransaction?: () => Promise<void>
 ) => {
@@ -27,6 +27,22 @@ export const getButtonProps = (
         title: "Transaction pending.",
       };
 
+    case Array.isArray(isApproved) && !isApproved[0] && Boolean(handleApprove):
+      return {
+        children: "Approve (0/2)",
+        key: "approve-first",
+        onClick: handleApprove,
+        title: "Click to approve.",
+      };
+
+    case Array.isArray(isApproved) && !isApproved[1] && Boolean(handleApprove):
+      return {
+        children: "Approve (1/2)",
+        key: "approve-second",
+        onClick: handleApprove,
+        title: "Click to approve.",
+      };
+
     case isApproved && Boolean(handleTransaction):
       return {
         children: capitalise(action),
@@ -37,7 +53,7 @@ export const getButtonProps = (
 
     default:
       return {
-        children: "Approve",
+        children: "Approve (0/1)",
         key: "approve",
         onClick: handleApprove,
         title: "Click to approve.",
