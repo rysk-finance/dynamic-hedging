a2e9938d0e41fd5d29970504b1010ef83452064d
kjr217 <kjraja217@gmail.com>
2023-10-08 02:18:28 +0100

    feat: update quant position lens

--- packages/contracts/deploy/arbitrum/deployQuantPositionLensMK1.ts
+++ packages/contracts/deploy/arbitrum/deployQuantPositionLensMK1.ts
@@ -4,10 +4,17 @@ import { QuantPositionLensMK1 } from "../../types/QuantPositionLensMK1"
 // update these addresses to connect to the appropriate set of contracts
 
 const protocolAddress = "0x4e920e9A901069d9b211646B6E191d81BA40E5FB"
+const catalogueAddress = "0x44227Dc2a1d71FC07DC254Dfd42B1C44aFF12168"
+const usdc = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"
+const weth = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1"
+
 export async function deployLens() {
 	const lensFactory = await ethers.getContractFactory("QuantPositionLensMK1")
 	const lens = (await lensFactory.deploy(
-		protocolAddress
+		protocolAddress,
+		catalogueAddress,
+		usdc,
+		weth
 	)) as QuantPositionLensMK1
 
 	console.log("quant lens contract deployed")
@@ -16,7 +23,10 @@ export async function deployLens() {
 		await hre.run("verify:verify", {
 			address: lens.address,
 			constructorArguments: [
-				protocolAddress
+				protocolAddress,
+				catalogueAddress,
+				usdc,
+				weth
 			]
 		})
 		console.log("quant lens verified")
