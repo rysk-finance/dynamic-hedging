154e1ad108f64c7d36fe5a9a015210ca68115d9d
Tim <timothy.dunphy@gmail.com>
2023-10-26 18:32:53 +0100

    RYSK-497: Add rewards page to navigation and header.

--- packages/front-end/src/config/appPaths.ts
+++ packages/front-end/src/config/appPaths.ts
@@ -4,6 +4,7 @@ export enum AppPaths {
   HOME = "/",
   OTC = "/otc",
   PRIVACY_POLICY = "/privacy-policy",
+  REWARDS = "/rewards",
   TERMS_OF_SERVICE = "/terms-of-service",
   TRADE = "/options",
   VAULT = "/vault",
